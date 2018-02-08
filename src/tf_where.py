5fb14e7300090f6b444c8ce802b9568cb86ae17b
Karel Ha <mathemage@gmail.com>
2018-02-08 16:27:35 +0100

    Compute strategies_lvl_2 via tf.where

--- src/tf_where.py
+++ src/tf_where.py
@@ -3,6 +3,9 @@
 import tensorflow as tf
 
 count_of_actions = 3
+shape3x3 = [count_of_actions, count_of_actions]
+shape3x3x3 = [count_of_actions, count_of_actions, count_of_actions]
+
 acting_players = tf.constant([[1, 0, 2],
                               [1, 1, 0],
                               [2, 0, 2]])
@@ -13,23 +16,24 @@ acting_players = tf.constant([[1, 0, 2],
 # reach_probabilities_without_normalization = [[0.05695065, 0.18175821, 0.19880699],
 #                                              [0.08976204, 0.15841936, 0.00814814],
 #                                              [0.03140438, 0.10418354, 0.17056671]]
-reach_probabilities_without_normalization = acting_players
-reach_probabilities = reach_probabilities_without_normalization\
-                      / tf.reduce_sum(reach_probabilities_without_normalization)
+reach_probabilities_without_normalization = tf.reshape(tf.range(0, 9), shape3x3)
+reach_probabilities_lvl_1 = reach_probabilities_without_normalization \
+                            / tf.reduce_sum(reach_probabilities_without_normalization)
 resolving_player = 1
+# resolving_player = 2
 
 mask_players = tf.equal(acting_players, resolving_player)
-shape3x3 = [count_of_actions, count_of_actions, count_of_actions]
-strategies = tf.reshape(tf.range(0, 27), shape3x3)
-
 mask_actions = tf.stack(values=[mask_players for _ in range(count_of_actions)], axis=-1)
 
-# reach_probabilities_lvl_2 = tf.where(mask_actions, tf.ones_like(reach_probabilities), reach_probabilities)
+strategies = tf.reshape(tf.range(0, 27), shape3x3x3)
+# contribution of strategies to the counterfactual probability
+strategies_lvl_2 = tf.where(mask_actions, tf.ones_like(strategies), strategies)
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	print("reach_probabilities:\n", sess.run(reach_probabilities))
-	print("mask_players:\n", sess.run(mask_players))
-	print("mask_actions:\n", sess.run(mask_actions))
-	# print("strategies:\n", sess.run(strategies))
-	# print("reach_probabilities_lvl_2:\n", sess.run(reach_probabilities_lvl_2))
+	# print("reach_probabilities_lvl_1:\n", sess.run(reach_probabilities_lvl_1))
+	print("acting_players:\n", sess.run(acting_players))
+	# print("mask_players:\n", sess.run(mask_players))
+	# print("mask_actions:\n", sess.run(mask_actions))
+	print("strategies:\n", sess.run(strategies))
+	print("strategies_lvl_2:\n", sess.run(strategies_lvl_2))

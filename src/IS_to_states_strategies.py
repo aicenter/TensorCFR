44bf5fa58e557279ba75cf688ba06599ea1451cb
Karel Ha <mathemage@gmail.com>
2018-02-09 15:45:42 +0100

    Define IS_strategies
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/IS_to_states_strategies.py
+++ src/IS_to_states_strategies.py
@@ -9,7 +9,13 @@ import tensorflow as tf
 from utils.tensor_utils import print_tensor
 
 state2IS = tf.Variable([0, 1, 2, 2, 4], name="state2IS")
+IS_strategies = tf.Variable([[1.0, 1.0, 1.0],  # of I0
+                             [0.1, 0.9, 0.0],  # of I1
+                             [0.2, 0.8, 0.0],  # of I2
+                             [0.3, 0.3, 0.3]],  # of Ic
+                            name="IS_strategies")
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
 	print_tensor(sess, state2IS)
+	print_tensor(sess, IS_strategies)

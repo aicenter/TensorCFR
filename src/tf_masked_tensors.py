4012d28456f592a36baeeaa4a19cea2604120e09
Karel Ha <mathemage@gmail.com>
2018-02-12 22:00:39 +0100

    Show that tf.where creates a copy of masked tensor
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/tf_masked_tensors.py
+++ src/tf_masked_tensors.py
@@ -8,11 +8,13 @@ from utils.tensor_utils import print_tensor
 tensor = tf.reshape(tf.range(1, 7), [3, 2], name="tensor")
 mask = tf.less(tensor, 3, name="mask")
 zero_tensor = tf.zeros_like(tensor, name="zero_tensor")
+original_tensor = tensor
 tensor = tf.where(mask, zero_tensor, tensor, name="tensor_v2")
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    print_tensor(sess, tensor)
+    # print_tensor(sess, tensor)
     # print_tensor(sess, zero_tensor)
     print_tensor(sess, mask)
     print_tensor(sess, tensor)
+    print_tensor(sess, original_tensor)

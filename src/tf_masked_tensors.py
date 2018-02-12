a3fe20a98f1b5786e60484e9d4a70e7e7d6609ea
Karel Ha <mathemage@gmail.com>
2018-02-12 21:52:56 +0100

    Mask tensor in tf_masked_tensors.py
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/tf_masked_tensors.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+
+# 1-D example
+from utils.tensor_utils import print_tensor
+
+tensor = tf.reshape(tf.range(1, 7), [3, 2], name="tensor")
+mask = tf.less(tensor, 3, name="mask")
+zero_tensor = tf.zeros_like(tensor, name="zero_tensor")
+tensor = tf.where(mask, zero_tensor, tensor, name="tensor_v2")
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print_tensor(sess, tensor)
+    # print_tensor(sess, zero_tensor)
+    print_tensor(sess, mask)
+    print_tensor(sess, tensor)

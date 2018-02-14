9afef2b77eacfd935561b5b18c2673838cd41efe
Karel Ha <mathemage@gmail.com>
2018-02-14 12:08:36 +0100

    Mask numbers <4 in tf_masked_tensors.py
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/tf_masked_tensors.py
+++ src/tf_masked_tensors.py
@@ -6,13 +6,12 @@ import tensorflow as tf
 from utils.tensor_utils import print_tensor
 
 tensor = tf.reshape(tf.range(1, 7), [3, 2], name="tensor")
-mask = tf.less(tensor, 3, name="mask")
+mask = tf.less(tensor, 4, name="mask")
 zero_tensor = tf.zeros_like(tensor, name="zero_tensor")
 original_tensor = tensor
 tensor = tf.where(mask, zero_tensor, tensor, name="tensor_v2")
-
 with tf.Session() as sess_via_tf_where:
     sess_via_tf_where.run(tf.global_variables_initializer())
+    print_tensor(sess_via_tf_where, original_tensor)
     print_tensor(sess_via_tf_where, mask)
     print_tensor(sess_via_tf_where, tensor)
-    print_tensor(sess_via_tf_where, original_tensor)

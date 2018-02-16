d7d1c970aa42ed7d0fd5f501f7dff0773fe27129
Karel Ha <mathemage@gmail.com>
2018-02-16 17:51:38 +0100

    Extract TestMaskedAssign.setUp() method
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/utils/test_masked_assign.py
+++ src/utils/test_masked_assign.py
@@ -4,20 +4,22 @@ import tensorflow as tf
 
 
 class TestMaskedAssign(TestCase):
-	def test_masked_assign(self):
-		shape2x2x2 = [2, 2, 2]
-		tensor = tf.Variable(tf.reshape(tf.range(1.0, 9.0), shape2x2x2), name="tensor")
-		mask = tf.less(tensor, 6.0, name="mask")
-		new_values = tf.fill(shape2x2x2, -0.5)
-		masked_assignment = masked_assign(ref=tensor, mask=mask, value=new_values)
-		expected_result = tf.constant( [[[-0.5, -0.5],
+	def setUp(self):
+		self.shape2x2x2 = [2, 2, 2]
+		self.tensor = tf.Variable(tf.reshape(tf.range(1.0, 9.0), self.shape2x2x2), name="tensor")
+		self.mask = tf.less(self.tensor, 6.0, name="mask")
+		self.new_values = tf.fill(self.shape2x2x2, -0.5)
+		self.masked_assignment = masked_assign(ref=self.tensor, mask=self.mask, value=self.new_values)
+
+	def test_range_2x2x2(self):
+		expected_result = tf.constant([[[-0.5, -0.5],
 		                                 [-0.5, -0.5]],
-		                                [[-0.5,    6],
-		                                 [    7,   8]]], dtype=tensor.dtype, name="expected_result")
-		are_tensors_equal = tf.reduce_all(tf.equal(x=tensor, y=expected_result))
+		                                [[-0.5,  6.0],
+		                                 [ 7.0,  8.0]]], dtype=self.tensor.dtype, name="expected_result")
+		are_tensors_equal = tf.reduce_all(tf.equal(x=self.tensor, y=expected_result))
 		with tf.Session() as sess:
 			sess.run(tf.global_variables_initializer())
 			self.assertFalse(sess.run(are_tensors_equal))
-			sess.run(masked_assignment)
-			print_tensors(sess, [tensor, mask, masked_assignment, expected_result, are_tensors_equal])
+			sess.run(self.masked_assignment)
+			print_tensors(sess, [self.tensor, self.mask, self.masked_assignment, expected_result, are_tensors_equal])
 			self.assertTrue(sess.run(are_tensors_equal))

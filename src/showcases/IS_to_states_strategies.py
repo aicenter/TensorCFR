d323183b4378730dcd9d6c48d153d360009ede2a
Karel Ha <mathemage@gmail.com>
2018-02-17 15:49:13 +0100

    Implement IS_to_states_strategies transformation
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/IS_to_states_strategies.py
+++ src/showcases/IS_to_states_strategies.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 import tensorflow as tf
-from utils.tensor_utils import print_tensor
 
-# I0 ... special index - all-1's strategy for counterfactual probability
-# I1 = { s1 }
-# I2 = { s2, s3 }
-# Ic = { s4 } -- chance node
+from utils.tensor_utils import print_tensors
 
-state2IS = tf.Variable([0, 1, 2, 2, 4], name="state2IS")
-IS_strategies = tf.Variable([[1.0, 1.0, 1.0],  # of I0
-                             [0.1, 0.9, 0.0],  # of I1
-                             [0.2, 0.8, 0.0],  # of I2
+# 3 actions, 1 layer looks like:
+#   I0 ... special index - all-1's strategy for counterfactual probability
+#   I1 = { s1 }
+#   I2 = { s2, s3 }
+#   I3 = Ic = { s4 } -- chance node
+state2IS = tf.Variable([0, 1, 2, 2, 3], name="state2IS")
+IS_strategies = tf.Variable([[1.0, 1.0, 1.0],   # of I0
+                             [0.1, 0.9, 0.0],   # of I1
+                             [0.2, 0.8, 0.0],   # of I2
                              [0.3, 0.3, 0.3]],  # of Ic
                             name="IS_strategies")
+state_strategies = tf.gather(params=IS_strategies, indices=state2IS, name="state_strategies")
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	print_tensor(sess, state2IS)
-	print_tensor(sess, IS_strategies)
+	print_tensors(sess, [state2IS, IS_strategies, state_strategies])

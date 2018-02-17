a1f9d601b2389e3b019424cecc73421228fbe8b0
Karel Ha <mathemage@gmail.com>
2018-02-17 23:10:08 +0100

    Make IS_to_states_strategies in 2 tree levels
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/IS_to_states_strategies.py
+++ src/showcases/IS_to_states_strategies.py
@@ -4,19 +4,32 @@ import tensorflow as tf
 
 from utils.tensor_utils import print_tensors
 
-# 3 actions, 1 layer looks like:
-#   I0 ... special index - all-1's strategy for counterfactual probability
-#   I1 = { s1 }
-#   I2 = { s2, s3 }
-#   I3 = Ic = { s4 } -- chance node
-state2IS = tf.Variable([0, 1, 2, 2, 3], name="state2IS")
-IS_strategies = tf.Variable([[1.0, 1.0, 1.0],   # of I0
-                             [0.1, 0.9, 0.0],   # of I1
-                             [0.2, 0.8, 0.0],   # of I2
-                             [0.3, 0.3, 0.3]],  # of Ic
-                            name="IS_strategies")
-state_strategies = tf.gather(params=IS_strategies, indices=state2IS, name="state_strategies")
-
+# level (depth) 0:
+#   I0,0 ... special index - all-1's strategy for counterfactual probability
+#   I0,1 = { sA, sB } -- root state, the opponent acts here
+#   there are 4 actions in s1
+state2IS_lvl0 = tf.Variable([1, 1], name="state2IS_lvl0")
+IS_strategies_lvl0 = tf.Variable([[1.0, 1.0, 1.0, 1.0],   # of I0,0
+                                  [0.5, .25, .05, 0.2]],  # of I0,1
+                                 name="IS_strategies_lvl0")
+state_strategies_lvl0 = tf.gather(params=IS_strategies_lvl0, indices=state2IS_lvl0, name="state_strategies_lvl0")
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	print_tensors(sess, [state2IS, IS_strategies, state_strategies])
+	print_tensors(sess, [state2IS_lvl0, IS_strategies_lvl0, state_strategies_lvl0])
+
+# level (depth) 1:
+#   I1,0 ... special index - all-1's strategy for counterfactual probability
+#   I1,1 = { s1 }
+#   I1,2 = { s2, s3 }
+#   I1,3 = Ic = { s4 } -- chance node
+#   each state 3 actions
+state2IS_lvl1 = tf.Variable([0, 1, 2, 2, 3], name="state2IS_lvl1")
+IS_strategies_lvl1 = tf.Variable([[1.0, 1.0, 1.0],   # of I1,0
+                                  [0.1, 0.9, 0.0],   # of I1,1
+                                  [0.2, 0.8, 0.0],   # of I1,2
+                                  [0.3, 0.3, 0.3]],  # of I1,c
+                                 name="IS_strategies_lvl1")
+state_strategies_lvl1 = tf.gather(params=IS_strategies_lvl1, indices=state2IS_lvl1, name="state_strategies_lvl1")
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	print_tensors(sess, [state2IS_lvl1, IS_strategies_lvl1, state_strategies_lvl1])

58f7c2ec80f2b0a4cfe345b0dd7a89182d79d4c8
Karel Ha <mathemage@gmail.com>
2018-02-17 15:40:00 +0100

    Test tf_gather with tensor3x3
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/tf_gather.py
+++ src/showcases/tf_gather.py
@@ -4,14 +4,23 @@ import tensorflow as tf
 
 from utils.tensor_utils import print_tensors
 
-count_of_actions = 2
-shape2x2 = [count_of_actions, count_of_actions]
+shape2x2 = [2, 2]
 tensor2x2 = tf.reshape(tf.range(0, 4), shape2x2, name="tensor2x2")
 mask2x2 = tf.Variable([[0, 1],
                        [1, 0]],
                       name="mask2x2")
 gathered_tensor = tf.gather(params=tensor2x2, indices=mask2x2, name="gathered_tensor")
-
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
 	print_tensors(sess, [tensor2x2, mask2x2, gathered_tensor])
+
+
+shape3x3 = [3, 3]
+tensor3x3 = tf.reshape(tf.range(0, 9), shape3x3, name="tensor3x3")
+mask2x2 = tf.Variable([[2, 1],
+                       [1, 0]],
+                      name="mask2x2")
+gathered_tensor = tf.gather(params=tensor3x3, indices=mask2x2, name="gathered_tensor")
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	print_tensors(sess, [tensor3x3, mask2x2, gathered_tensor])

9504199eb4b53161cb7480d307f447af915d1981
Karel Ha <mathemage@gmail.com>
2018-02-19 13:53:01 +0100

    Rename strategies to reach probabilities
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/IS_to_states_strategies.py
+++ src/showcases/IS_to_states_strategies.py
@@ -12,9 +12,9 @@ state2IS_lvl0 = tf.Variable([1], name="state2IS_lvl0")
 IS_strategies_lvl0 = tf.Variable([[1.0, 1.0, 1.0, 1.0, 1.0],   # of I0,0
                                   [0.5, .25, 0.1, 0.1, .05]],  # of I0,1
                                  name="IS_strategies_lvl0")
-state_strategies_lvl0 = tf.gather(params=IS_strategies_lvl0, indices=state2IS_lvl0, name="gathered_strategies_lvl1")
-state_strategies_lvl0 = tf.transpose(state_strategies_lvl0,
-                                   name="state_strategies_lvl0")  # 1st dim (rows) indexed by states, 2nd dim by actions
+state_strategies_lvl0 = tf.gather(params=IS_strategies_lvl0, indices=state2IS_lvl0, name="state_strategies_lvl0")
+reach_probabilities_lvl1 = tf.transpose(state_strategies_lvl0,
+                                        name="reach_probabilities_lvl1")  # 1st dim: by states, 2nd dim: by actions
 
 # level (depth) 1:
 #   I1,0 ... special index - all-1's strategy for counterfactual probability
@@ -29,14 +29,12 @@ IS_strategies_lvl1 = tf.Variable([[1.0, 1.0, 1.0],   # of I1,0
                                   [0.3, 0.3, 0.3]],  # of I1,c
                                  name="IS_strategies_lvl1")
 state_strategies_lvl1 = tf.gather(params=IS_strategies_lvl1, indices=state2IS_lvl1, name="state_strategies_lvl1")
+reach_probabilities_lvl2 = tf.multiply(reach_probabilities_lvl1, state_strategies_lvl1, name="reach_probabilities_lvl2")
 
-reach_probabilities = tf.multiply(state_strategies_lvl0, state_strategies_lvl1, name="reach_probabilities")
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
 	# more verbose output
-	print_tensors(sess, [state2IS_lvl0, IS_strategies_lvl0, state_strategies_lvl0])
-	print_tensors(sess, [state2IS_lvl1, IS_strategies_lvl1, state_strategies_lvl1])
-	print_tensors(sess, [reach_probabilities])
-
-	# more compact output
-	# print_tensors(sess, [state_strategies_lvl0, state_strategies_lvl1, reach_probabilities])
+	print("########## Level 0 ##########")
+	print_tensors(sess, [state2IS_lvl0, IS_strategies_lvl0, state_strategies_lvl0, reach_probabilities_lvl1])
+	print("########## Level 1 ##########")
+	print_tensors(sess, [state2IS_lvl1, IS_strategies_lvl1, state_strategies_lvl1, reach_probabilities_lvl2])

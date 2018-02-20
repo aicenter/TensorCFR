1bca9b5276e60a573cba27ec3df938cc4b95a8c6
Karel Ha <mathemage@gmail.com>
2018-02-20 22:41:32 +0100

    Use tf.assert_equal for test_masked_assign.py
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/utils/test_masked_assign.py
+++ src/utils/test_masked_assign.py
@@ -13,16 +13,15 @@ class TestMaskedAssign(TestCase):
 
 	def test_range_2x2x2(self):
 		expected_result = tf.constant([[[-0.5, -0.5],
-		                                 [-0.5, -0.5]],
-		                                [[-0.5,  6.0],
-		                                 [ 7.0,  8.0]]], dtype=self.tensor.dtype, name="expected_result")
-		are_tensors_equal = tf.reduce_all(tf.equal(x=self.tensor, y=expected_result))
+		                                [-0.5, -0.5]],
+
+		                               [[-0.5,  6.0],
+		                                [ 7.0,  8.0]]], dtype=self.tensor.dtype, name="expected_result")
 		with tf.Session() as sess:
 			sess.run(tf.global_variables_initializer())
-			self.assertFalse(sess.run(are_tensors_equal))
 			sess.run(self.masked_assignment)
-			print_tensors(sess, [self.tensor, self.mask, self.masked_assignment, expected_result, are_tensors_equal])
-			self.assertTrue(sess.run(are_tensors_equal))
+			print_tensors(sess, [self.tensor, self.mask, self.masked_assignment, expected_result])
+			tf.assert_equal(self.tensor, expected_result)
 
 	def test_mismatched_shape(self):
 		mismatched_shapes = [[1, 8], [8, 1], [2, 4], [4, 2]]

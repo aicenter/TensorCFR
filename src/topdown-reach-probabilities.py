5d289dbbceb929fff6b5b740047d9d3444def6e2
Karel Ha <mathemage@gmail.com>
2018-02-22 11:26:54 +0100

    Move computation of reach probabilities to topdown-reach-probabilities.py
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/topdown-reach-probabilities.py
+++ src/topdown-reach-probabilities.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 
-from utils.tensor_utils import print_tensors
+from utils.tensor_utils import print_tensors, expanded_multiply
 
 # custom-made game: doc/domain_01.png (https://gitlab.com/beyond-deepstack/TensorCFR/blob/master/doc/domain_01.png)
 
@@ -16,7 +16,11 @@ state2IS_lvl0 = tf.Variable(1, name="state2IS_lvl0")
 IS_strategies_lvl0 = tf.Variable([[1.0, 1.0, 1.0, 1.0, 1.0],   # of I0,0
                                   [0.5, .25, 0.1, 0.1, .05]],  # of I0,1
                                  name="IS_strategies_lvl0")
+# tensors to be computed at level 0
 state_strategies_lvl0 = tf.gather(params=IS_strategies_lvl0, indices=state2IS_lvl0, name="state_strategies_lvl0")
+
+reach_probabilities_lvl1 = expanded_multiply(expandable_tensor=reach_probabilities_lvl0,
+                                             expanded_tensor=state_strategies_lvl0, name="reach_probabilities_lvl1")
 
 ########## Level 1 ##########
 # I1,0 = { s' } ... special index - all-1's strategy for counterfactual probability
@@ -32,6 +36,8 @@ IS_strategies_lvl1 = tf.Variable([[1.0, 1.0, 1.0],   # of I1,0
                                  name="IS_strategies_lvl1")
 # tensors to be computed at level 1
 state_strategies_lvl1 = tf.gather(params=IS_strategies_lvl1, indices=state2IS_lvl1, name="state_strategies_lvl1")
+reach_probabilities_lvl2 = expanded_multiply(expandable_tensor=reach_probabilities_lvl1,
+                                             expanded_tensor=state_strategies_lvl1, name="reach_probabilities_lvl2")
 
 ########## Level 2 ##########
 # I2,0 = { s5, s8, s9, s18 } ... special index - all-1's strategy for counterfactual probability
@@ -54,14 +60,19 @@ IS_strategies_lvl2 = tf.Variable([[1.0, 1.0],   # of I2,0
                                   [0.4, 0.6],   # of I2,4
                                   [0.0, 0.0]],  # of I2,t ... no strategies terminal nodes <- mock-up strategy
                                  name="IS_strategies_lvl2")
+# tensors to be computed at level 2
 state_strategies_lvl2 = tf.gather(params=IS_strategies_lvl2, indices=state2IS_lvl2, name="state_strategies_lvl2")
+reach_probabilities_lvl3 = expanded_multiply(expandable_tensor=reach_probabilities_lvl2,
+                                             expanded_tensor=state_strategies_lvl2, name="reach_probabilities_lvl3")
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
 	# more verbose output
 	print("########## Level 0 ##########")
-	print_tensors(sess, [state2IS_lvl0, IS_strategies_lvl0, state_strategies_lvl0])
+	print_tensors(sess, [reach_probabilities_lvl0, state2IS_lvl0, IS_strategies_lvl0, state_strategies_lvl0])
 	print("########## Level 1 ##########")
-	print_tensors(sess, [state2IS_lvl1, IS_strategies_lvl1, state_strategies_lvl1])
+	print_tensors(sess, [reach_probabilities_lvl1, state2IS_lvl1, IS_strategies_lvl1, state_strategies_lvl1])
 	print("########## Level 2 ##########")
-	print_tensors(sess, [state2IS_lvl2, IS_strategies_lvl2, state_strategies_lvl2])
+	print_tensors(sess, [reach_probabilities_lvl2, state2IS_lvl2, IS_strategies_lvl2, state_strategies_lvl2])
+	print("########## Level 3 ##########")
+	print_tensors(sess, [reach_probabilities_lvl3])

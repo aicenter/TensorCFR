700d2ad62976d5583bfcf2279be54ba6a22f42e5
Karel Ha <mathemage@gmail.com>
2018-02-25 16:58:12 +0100

    Change values of acting players
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/constants.py
+++ src/constants.py
@@ -9,7 +9,7 @@ TERMINAL_NODE = 1
 IMAGINARY_NODE = 2
 
 # type of players
-ACTING_PLAYER = 0
-OPPONENT = 1
-CHANCE_PLAYER = 2
+ACTING_PLAYER = 1
+OPPONENT = 2
+CHANCE_PLAYER = 0
 TERMINAL_PLAYER = -1  # dummy value for acting players in terminal nodes

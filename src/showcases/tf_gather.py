a4ad9af90a1ee2631b0ea8796732bf5f5de4bcc7
Karel Ha <mathemage@gmail.com>
2018-02-28 00:13:06 +0100

    Try negative indices for tf.gather: not ok
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/tf_gather.py
+++ src/showcases/tf_gather.py
@@ -24,3 +24,14 @@ gathered_tensor = tf.gather(params=tensor3x3, indices=mask2x2, name="gathered_te
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
 	print_tensors(sess, [tensor3x3, mask2x2, gathered_tensor])
+
+# negative indices don't work in tf.gather
+# shape3x2 = [3, 2]
+# tensor3x2 = tf.reshape(tf.range(0, 6), shape3x2, name="tensor3x2")
+# mask2x2_negative = tf.Variable([[0, -1],
+#                                 [-2, -3]],
+#                                name="mask2x2")
+# gathered_tensor = tf.gather(params=tensor3x2, indices=mask2x2_negative, name="gathered_tensor")
+# with tf.Session() as sess:
+# 	sess.run(tf.global_variables_initializer())
+# 	print_tensors(sess, [tensor3x2, mask2x2_negative, gathered_tensor])

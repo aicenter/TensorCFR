a9fa13cf8f4f0d88d5c9a10612c6cf592bdcec7c
Karel Ha <mathemage@gmail.com>
2018-03-03 16:50:05 +0100

    Mask out assign_strategies_to_states(bool updating_player=NONE)
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/assign_strategies_to_nodes.py
+++ src/assign_strategies_to_nodes.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def assign_strategies_to_nodes(infoset_strategies, node_to_infoset, name):
+def assign_strategies_to_nodes(infoset_strategies, node_to_infoset, name, updating_player=None, acting_players=None):
 	"""Translate 2-D tensor `infoset_strategies` of strategies per information sets to strategies per game states.
 	The translation is done based on N-D tensor `states_to_infosets`: each state (indexed by N-D coordinate)
 	stores the index of its information set.
@@ -14,8 +14,16 @@ def assign_strategies_to_nodes(infoset_strategies, node_to_infoset, name):
 		:param infoset_strategies: A 2-D tensor of floats.
 		:param node_to_infoset: An N-D tensor of ints.
 		:param name: A string to name the resulting tensor operation.
+		:param updating_player: The index of the updating player. If both `updating_player` and `acting_acting_players` are
+			`None` (default), no masking is used for strategies.
+		:param acting_players: A tensor of the same shape as `node_to_infoset`, representing acting players per infosets.
 
 	Returns:
 		A corresponding TensorFlow operation (from the computation graph).
 	"""
-	return tf.gather(params=infoset_strategies, indices=node_to_infoset, name=name)
+	if (updating_player is not None) and (acting_players is not None):  # counterfactual reach probabilities
+		strategies = tf.where(condition=tf.equal(acting_players, updating_player), x=tf.ones_like(infoset_strategies),
+		                      y=infoset_strategies)
+	else:
+		strategies = infoset_strategies
+	return tf.gather(params=strategies, indices=node_to_infoset, name=name)
--- src/topdown-reach-probabilities.py
+++ src/topdown-reach-probabilities.py
@@ -3,25 +3,32 @@
 import tensorflow as tf
 
 from assign_strategies_to_nodes import assign_strategies_to_nodes
+from constants import UPDATING_PLAYER
 from domain.domain_01 import reach_probabilities_lvl0, node_to_IS_lvl0, IS_strategies_lvl0, node_to_IS_lvl1, \
-	IS_strategies_lvl1, node_to_IS_lvl2, IS_strategies_lvl2
+	IS_strategies_lvl1, node_to_IS_lvl2, IS_strategies_lvl2, IS_acting_players_lvl0, IS_acting_players_lvl1, \
+	IS_acting_players_lvl2
 from utils.tensor_utils import print_tensors, expanded_multiply
 
 # custom-made game: doc/domain_01.png (https://gitlab.com/beyond-deepstack/TensorCFR/blob/master/doc/domain_01.png)
 
+updating_player = UPDATING_PLAYER
+
 # tensors to be computed at level 0
-node_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0, name="node_strategies_lvl0")
+node_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0, updating_player=updating_player,
+                                                  acting_players=IS_acting_players_lvl0, name="node_strategies_lvl0")
 
 reach_probabilities_lvl1 = expanded_multiply(expandable_tensor=reach_probabilities_lvl0,
                                              expanded_tensor=node_strategies_lvl0, name="reach_probabilities_lvl1")
 
 # tensors to be computed at level 1
-node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1, name="node_strategies_lvl1")
+node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1, updating_player=updating_player,
+                                                  acting_players=IS_acting_players_lvl1, name="node_strategies_lvl1")
 reach_probabilities_lvl2 = expanded_multiply(expandable_tensor=reach_probabilities_lvl1,
                                              expanded_tensor=node_strategies_lvl1, name="reach_probabilities_lvl2")
 
 # tensors to be computed at level 2
-node_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2, name="node_strategies_lvl2")
+node_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2, updating_player=updating_player,
+                                                  acting_players=IS_acting_players_lvl2, name="node_strategies_lvl2")
 reach_probabilities_lvl3 = expanded_multiply(expandable_tensor=reach_probabilities_lvl2,
                                              expanded_tensor=node_strategies_lvl2, name="reach_probabilities_lvl3")
 

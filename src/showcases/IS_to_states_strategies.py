971d63a6cb63014336477fad06f75e93d42d2d65
Karel Ha <mathemage@gmail.com>
2018-03-03 17:02:30 +0100

    Rename node_strategies_cf_lvl* to node_strategies_lvl* in IS_to_states_strategies.py
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/IS_to_states_strategies.py
+++ src/showcases/IS_to_states_strategies.py
@@ -13,14 +13,14 @@ from utils.tensor_utils import print_tensors
 ########## Level 0 ##########
 # I0,0 = {} ... special index - all-1's strategy for counterfactual probability
 # I0,1 = { s } ... root node, the opponent acts here
-node_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0, name="node_cf_strategies_lvl0")
+node_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0, name="node_strategies_lvl0")
 
 ########## Level 1 ##########
 # I1,0 = { s' } ... special index - all-1's strategy for counterfactual probability
 # I1,1 = { s1 }
 # I1,2 = { s2, s3 }
 # I1,3 = Ic = { s4 } ... chance node
-node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1, name="node_cf_strategies_lvl1")
+node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1, name="node_strategies_lvl1")
 
 ########## Level 2 ##########
 # I2,0 = { s5, s8, s9, s18 } ... special index - all-1's strategy for counterfactual probability
@@ -29,7 +29,7 @@ node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS
 # I2,3 = { s12, s15 } ... chance nodes
 # I2,4 = { s19 }
 # I2,t = { s7, s10, s13, s16, s17 } ... terminal nodes
-node_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2, name="node_cf_strategies_lvl2")
+node_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2, name="node_strategies_lvl2")
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())

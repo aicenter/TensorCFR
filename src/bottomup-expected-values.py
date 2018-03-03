0221e219b09f23b005b2aea2182ddd8ca7896238
Karel Ha <mathemage@gmail.com>
2018-03-03 16:59:02 +0100

    Rename node_strategies_lvl* to node_cf_strategies_lvl*
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/bottomup-expected-values.py
+++ src/bottomup-expected-values.py
@@ -10,18 +10,18 @@ from utils.tensor_utils import print_tensors, expanded_multiply
 # custom-made game: doc/domain_01.png (https://gitlab.com/beyond-deepstack/TensorCFR/blob/master/doc/domain_01.png)
 
 # tensors to be computed at level 0
-node_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0, name="node_strategies_lvl0")
+node_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0, name="node_cf_strategies_lvl0")
 
 reach_probabilities_lvl1 = expanded_multiply(expandable_tensor=reach_probabilities_lvl0,
                                              expanded_tensor=node_strategies_lvl0, name="reach_probabilities_lvl1")
 
 # tensors to be computed at level 1
-node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1, name="node_strategies_lvl1")
+node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1, name="node_cf_strategies_lvl1")
 reach_probabilities_lvl2 = expanded_multiply(expandable_tensor=reach_probabilities_lvl1,
                                              expanded_tensor=node_strategies_lvl1, name="reach_probabilities_lvl2")
 
 # tensors to be computed at level 2
-node_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2, name="node_strategies_lvl2")
+node_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2, name="node_cf_strategies_lvl2")
 reach_probabilities_lvl3 = expanded_multiply(expandable_tensor=reach_probabilities_lvl2,
                                              expanded_tensor=node_strategies_lvl2, name="reach_probabilities_lvl3")
 

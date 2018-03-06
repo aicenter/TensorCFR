60cd0de63df8ddb298222813c42fd42312703f5c
Karel Ha <mathemage@gmail.com>
2018-03-06 22:30:27 +0100

    Put updating_player inside get_node_cf_strategies()
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domain/topdown_reach_probabilities_domain01.py
+++ src/domain/topdown_reach_probabilities_domain01.py
@@ -7,9 +7,8 @@ from domain.domain_01 import reach_probabilities_lvl0, node_to_IS_lvl0, IS_strat
 	IS_acting_players_lvl2
 from utils.tensor_utils import print_tensors, expanded_multiply
 
-# custom-made game: doc/domain_01.png (https://gitlab.com/beyond-deepstack/TensorCFR/blob/master/doc/domain_01.png)
 
-updating_player = PLAYER1
+# custom-made game: doc/domain_01.png (https://gitlab.com/beyond-deepstack/TensorCFR/blob/master/doc/domain_01.png)
 
 
 def get_node_strategies():
@@ -19,7 +18,7 @@ def get_node_strategies():
 	return [node_strategies_lvl0, node_strategies_lvl1, node_strategies_lvl2]
 
 
-def get_node_cf_strategies():
+def get_node_cf_strategies(updating_player=PLAYER1):
 	# TODO generate node_cf_strategies_* with tf.where on node_strategies
 	node_cf_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0,
 	                                                     updating_player=updating_player,
@@ -48,19 +47,20 @@ def get_reach_probabilities():
 
 
 if __name__ == '__main__':
+	updating_player = PLAYER1
 	node_strategies = get_node_strategies()
-	node_cf_strategies = get_node_cf_strategies()
+	node_cf_strategies = get_node_cf_strategies(updating_player=updating_player)
 	reach_probabilities = get_reach_probabilities()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		print("########## Level 0 ##########")
 		print_tensors(sess, [reach_probabilities[0], node_to_IS_lvl0, IS_strategies_lvl0, node_strategies[0],
-												 node_cf_strategies[0]])
+		                     node_cf_strategies[0]])
 		print("########## Level 1 ##########")
 		print_tensors(sess, [reach_probabilities[1], node_to_IS_lvl1, IS_strategies_lvl1, node_strategies[1],
-												 node_cf_strategies[1]])
+		                     node_cf_strategies[1]])
 		print("########## Level 2 ##########")
 		print_tensors(sess, [reach_probabilities[2], node_to_IS_lvl2, IS_strategies_lvl2, node_strategies[2],
-												 node_cf_strategies[2]])
+		                     node_cf_strategies[2]])
 		print("########## Level 3 ##########")
 		print_tensors(sess, [reach_probabilities[3]])

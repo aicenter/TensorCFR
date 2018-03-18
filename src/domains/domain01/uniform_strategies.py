8ab82a09575af137000195ce9e384cab2130a847
Karel Ha <mathemage@gmail.com>
2018-03-18 19:48:58 +0100

    Implement `get_IS_uniform_strategies()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/uniform_strategies.py
+++ src/domains/domain01/uniform_strategies.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+from constants import IMAGINARY_NODE
 from domains.domain01.domain_01 import levels, get_node_types, get_node_to_IS, get_IS_strategies
 from utils.tensor_utils import print_tensors
 
@@ -24,19 +25,25 @@ def get_IS_children_types():  # TODO unittest
 	return IS_children_types
 
 
-# def get_IS_uniform_strategies():
-# 	# TODO
-# 	return [None] * (levels - 1)
+def get_IS_uniform_strategies():
+	IS_children_types = get_IS_children_types()
+	IS_uniform_strategies = [None] * (levels - 1)
+	for level in range(levels - 1):
+		IS_uniform_strategies[level] = tf.to_float(tf.not_equal(IS_children_types[level], IMAGINARY_NODE))
+		# Note: An all-0's row cannot be normalized. This is caused when IS has only imaginary children. As of now,
+		# `tf.divide` produces `nan` in the entire row.
+		IS_uniform_strategies[level] = tf.divide(IS_uniform_strategies[level],
+		                                         tf.reduce_sum(IS_uniform_strategies[level], axis=-1, keep_dims=True),
+		                                         name="IS_uniform_strategies_lvl{}".format(level))
+	return IS_uniform_strategies
 
 
 if __name__ == '__main__':
 	node_types_ = get_node_types()
-	# IS_uniform_strategies_ = get_IS_uniform_strategies()
+	IS_uniform_strategies_ = get_IS_uniform_strategies()
 	IS_children_types_ = get_IS_children_types()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		# for i in range(levels - 1):
-		for i in range(0, levels - 1):
+		for i in range(levels - 1):
 			print("########## Level {} ##########".format(i))
-			# print_tensors(sess, [node_types_[i], node_uniform_strategies_[i], IS_uniform_strategies_[i]])
-			print_tensors(sess, [node_types_[i], IS_children_types_[i]])
+			print_tensors(sess, [node_types_[i], IS_children_types_[i], IS_uniform_strategies_[i]])

870cb3a883fbc4aff147330f6a7bc89b85c794b9
Karel Ha <mathemage@gmail.com>
2018-03-20 22:51:36 +0100

    Showcase `tf_numerical_instability` with `tf.float64`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/tf_numerical_instability.py
+++ src/showcases/tf_numerical_instability.py
@@ -4,10 +4,12 @@ import tensorflow as tf
 
 from utils.tensor_utils import print_tensors
 
-result = tf.multiply(tf.constant(0.8), tf.constant(0.1), name="test_tensor")
-expected_result = tf.constant(0.08, name="expected_result")
+a = tf.constant(0.8, dtype=tf.float64)
+b = tf.constant(0.1, dtype=tf.float64)
+result = tf.multiply(a, b, name="test_tensor")
+expected_result = tf.constant(0.08, dtype=tf.float64, name="expected_result")
 equality_of_results = tf.equal(result, expected_result, name="equality_of_results")
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	print_tensors(sess, [result, expected_result, equality_of_results])
+	print_tensors(sess, [a, b, result, expected_result, equality_of_results])

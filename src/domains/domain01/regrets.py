a66aebf2d421536f3fda89f30e9bbed528755d6b
Karel Ha <mathemage@gmail.com>
2018-03-22 23:42:05 +0100

    Update positive cumulative (counterfactual) regrets
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/regrets.py
+++ src/domains/domain01/regrets.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 
 from domains.domain01.counterfactual_values import get_cf_values_IS_actions, get_cf_values_IS
-from domains.domain01.domain_01 import levels
+from domains.domain01.domain_01 import levels, positive_cumulative_regrets
 from utils.tensor_utils import print_tensors
 
 
@@ -16,12 +16,32 @@ def get_regrets():  # TODO verify and write a unittest
 	        for level in range(levels - 1)]
 
 
+def update_positive_cumulative_regrets(regrets=get_regrets()):  # TODO verify and write a unittest
+	updated_regrets = [None] * (levels - 1)
+	for level in range(levels - 1):
+		maximum_addition = tf.maximum(- positive_cumulative_regrets[level],  # to keep cumulative regret still positive
+		                              regrets[level])
+		updated_regrets[level] = tf.assign_add(ref=positive_cumulative_regrets[level], value=maximum_addition,
+		                                       name="update_regrets_lvl{}".format(level))
+	return updated_regrets
+
+
 if __name__ == '__main__':
 	cf_values_IS_actions_ = get_cf_values_IS_actions()
 	cf_values_IS_ = get_cf_values_IS()
-	regrets = get_regrets()
+	regrets_ = get_regrets()
+	update_regrets = update_positive_cumulative_regrets()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		for i in range(levels - 1):
 			print("########## Level {} ##########".format(i))
-			print_tensors(sess, [cf_values_IS_actions_[i], cf_values_IS_[i], regrets[i]])
+			print_tensors(sess, [cf_values_IS_actions_[i], cf_values_IS_[i], regrets_[i], update_regrets[i],
+			                     positive_cumulative_regrets[i]])
+
+			# TODO create a unit out of following lines
+			# print_tensors(sess, [regrets_[i]])
+			# print_tensors(sess, [positive_cumulative_regrets[i]])
+			# sess.run(update_regrets[i])
+			# print_tensors(sess, [positive_cumulative_regrets[i]])
+			# print_tensors(sess, [update_regrets[i]])
+			# print_tensors(sess, [positive_cumulative_regrets[i], positive_cumulative_regrets[i]])

6cffbc2f45dc9a3105dc0d0cda3a40fbde69650c
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-03-23 17:52:13 +0100

    [#3] Add test cases to test the normalize method

--- src/utils/test_normalize.py
+++ src/utils/test_normalize.py
@@ -69,3 +69,75 @@ class TestTensorNormalization(tf.test.TestCase):
 			output = sess.run(normalize(self.input_tensor_3dim, order=2))
 
 			self.assertNDArrayNear(output, expected_output, self.error_tolerance)
+
+	def test_3d_tensor_l1_norm_axis0(self):
+		expected_output = np.array([[[0., 0.16666667],
+		                             [0.25, 0.3]],
+
+		                            [[1, 0.83333333],
+		                             [0.75, 0.7]]])
+
+		with self.test_session() as sess:
+			output = sess.run(normalize(self.input_tensor_3dim, axis=0, order=1))
+
+			self.assertNDArrayNear(output, expected_output, self.error_tolerance)
+
+	def test_3d_tensor_l1_norm_axis1(self):
+		expected_output = np.array([[[0., 0.25],
+		                             [1., 0.75]],
+
+		                            [[0.4, 0.41666667],
+		                             [0.6, 0.58333333]]])
+
+		with self.test_session() as sess:
+			output = sess.run(normalize(self.input_tensor_3dim, axis=1, order=1))
+
+			self.assertNDArrayNear(output, expected_output, self.error_tolerance)
+
+	def test_3d_tensor_l1_norm_axis2(self):
+		expected_output = np.array([[[0., 1.],
+		                             [0.4, 0.6]],
+
+		                            [[0.44444444, 0.55555556],
+		                             [0.46153846, 0.53846154]]])
+
+		with self.test_session() as sess:
+			output = sess.run(normalize(self.input_tensor_3dim, axis=2, order=1))
+
+			self.assertNDArrayNear(output, expected_output, self.error_tolerance)
+
+	def test_3d_tensor_l2_norm_axis0(self):
+		expected_output = np.array([[[0., 0.19611614],
+		                             [0.31622777, 0.3939193]],
+
+		                            [[1, 0.98058068],
+		                             [0.9486833, 0.91914503]]])
+
+		with self.test_session() as sess:
+			output = sess.run(normalize(self.input_tensor_3dim, axis=0, order=2))
+
+			self.assertNDArrayNear(output, expected_output, self.error_tolerance)
+
+	def test_3d_tensor_l2_norm_axis1(self):
+		expected_output = np.array([[[0., 0.31622777],
+		                             [1., 0.9486833]],
+
+		                            [[0.5547002, 0.58123819],
+		                             [0.83205029, 0.81373347]]])
+
+		with self.test_session() as sess:
+			output = sess.run(normalize(self.input_tensor_3dim, axis=1, order=2))
+
+			self.assertNDArrayNear(output, expected_output, self.error_tolerance)
+
+	def test_3d_tensor_l2_norm_axis2(self):
+		expected_output = np.array([[[0., 1.],
+		                             [0.5547002 , 0.83205029]],
+
+		                            [[0.62469505, 0.78086881],
+		                             [0.65079137, 0.7592566]]])
+
+		with self.test_session() as sess:
+			output = sess.run(normalize(self.input_tensor_3dim, axis=2, order=2))
+
+			self.assertNDArrayNear(output, expected_output, self.error_tolerance)

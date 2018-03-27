342e371fbb178f8eddcc6d14c140c84e5b374857
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-03-27 12:47:21 +0200

    [9] Refactor node_strategies.py with lists from domain_01.py

--- src/domains/domain01/node_strategies.py
+++ src/domains/domain01/node_strategies.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 
 from assign_strategies_to_nodes import assign_strategies_to_nodes
 from constants import PLAYER1
-from domains.domain01.domain_01 import node_to_IS_lvl0, IS_strategies_lvl0, node_to_IS_lvl1, \
-	IS_strategies_lvl1, node_to_IS_lvl2, IS_strategies_lvl2, IS_acting_players_lvl0, IS_acting_players_lvl1, \
-	IS_acting_players_lvl2
+#from domains.domain01.domain_01 import node_to_IS_lvl0, IS_strategies_lvl0, node_to_IS_lvl1, \
+#	IS_strategies_lvl1, node_to_IS_lvl2, IS_strategies_lvl2, IS_acting_players_lvl0, IS_acting_players_lvl1, \
+#	IS_acting_players_lvl2
+from domains.domain01.domain_01 import node_to_is, is_strategies, is_acting_players
 from utils.tensor_utils import print_tensors
 
 
@@ -12,40 +13,75 @@ from utils.tensor_utils import print_tensors
 
 
 def get_node_strategies():
-	node_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0, name="node_strategies_lvl0")
-	node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1, name="node_strategies_lvl1")
-	node_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2, name="node_strategies_lvl2")
-	return [node_strategies_lvl0, node_strategies_lvl1, node_strategies_lvl2]
+    node_strategies_lvl0 = assign_strategies_to_nodes(is_strategies[0], node_to_is[0], name="node_strategies_lvl0")
+    node_strategies_lvl1 = assign_strategies_to_nodes(is_strategies[1], node_to_is[1], name="node_strategies_lvl1")
+    node_strategies_lvl2 = assign_strategies_to_nodes(is_strategies[2], node_to_is[2], name="node_strategies_lvl2")
+    return [node_strategies_lvl0, node_strategies_lvl1, node_strategies_lvl2]
 
 
 def get_node_cf_strategies(updating_player=PLAYER1):
-	node_cf_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0,
-	                                                     updating_player=updating_player,
-	                                                     acting_players=IS_acting_players_lvl0,
-	                                                     name="node_cf_strategies_lvl0")
-	node_cf_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1,
-	                                                     updating_player=updating_player,
-	                                                     acting_players=IS_acting_players_lvl1,
-	                                                     name="node_cf_strategies_lvl1")
-	node_cf_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2,
-	                                                     updating_player=updating_player,
-	                                                     acting_players=IS_acting_players_lvl2,
-	                                                     name="node_cf_strategies_lvl2")
-	return [node_cf_strategies_lvl0, node_cf_strategies_lvl1, node_cf_strategies_lvl2]
+    # TODO generate node_cf_strategies_* with tf.where on node_strategies
+    node_cf_strategies_lvl0 = assign_strategies_to_nodes(
+        is_strategies[0],
+        node_to_is[0],
+        updating_player=updating_player,
+        acting_players=is_acting_players[0],
+        name="node_cf_strategies_lvl0"
+    )
+    node_cf_strategies_lvl1 = assign_strategies_to_nodes(
+        is_strategies[1],
+        node_to_is[1],
+        updating_player=updating_player,
+        acting_players=is_acting_players[1],
+        name="node_cf_strategies_lvl1"
+    )
+    node_cf_strategies_lvl2 = assign_strategies_to_nodes(
+        is_strategies[2],
+        node_to_is[2],
+        updating_player=updating_player,
+        acting_players=is_acting_players[2],
+        name="node_cf_strategies_lvl2"
+    )
+    return [node_cf_strategies_lvl0, node_cf_strategies_lvl1, node_cf_strategies_lvl2]
 
 
 if __name__ == '__main__':
-	updating_player = PLAYER1
-	node_strategies = get_node_strategies()
-	node_cf_strategies = get_node_cf_strategies(updating_player=updating_player)
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		print("########## Level 0 ##########")
-		print_tensors(sess, [node_to_IS_lvl0, IS_strategies_lvl0, node_strategies[0],
-		                     node_cf_strategies[0]])
-		print("########## Level 1 ##########")
-		print_tensors(sess, [node_to_IS_lvl1, IS_strategies_lvl1, node_strategies[1],
-		                     node_cf_strategies[1]])
-		print("########## Level 2 ##########")
-		print_tensors(sess, [node_to_IS_lvl2, IS_strategies_lvl2, node_strategies[2],
-		                     node_cf_strategies[2]])
+    updating_player = PLAYER1
+
+    node_strategies = get_node_strategies()
+
+    node_cf_strategies = get_node_cf_strategies(updating_player=updating_player)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print("########## Level 0 ##########")
+        print_tensors(
+            sess,
+            [
+                node_to_is[0],
+                is_strategies[0],
+                node_strategies[0],
+                node_cf_strategies[0]
+            ]
+        )
+        print("########## Level 1 ##########")
+        print_tensors(
+            sess,
+            [
+                node_to_is[1],
+                is_strategies[1],
+                node_strategies[1],
+                node_cf_strategies[1]
+            ]
+        )
+        print("########## Level 2 ##########")
+        print_tensors(
+            sess,
+            [
+                node_to_is[2],
+                is_strategies[2],
+                node_strategies[2],
+                node_cf_strategies[2]
+            ]
+        )
+

2d64ee68307a1db2510d636f3a7ed1c480f65afa
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-03-27 12:25:38 +0200

    [9] Refactor bottomup_expected_values.py with lists from domain_01.py

--- src/domains/domain01/bottomup_expected_values.py
+++ src/domains/domain01/bottomup_expected_values.py
@@ -4,47 +4,77 @@ import tensorflow as tf
 
 from assign_strategies_to_nodes import assign_strategies_to_nodes
 from constants import TERMINAL_NODE
-from domains.domain01.domain_01 import node_to_IS_lvl0, IS_strategies_lvl0, node_to_IS_lvl1, \
-	IS_strategies_lvl1, node_to_IS_lvl2, IS_strategies_lvl2, utilities_lvl3, utilities_lvl2, node_types_lvl2, \
-	utilities_lvl1, node_types_lvl1, node_types_lvl0, utilities_lvl0
+from domains.domain01.domain_01 import node_to_is, is_strategies, utilities, node_types
 from utils.tensor_utils import print_tensors
 
 # custom-made game: see doc/domain_01_via_drawing.png and doc/domain_01_via_gambit.png
 
-node_strategies_lvl0 = assign_strategies_to_nodes(IS_strategies_lvl0, node_to_IS_lvl0, name="node_strategies_lvl0")
-node_strategies_lvl1 = assign_strategies_to_nodes(IS_strategies_lvl1, node_to_IS_lvl1, name="node_strategies_lvl1")
-node_strategies_lvl2 = assign_strategies_to_nodes(IS_strategies_lvl2, node_to_IS_lvl2, name="node_strategies_lvl2")
-
+node_strategies = [None] * 3
+node_strategies_lvl0 = assign_strategies_to_nodes(is_strategies[0], node_to_is[0], name="node_strategies_lvl0")
+node_strategies[0] = node_strategies_lvl0
+node_strategies_lvl1 = assign_strategies_to_nodes(is_strategies[1], node_to_is[1], name="node_strategies_lvl1")
+node_strategies[1] = node_strategies_lvl1
+node_strategies_lvl2 = assign_strategies_to_nodes(is_strategies[2], node_to_is[2], name="node_strategies_lvl2")
+node_strategies[2] = node_strategies_lvl2
 
 def get_expected_values():
-	expected_values_lvl3 = tf.identity(utilities_lvl3, name="expected_values_lvl3")
-	weighted_sum_of_values_lvl2 = tf.reduce_sum(input_tensor=node_strategies_lvl2 * expected_values_lvl3, axis=-1,
-	                                            name="weighted_sum_of_values_lvl2")
-	expected_values_lvl2 = tf.where(condition=tf.equal(node_types_lvl2, TERMINAL_NODE), x=utilities_lvl2,
-	                                y=weighted_sum_of_values_lvl2, name="expected_values_lvl2")
-	weighted_sum_of_values_lvl1 = tf.reduce_sum(input_tensor=node_strategies_lvl1 * expected_values_lvl2, axis=-1,
-	                                            name="weighted_sum_of_values_lvl1")
-	expected_values_lvl1 = tf.where(condition=tf.equal(node_types_lvl1, TERMINAL_NODE), x=utilities_lvl1,
-	                                y=weighted_sum_of_values_lvl1, name="expected_values_lvl1")
-	weighted_sum_of_values_lvl0 = tf.reduce_sum(input_tensor=node_strategies_lvl0 * expected_values_lvl1, axis=-1,
-	                                            name="weighted_sum_of_values_lvl0")
-	expected_values_lvl0 = tf.where(condition=tf.equal(node_types_lvl0, TERMINAL_NODE), x=utilities_lvl0,
-	                                y=weighted_sum_of_values_lvl0, name="expected_values_lvl0")
-	return [expected_values_lvl0, expected_values_lvl1, expected_values_lvl2, expected_values_lvl3]
+    expected_values_lvl3 = tf.identity(utilities[3], name="expected_values_lvl3")
+
+    weighted_sum_of_values_lvl2 = tf.reduce_sum(
+        input_tensor=node_strategies[2] * expected_values_lvl3,
+        axis=-1,
+        name="weighted_sum_of_values_lvl2"
+    )
+
+    expected_values_lvl2 = tf.where(
+        condition=tf.equal(node_types[2], TERMINAL_NODE),
+        x=utilities[2],
+        y=weighted_sum_of_values_lvl2,
+        name="expected_values_lvl2"
+    )
+
+    weighted_sum_of_values_lvl1 = tf.reduce_sum(
+        input_tensor=node_strategies[1] * expected_values_lvl2,
+        axis=-1,
+        name="weighted_sum_of_values_lvl1"
+    )
+
+    expected_values_lvl1 = tf.where(
+        condition=tf.equal(node_types[1], TERMINAL_NODE),
+        x=utilities[1],
+        y=weighted_sum_of_values_lvl1,
+        name="expected_values_lvl1"
+    )
+
+    weighted_sum_of_values_lvl0 = tf.reduce_sum(
+        input_tensor=node_strategies[0] * expected_values_lvl1,
+        axis=-1,
+        name="weighted_sum_of_values_lvl0"
+    )
+
+    expected_values_lvl0 = tf.where(
+        condition=tf.equal(node_types[0], TERMINAL_NODE),
+        x=utilities[0],
+        y=weighted_sum_of_values_lvl0,
+        name="expected_values_lvl0"
+    )
+
+    return [expected_values_lvl0, expected_values_lvl1, expected_values_lvl2, expected_values_lvl3]
 
 
 if __name__ == '__main__':
-	expected_values = get_expected_values()
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		print("########## Level 3 ##########")
-		print_tensors(sess, [utilities_lvl3, expected_values[3]])
-		print("########## Level 2 ##########")
-		print_tensors(sess, [node_strategies_lvl2])
-		print_tensors(sess, [utilities_lvl2, expected_values[2]])
-		print("########## Level 1 ##########")
-		print_tensors(sess, [node_strategies_lvl1])
-		print_tensors(sess, [utilities_lvl1, expected_values[1]])
-		print("########## Level 0 ##########")
-		print_tensors(sess, [node_strategies_lvl0])
-		print_tensors(sess, [utilities_lvl0, expected_values[0]])
+    expected_values = get_expected_values()
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print("########## Level 3 ##########")
+        print_tensors(sess, [utilities[3], expected_values[3]])
+        print("########## Level 2 ##########")
+        print_tensors(sess, [node_strategies_lvl2])
+        print_tensors(sess, [utilities[2], expected_values[2]])
+        print("########## Level 1 ##########")
+        print_tensors(sess, [node_strategies_lvl1])
+        print_tensors(sess, [utilities[1], expected_values[1]])
+        print("########## Level 0 ##########")
+        print_tensors(sess, [node_strategies_lvl0])
+        print_tensors(sess, [utilities[0], expected_values[0]])

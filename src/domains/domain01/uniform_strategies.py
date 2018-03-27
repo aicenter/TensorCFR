bb193b2a286235d5cb5e1bda18771be2676694b1
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-03-27 11:18:52 +0200

    [9] Use new variables

--- src/domains/domain01/uniform_strategies.py
+++ src/domains/domain01/uniform_strategies.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from constants import IMAGINARY_NODE
-from domains.domain01.domain_01 import levels, get_node_types, get_node_to_IS, get_IS_strategies
+from domains.domain01.domain_01 import levels, node_types, node_to_is, is_strategies
 from utils.tensor_utils import print_tensors
 
 
@@ -9,41 +9,49 @@ from utils.tensor_utils import print_tensors
 
 
 def get_IS_children_types():  # TODO unittest
-	node_to_IS = get_node_to_IS()
-	node_types = get_node_types()
-	IS_strategies = get_IS_strategies()
-	IS_children_types = [None] * (levels - 1)
-	for level in range(levels - 1):
-		if level == 0:
-			IS_children_types[0] = tf.expand_dims(node_types[1], axis=0, name="IS_children_types_lvl0")
-		else:
-			IS_children_types[level] = tf.scatter_nd_update(ref=tf.Variable(tf.zeros_like(IS_strategies[level],
-			                                                                              dtype=node_types[level + 1].dtype)),
-			                                                indices=tf.expand_dims(node_to_IS[level], axis=-1),
-			                                                updates=node_types[level + 1],
-			                                                name="IS_children_types_lvl{}".format(level))
-	return IS_children_types
+    IS_children_types = [None] * (levels - 1)
+    for level in range(levels - 1):
+        if level == 0:
+            IS_children_types[0] = tf.expand_dims(node_types[1], axis=0, name="IS_children_types_lvl0")
+        else:
+            IS_children_types[level] = tf.scatter_nd_update(
+                ref=tf.Variable(
+                    tf.zeros_like(
+                        is_strategies[level],
+                        dtype=node_types[level + 1].dtype
+                    )
+                ),
+                indices=tf.expand_dims(node_to_is[level], axis=-1),
+                updates=node_types[level + 1],
+                name="IS_children_types_lvl{}".format(level)
+            )
+    return IS_children_types
 
 
 def get_IS_uniform_strategies():  # TODO unittest
-	IS_children_types = get_IS_children_types()
-	IS_uniform_strategies = [None] * (levels - 1)
-	for level in range(levels - 1):
-		IS_uniform_strategies[level] = tf.to_float(tf.not_equal(IS_children_types[level], IMAGINARY_NODE))
-		# Note: An all-0's row cannot be normalized. This is caused when IS has only imaginary children. As of now,
-		# `tf.divide` produces `nan` in the entire row.
-		IS_uniform_strategies[level] = tf.divide(IS_uniform_strategies[level],
-		                                         tf.reduce_sum(IS_uniform_strategies[level], axis=-1, keepdims=True),
-		                                         name="IS_uniform_strategies_lvl{}".format(level))
-	return IS_uniform_strategies
+    IS_children_types = get_IS_children_types()
+    IS_uniform_strategies = [None] * (levels - 1)
+
+    for level in range(levels - 1):
+        IS_uniform_strategies[level] = tf.to_float(tf.not_equal(IS_children_types[level], IMAGINARY_NODE))
+        # Note: An all-0's row cannot be normalized. This is caused when IS has only imaginary children. As of now,
+        #  `tf.divide` produces `nan` in the entire row.
+        IS_uniform_strategies[level] = tf.divide(
+            IS_uniform_strategies[level],
+            tf.reduce_sum(IS_uniform_strategies[level], axis=-1, keep_dims=True),
+            name="IS_uniform_strategies_lvl{}".format(level)
+        )
+
+    return IS_uniform_strategies
 
 
 if __name__ == '__main__':
-	node_types_ = get_node_types()
-	IS_uniform_strategies_ = get_IS_uniform_strategies()
-	IS_children_types_ = get_IS_children_types()
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		for i in range(levels - 1):
-			print("########## Level {} ##########".format(i))
-			print_tensors(sess, [node_types_[i], IS_children_types_[i], IS_uniform_strategies_[i]])
+    IS_uniform_strategies_ = get_IS_uniform_strategies()
+    IS_children_types_ = get_IS_children_types()
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for i in range(levels - 1):
+            print("########## Level {} ##########".format(i))
+            print_tensors(sess, [node_types[i], IS_children_types_[i], IS_uniform_strategies_[i]])

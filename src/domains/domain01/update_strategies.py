896c726761cb7acb0b4026e6f1edcbaea45e95a0
Karel Ha <mathemage@gmail.com>
2018-03-28 14:20:50 +0200

    Rewrite `update_strategy_of_acting_player` for its actual functionality
    Originally just copied from body function from other files
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/update_strategies.py
+++ src/domains/domain01/update_strategies.py
@@ -1,25 +1,25 @@
 import tensorflow as tf
 
-from domains.domain01.domain_01 import levels, positive_cumulative_regrets
-from domains.domain01.regrets import update_positive_cumulative_regrets
-from domains.domain01.uniform_strategies import get_IS_uniform_strategies
-from utils.tensor_utils import print_tensors
+from constants import PLAYER1
+from domains.domain01.domain_01 import levels, get_IS_strategies, get_IS_acting_players
+from domains.domain01.strategy_matched_to_regrets import get_strategy_matched_to_regrets
+from utils.tensor_utils import print_tensors, masked_assign
 
 
 # custom-made game: doc/domain_01.png (https://gitlab.com/beyond-deepstack/TensorCFR/blob/master/doc/domain_01.png)
 
-def update_strategy_of_acting_player():  # TODO unittest
-	IS_uniform_strategies = get_IS_uniform_strategies()
-	IS_strategies = [None] * (levels - 1)
-	for level in range(levels - 1):
-		sums_of_regrets = tf.reduce_sum(positive_cumulative_regrets[level], axis=-1, keepdims=True,
-		                                name="sums_of_regrets_lvl{}".format(level))
-		normalized_regrets = tf.divide(positive_cumulative_regrets[level], sums_of_regrets,
-		                               name="normalized_regrets_lvl{}".format(level))
-		zero_sums = tf.squeeze(tf.equal(sums_of_regrets, 0), name="zero_sums_lvl{}".format(level))
-		# Note: An all-0's row cannot be normalized. Thus, when PCRegrets sum to 0, a uniform strategy is used instead.
-		IS_strategies[level] = tf.where(condition=zero_sums, x=IS_uniform_strategies[level],
-		                                y=normalized_regrets, name="strategies_matched_to_regrets_lvl{}".format(level))
+def update_strategy_of_acting_player(acting_player):  # TODO unittest
+	IS_strategies_matched_to_regrets = get_strategy_matched_to_regrets()
+	IS_strategies = get_IS_strategies()
+	IS_acting_players = get_IS_acting_players()
+	acting_depth = len(IS_strategies)
+	update_IS_strategies_ops = [None] * acting_depth
+	for level in range(acting_depth):
+		# TODO fix and solve TF complaints (Run)
+		update_IS_strategies_ops[level] = masked_assign(ref=IS_strategies,
+		                                                mask=tf.equal(IS_acting_players[level], acting_player),
+		                                                value=IS_strategies_matched_to_regrets,
+		                                                name="update_IS_strategies_ops_lvl{}".format(level))
 	return IS_strategies
 
 
@@ -28,11 +28,16 @@ def cumulate_strategy_of_opponent():  # TODO unittest
 
 
 if __name__ == '__main__':
-	IS_strategies_matched_to_regrets_ = update_strategy_of_acting_player()
-	update_regrets = update_positive_cumulative_regrets()
+	IS_strategies_ = get_IS_strategies()
+	IS_strategies_matched_to_regrets_ = get_strategy_matched_to_regrets()
+	update_IS_strategies = update_strategy_of_acting_player(acting_player=PLAYER1)
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		for i in range(levels - 1):
 			print("########## Level {} ##########".format(i))
-			print_tensors(sess, [IS_strategies_matched_to_regrets_[i], update_regrets[i], IS_strategies_matched_to_regrets_[i]])
-
+			print_tensors(sess, [
+				IS_strategies_[i],
+				IS_strategies_matched_to_regrets_[i],
+				update_IS_strategies[i],
+				IS_strategies_[i]
+			])

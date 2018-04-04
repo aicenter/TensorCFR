69aaf88763e20439cb0406a15b60850d99bc99bc
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-04-04 15:01:18 +0200

    [11] Add tests for bottomup expected values

---
+++ tests/domains/domain01/test_bottomup_expected_values.py
@@ -0,0 +1,72 @@
+import numpy as np
+import tensorflow as tf
+
+from src.constants import small_error_tolerance, large_error_tolerance
+from src.domains.domain01.bottomup_expected_values import get_expected_values
+
+
+class TestTopDownReachProbabilities(tf.test.TestCase):
+	def setUp(self):
+		self.expected_values = get_expected_values()
+
+	def test_level_0(self):
+		expected_output = np.array(83.9625)
+
+		with self.test_session() as sess:
+			sess.run(tf.global_variables_initializer())
+
+			output = sess.run(self.expected_values[0])
+
+			self.assertNDArrayNear(output, expected_output, large_error_tolerance)
+
+	def test_level_1(self):
+		expected_output = np.array([25.45, 95.5, 154.2, 214.2, 210.45])
+
+		with self.test_session() as sess:
+			sess.run(tf.global_variables_initializer())
+
+			output = sess.run(self.expected_values[1])
+
+			self.assertNDArrayNear(output, expected_output, large_error_tolerance)
+
+	def test_level_2(self):
+		expected_output = np.array([[18.5, 33, 30],
+									[77.5, 97.5, 0],
+									[135, 159, 0],
+									[195, 219, 0],
+									[130, 275.5, 296]])
+
+		with self.test_session() as sess:
+			sess.run(tf.global_variables_initializer())
+
+			output = sess.run(self.expected_values[2])
+
+			self.assertNDArrayNear(output, expected_output, small_error_tolerance)
+
+	def test_level_3(self):
+		expected_output = np.array([[[10, 20],
+									 [30, 40],
+									 [0, 0]],
+
+									[[70, 80],
+									 [90, 100],
+									 [0, 0]],
+
+									[[130, 140],
+									 [150, 160],
+									 [0, 0]],
+
+									[[190, 200],
+									 [210, 220],
+									 [0, 0]],
+
+									[[0, 0],
+									 [270, 280],
+									 [290, 300]]])
+
+		with self.test_session() as sess:
+			sess.run(tf.global_variables_initializer())
+
+			output = sess.run(self.expected_values[3])
+
+			self.assertNDArrayNear(output, expected_output, small_error_tolerance)

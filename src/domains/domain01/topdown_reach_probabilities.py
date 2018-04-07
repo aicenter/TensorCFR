4e1d65997dca02dd5734a363fc888842d7b7159d
Karel Ha <mathemage@gmail.com>
2018-04-07 16:20:04 +0200

    Rename `node_cf_strategies` w/ right usage of underscores in `topdown`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/topdown_reach_probabilities.py
+++ src/domains/domain01/topdown_reach_probabilities.py
@@ -10,20 +10,20 @@ from src.utils.tensor_utils import print_tensors, expanded_multiply
 
 
 def get_reach_probabilities():
-    _node_cf_strategies = get_node_cf_strategies()
+    node_cf_strategies = get_node_cf_strategies()
     reach_probabilities_lvl1 = expanded_multiply(
         expandable_tensor=reach_probabilities[0],
-        expanded_tensor=_node_cf_strategies[0],
+        expanded_tensor=node_cf_strategies[0],
         name="reach_probabilities_lvl1"
     )
     reach_probabilities_lvl2 = expanded_multiply(
         expandable_tensor=reach_probabilities_lvl1,
-        expanded_tensor=_node_cf_strategies[1],
+        expanded_tensor=node_cf_strategies[1],
         name="reach_probabilities_lvl2"
     )
     reach_probabilities_lvl3 = expanded_multiply(
         expandable_tensor=reach_probabilities_lvl2,
-        expanded_tensor=_node_cf_strategies[2],
+        expanded_tensor=node_cf_strategies[2],
         name="reach_probabilities_lvl3"
     )
     return [reach_probabilities[0], reach_probabilities_lvl1, reach_probabilities_lvl2, reach_probabilities_lvl3]
@@ -31,15 +31,15 @@ def get_reach_probabilities():
 
 if __name__ == '__main__':
     updating_player = PLAYER1
-    node_cf_strategies = get_node_cf_strategies(updating_player=updating_player)
+    node_cf_strategies_ = get_node_cf_strategies(updating_player=updating_player)
     reach_probabilities = get_reach_probabilities()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         print("########## Level 0 ##########")
-        print_tensors(sess, [reach_probabilities[0], node_to_infoset[0], infoset_strategies[0], node_cf_strategies[0]])
+        print_tensors(sess, [reach_probabilities[0], node_to_infoset[0], infoset_strategies[0], node_cf_strategies_[0]])
         print("########## Level 1 ##########")
-        print_tensors(sess, [reach_probabilities[1], node_to_infoset[1], infoset_strategies[1], node_cf_strategies[1]])
+        print_tensors(sess, [reach_probabilities[1], node_to_infoset[1], infoset_strategies[1], node_cf_strategies_[1]])
         print("########## Level 2 ##########")
-        print_tensors(sess, [reach_probabilities[2], node_to_infoset[2], infoset_strategies[2], node_cf_strategies[2]])
+        print_tensors(sess, [reach_probabilities[2], node_to_infoset[2], infoset_strategies[2], node_cf_strategies_[2]])
         print("########## Level 3 ##########")
         print_tensors(sess, [reach_probabilities[3]])

29bb074056bb0cf6f0cca218389948520e0b82ff
Karel Ha <mathemage@gmail.com>
2018-04-08 00:14:50 +0200

    Replace `*IS*` with `*infoset*`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/update_strategies.py
+++ src/domains/domain01/update_strategies.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from src.constants import PLAYER1
-from src.domains.domain01.domain_01 import levels, get_IS_strategies, get_IS_acting_players
+from src.domains.domain01.domain01 import levels, get_infoset_strategies, get_infoset_acting_players
 from src.domains.domain01.strategy_matched_to_regrets import get_strategy_matched_to_regrets
 from src.utils.tensor_utils import print_tensors, masked_assign
 
@@ -9,19 +9,20 @@ from src.utils.tensor_utils import print_tensors, masked_assign
 # custom-made game: doc/domain_01.png (https://gitlab.com/beyond-deepstack/TensorCFR/blob/master/doc/domain_01.png)
 
 def update_strategy_of_acting_player(acting_player):  # TODO unittest
-	IS_strategies_matched_to_regrets = get_strategy_matched_to_regrets()
-	IS_strategies = get_IS_strategies()
-	IS_acting_players = get_IS_acting_players()
-	acting_depth = len(IS_strategies)
-	update_IS_strategies_ops = [None] * acting_depth
+	infoset_strategies_matched_to_regrets = get_strategy_matched_to_regrets()
+	infoset_strategies = get_infoset_strategies()
+	infoset_acting_players = get_infoset_acting_players()
+	acting_depth = len(infoset_strategies)
+	update_infoset_strategies_ops = [None] * acting_depth
 	for level in range(acting_depth):
-		infosets_belonging_to_acting_player = tf.reshape(tf.equal(IS_acting_players[level], acting_player),
-		                                                 shape=[IS_strategies[level].shape[0]],
+		infosets_belonging_to_acting_player = tf.reshape(tf.equal(infoset_acting_players[level], acting_player),
+		                                                 shape=[infoset_strategies[level].shape[0]],
 		                                                 name="infosets_of_acting_player_lvl{}".format(level))
-		update_IS_strategies_ops[level] = masked_assign(ref=IS_strategies[level], mask=infosets_belonging_to_acting_player,
-		                                                value=IS_strategies_matched_to_regrets[level],
-		                                                name="update_IS_strategies_ops_lvl{}".format(level))
-	return update_IS_strategies_ops
+		update_infoset_strategies_ops[level] = masked_assign(ref=infoset_strategies[level],
+		                                                     mask=infosets_belonging_to_acting_player,
+		                                                     value=infoset_strategies_matched_to_regrets[level],
+		                                                     name="update_infoset_strategies_ops_lvl{}".format(level))
+	return update_infoset_strategies_ops
 
 
 def cumulate_strategy_of_opponent():  # TODO unittest
@@ -29,18 +30,18 @@ def cumulate_strategy_of_opponent():  # TODO unittest
 
 
 if __name__ == '__main__':
-	IS_strategies_ = get_IS_strategies()
-	IS_acting_players_ = get_IS_acting_players()
-	IS_strategies_matched_to_regrets_ = get_strategy_matched_to_regrets()
-	update_IS_strategies = update_strategy_of_acting_player(acting_player=PLAYER1)
+	infoset_strategies_ = get_infoset_strategies()
+	infoset_acting_players_ = get_infoset_acting_players()
+	infoset_strategies_matched_to_regrets_ = get_strategy_matched_to_regrets()
+	update_infoset_strategies = update_strategy_of_acting_player(acting_player=PLAYER1)
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		for i in range(levels - 1):
 			print("########## Level {} ##########".format(i))
 			print_tensors(sess, [
-				IS_strategies_[i],
-				IS_acting_players_[i],
-				IS_strategies_matched_to_regrets_[i],
-				update_IS_strategies[i],
-				IS_strategies_[i]
+				infoset_strategies_[i],
+				infoset_acting_players_[i],
+				infoset_strategies_matched_to_regrets_[i],
+				update_infoset_strategies[i],
+				infoset_strategies_[i]
 			])

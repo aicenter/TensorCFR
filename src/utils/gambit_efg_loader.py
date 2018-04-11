48e018d662d0ed0c99c415f587ebe3fbff9c1f6e
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-04-11 23:29:39 +0200

    [#35] Refactor code for parsing chance node

--- src/utils/gambit_efg_loader.py
+++ src/utils/gambit_efg_loader.py
@@ -28,16 +28,22 @@ class GambitEFGLoader:
 		else:
 			return False
 
+	def parse_actions(self, input_actions_str):
+		parse_actions = re.findall(r'"(?P<name>[^"]*)" (?P<probability>[\d\.]+)', input_actions_str)
+		return [{'name': action[0], 'probability': float(action[1])} for action in parse_actions]
+
+	def parse_payoffs(self, input_payoffs_str):
+		parse_payoffs = re.findall(r'\d+', input_payoffs_str)
+		return [int(payoff) for payoff in parse_payoffs]
+
 	def parse_chance_node(self, input_line):
 		parse_line = re.search(
 			r'^(?P<type>c) "(?P<name>[^"]*)" (?P<information_set_number>\d+) "(?P<information_set_name>[^"]*)" \{ (?P<actions_str>.*) \} (?P<outcome>\d+) \"\" \{ (?P<payoffs_str>.*) \}',
 			input_line
 		)
-		parse_actions = re.findall(r'"(?P<name>[^"]*)" (?P<probability>[\d\.]+)', parse_line.group('actions_str'))
-		parse_payoffs = re.findall(r'\d+', parse_line.group('payoffs_str'))
 
-		actions = [{'name': action[0], 'probability': float(action[1])} for action in parse_actions]
-		payoffs = [int(payoff) for payoff in parse_payoffs]
+		actions = self.parse_actions(parse_line.group('actions_str'))
+		payoffs = self.parse_payoffs(parse_line.group('payoffs_str'))
 
 		return {
 			'type': parse_line.group('type'),
@@ -51,10 +57,14 @@ class GambitEFGLoader:
 
 
 if __name__ == '__main__':
-	input_line = 'c "" 1 "" { "Ea (0.05)" 0.05 "Da (0.1)" 0.1 "Ca (0.1)" 0.1 "Ba (0.25)" 0.25 "Aa (0.5)" 0.5 } 1 "" { 0, 0 }'
+	input_line_chance = 'c "" 1 "" { "Ea (0.05)" 0.05 "Da (0.1)" 0.1 "Ca (0.1)" 0.1 "Ba (0.25)" 0.25 "Aa (0.5)" 0.5 } 1 "" { 0, 0 }'
+	input_line_player = 'p "" 2 4 "" { "Ja (0.9)" "Ia (0.1)" } 24 "" { 0, 0 }'
+	input_line_terminal = 't "" 38 "" { 10, -10 }'
 
-	gambit_efg_loader = GambitEFGLoader('pokus')
-	print(gambit_efg_loader.parse_node(input_line))
+	gambit_efg_loader = GambitEFGLoader('dummy')
+	print(gambit_efg_loader.parse_node(input_line_chance))
+	#print(gambit_efg_loader.parse_node(input_line_player))
+	#print(gambit_efg_loader.parse_node(input_line_terminal))
 
 
 """

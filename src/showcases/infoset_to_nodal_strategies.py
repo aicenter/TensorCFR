eec45a3a967a4e59061fbb09a59c49b15f2f3f73
Karel Ha <mathemage@gmail.com>
2018-04-12 16:38:39 +0200

    Use list of tensors and for-loops in `infoset_to_nodal_strategies.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/infoset_to_nodal_strategies.py
+++ src/showcases/infoset_to_nodal_strategies.py
@@ -2,23 +2,25 @@
 
 import tensorflow as tf
 
-from src.domains.domain01.domain01 import infoset_strategies, node_to_infoset
+from src.domains.domain01.domain01 import infoset_strategies, node_to_infoset, acting_depth
 from src.domains.domain01.node_strategies import assign_strategies_to_nodes
 from src.utils.tensor_utils import print_tensors
 
 # custom-made game: see doc/domain01_via_drawing.png and doc/domain01_via_gambit.png
 # TODO extract to a test
 
-node_strategies_lvl0 = assign_strategies_to_nodes(infoset_strategies[0], node_to_infoset[0], name="node_strategies_lvl0")
-node_strategies_lvl1 = assign_strategies_to_nodes(infoset_strategies[1], node_to_infoset[1], name="node_strategies_lvl1")
-node_strategies_lvl2 = assign_strategies_to_nodes(infoset_strategies[2], node_to_infoset[2], name="node_strategies_lvl2")
+# node_strategies = [None] * acting_depth
+node_strategies = [
+	assign_strategies_to_nodes(
+			infoset_strategies[level],
+			node_to_infoset[level],
+			name="node_strategies_lvl{}".format(level)
+	) for level in range(acting_depth)
+]
 
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    # more verbose output
-    print("########## Level 0 ##########")
-    print_tensors(sess, [node_to_infoset[0], infoset_strategies[0], node_strategies_lvl0])
-    print("########## Level 1 ##########")
-    print_tensors(sess, [node_to_infoset[1], infoset_strategies[1], node_strategies_lvl1])
-    print("########## Level 2 ##########")
-    print_tensors(sess, [node_to_infoset[2], infoset_strategies[2], node_strategies_lvl2])
+	sess.run(tf.global_variables_initializer())
+	# more verbose output
+	for level in range(acting_depth):
+		print("########## Level {} ##########".format(level))
+		print_tensors(sess, [node_to_infoset[level], infoset_strategies[level], node_strategies[level]])

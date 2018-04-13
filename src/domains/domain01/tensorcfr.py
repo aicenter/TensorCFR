6efa016f1c23df6d0f175a66731bf085b14c4b37
Karel Ha <mathemage@gmail.com>
2018-04-13 11:13:08 +0200

    Initialize to uniform strategies in `tensorcfr.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/tensorcfr.py
+++ src/domains/domain01/tensorcfr.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 
 from src.domains.domain01.cfr_step import do_cfr_step
-from src.domains.domain01.domain01 import cfr_step, infoset_strategies
+from src.domains.domain01.domain01 import cfr_step, infoset_strategies, acting_depth
+from src.domains.domain01.uniform_strategies import get_infoset_uniform_strategies
 from src.utils.tensor_utils import print_tensors
 
 # custom-made game: see doc/domain01_via_drawing.png and doc/domain01_via_gambit.png
@@ -9,10 +10,14 @@ from src.utils.tensor_utils import print_tensors
 
 total_steps = 1000
 cfr_step_op = do_cfr_step()
+uniform_strategies = get_infoset_uniform_strategies()
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
+	print("Initializing strategies to uniform ones...\n")
+	sess.run([tf.assign(ref=infoset_strategies[level], value=uniform_strategies[level]) for level in range(acting_depth)])
+	print_tensors(sess, infoset_strategies)
 	print("Running {} CFR+ iterations...\n".format(total_steps))
 	for _ in range(total_steps):
 		print("########## CFR+ step #{} ##########".format(cfr_step.eval()))
-		print_tensors(sess, infoset_strategies)
 		sess.run(cfr_step_op)
+		print_tensors(sess, infoset_strategies)

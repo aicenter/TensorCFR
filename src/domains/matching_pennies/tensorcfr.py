6dedca66826dce433ee98e1107f9bad559af28cd
Karel Ha <mathemage@gmail.com>
2018-04-14 23:40:06 +0200

    Remove initializing `tensorcfr` to uniform strategies
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/matching_pennies/tensorcfr.py
+++ src/domains/matching_pennies/tensorcfr.py
@@ -3,9 +3,8 @@ import tensorflow as tf
 from src.constants import DEFAULT_TOTAL_STEPS, DEFAULT_TOTAL_STEPS_ON_SMALL_DOMAINS
 from src.domains.matching_pennies.cfr_step import do_cfr_step
 from src.domains.matching_pennies.domain_definitions import cfr_step, current_infoset_strategies, \
-	cumulative_infoset_strategies, positive_cumulative_regrets
+	cumulative_infoset_strategies
 from src.domains.matching_pennies.strategy_matched_to_regrets import get_strategy_matched_to_regrets
-from src.domains.matching_pennies.uniform_strategies import assign_uniform_strategies_to_players
 from src.domains.matching_pennies.update_strategies import get_average_infoset_strategies
 from src.utils.tensor_utils import print_tensors
 
@@ -14,25 +13,24 @@ from src.utils.tensor_utils import print_tensors
 
 def run_cfr(total_steps=DEFAULT_TOTAL_STEPS):
 	cfr_step_op = do_cfr_step()
-	ops_assign_uniform_strategies = assign_uniform_strategies_to_players()
 	strategies_matched_to_regrets = get_strategy_matched_to_regrets()
 	average_infoset_strategies = get_average_infoset_strategies()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		print("Initializing strategies to uniform ones...\n")
-		sess.run(ops_assign_uniform_strategies)
+		print("TensorCFR\n")
+		print("Using user-defined strategies...\n")
 		print_tensors(sess, current_infoset_strategies)
 		print("Running {} CFR+ iterations...\n".format(total_steps))
 		for _ in range(total_steps):
 			print("########## CFR+ step #{} ##########".format(cfr_step.eval()))
 			sess.run(cfr_step_op)
-			print_tensors(sess, positive_cumulative_regrets)
-			print("___________________________________\n")
-			print_tensors(sess, strategies_matched_to_regrets)
-			print("___________________________________\n")
+			# print_tensors(sess, positive_cumulative_regrets)
+			# print("___________________________________\n")
+			# print_tensors(sess, strategies_matched_to_regrets)
+			# print("___________________________________\n")
 			print_tensors(sess, current_infoset_strategies)
-			print("___________________________________\n")
-			print_tensors(sess, cumulative_infoset_strategies)
+		print("###################################\n")
+		print_tensors(sess, cumulative_infoset_strategies)
 		print("___________________________________\n")
 		print_tensors(sess, average_infoset_strategies)
 

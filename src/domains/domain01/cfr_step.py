f98afc8284a8254e2e7a07adff2d79db4c6607a9
Karel Ha <mathemage@gmail.com>
2018-04-14 18:26:15 +0200

    Print `positive_cumulative_regrets[]` in `cfr_step.__main__`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/cfr_step.py
+++ src/domains/domain01/cfr_step.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 
 from src.constants import DEFAULT_TOTAL_STEPS_ON_SMALL_DOMAINS
 from src.domains.domain01.domain01 import get_infoset_acting_players, cfr_step, \
-	current_updating_player, current_opponent, cumulative_infoset_strategies, current_infoset_strategies
+	current_updating_player, current_opponent, cumulative_infoset_strategies, current_infoset_strategies, \
+	positive_cumulative_regrets
 from src.domains.domain01.swap_players import swap_players
 from src.domains.domain01.update_strategies import process_strategies
 from src.utils.tensor_utils import print_tensors
@@ -37,6 +38,10 @@ if __name__ == '__main__':
 		for _ in range(total_steps):
 			print("########## Start of CFR+ step {} ##########".format(cfr_step.eval()))
 			print_tensors(sess, [current_updating_player, current_opponent])
+			print("___________________________________\n")
+			print_tensors(sess, positive_cumulative_regrets)
+			print("___________________________________\n")
 			print_tensors(sess, current_infoset_strategies)
+			print("___________________________________\n")
 			print_tensors(sess, cumulative_infoset_strategies)
 			sess.run(cfr_step_op)

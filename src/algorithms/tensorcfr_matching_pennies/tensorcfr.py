ad7622eb2b15200795291213eff443a1400ac8d1
Karel Ha <mathemage@gmail.com>
2018-04-15 16:36:23 +0200

    Extract `matching_pennies.tensorcfr.get_strategy_initializer()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_matching_pennies/tensorcfr.py
+++ src/algorithms/tensorcfr_matching_pennies/tensorcfr.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+from src.algorithms.tensorcfr_matching_pennies.uniform_strategies import assign_uniform_strategies_to_players
 from src.commons.constants import DEFAULT_TOTAL_STEPS
 from src.algorithms.tensorcfr_matching_pennies.cfr_step import do_cfr_step
 from src.domains.matching_pennies.domain_definitions import cfr_step, current_infoset_strategies, \
@@ -11,15 +12,22 @@ from src.utils.tensor_utils import print_tensors
 
 # game of matching pennies: see doc/matching_pennies_efg_illustration.jpg
 
+def get_strategy_initializer():
+	return "Initializing strategies to uniform ones...\n", assign_uniform_strategies_to_players()
+
+
 def run_cfr(total_steps=DEFAULT_TOTAL_STEPS):
+	strategy_initializer_message, strategy_initializer = get_strategy_initializer()
 	cfr_step_op = do_cfr_step()
 	strategies_matched_to_regrets = get_strategy_matched_to_regrets()
 	average_infoset_strategies = get_average_infoset_strategies()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		print("TensorCFR\n")
-		print("Using user-defined strategies...\n")
-		print_tensors(sess, current_infoset_strategies)
+		# print("Using user-defined strategies...\n")
+		# print_tensors(sess, current_infoset_strategies)
+		print(strategy_initializer_message)
+		sess.run(strategy_initializer)
 		print("Running {} CFR+ iterations...\n".format(total_steps))
 		for _ in range(total_steps):
 			print("########## CFR+ step #{} ##########".format(cfr_step.eval()))

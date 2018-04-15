63f8f6395de630dff1dd299f34579aa7d6dd7484
Karel Ha <mathemage@gmail.com>
2018-04-15 11:43:19 +0200

    Move `regrets` to `algorithms.tensorcfr_matching_pennies`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/matching_pennies/cfr_step.py
+++ src/domains/matching_pennies/cfr_step.py
@@ -5,7 +5,7 @@ from src.commons.constants import DEFAULT_TOTAL_STEPS_ON_SMALL_DOMAINS, PLAYER1,
 from src.domains.matching_pennies.domain_definitions import get_infoset_acting_players, cfr_step, \
 	current_updating_player, current_opponent, cumulative_infoset_strategies, current_infoset_strategies, \
 	positive_cumulative_regrets
-from src.domains.matching_pennies.regrets import update_positive_cumulative_regrets
+from src.algorithms.tensorcfr_matching_pennies.regrets import update_positive_cumulative_regrets
 from src.domains.matching_pennies.update_strategies import update_strategy_of_acting_player, \
 	cumulate_strategy_of_opponent
 from src.utils.tensor_utils import print_tensors

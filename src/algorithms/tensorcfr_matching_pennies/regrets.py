50814a854177c2690b464c3ca42e87c402472a60
Karel Ha <mathemage@gmail.com>
2018-04-17 10:36:06 +0200

    Inline `maximum_addition` in `matching_pennies.regrets.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_matching_pennies/regrets.py
+++ src/algorithms/tensorcfr_matching_pennies/regrets.py
@@ -27,10 +27,6 @@ def update_positive_cumulative_regrets(regrets=get_regrets()):  # TODO verify an
 	update_regrets_ops = [None] * (levels - 1)
 	for level in range(levels - 1):
 		# to keep cumulative regret still positive:
-		maximum_addition = tf.maximum(
-			- positive_cumulative_regrets[level],
-			regrets[level]
-		)
 		# TODO optimize by: pre-define `infosets_of_player1` and `infosets_of_player2` (in domain definitions) and switch
 		infosets_of_updating_player = tf.reshape(
 				tf.equal(infoset_acting_players[level], current_updating_player),
@@ -41,7 +37,10 @@ def update_positive_cumulative_regrets(regrets=get_regrets()):  # TODO verify an
 		update_regrets_ops[level] = masked_assign(
 			ref=positive_cumulative_regrets[level],
 			mask=infosets_of_updating_player,
-			value=positive_cumulative_regrets[level] + maximum_addition,
+			value=tf.maximum(
+					0.0,
+					positive_cumulative_regrets[level] + regrets[level]
+			),
 			name="update_regrets_lvl{}".format(level)
 		)
 	return update_regrets_ops

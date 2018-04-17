c3807a5b2e6eafce95587d39e0eb473c305bd22a
Karel Ha <mathemage@gmail.com>
2018-04-17 10:57:04 +0200

    Reformat `matching_pennies.uniform_strategies.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_matching_pennies/uniform_strategies.py
+++ src/algorithms/tensorcfr_matching_pennies/uniform_strategies.py
@@ -12,7 +12,11 @@ def get_infoset_children_types():  # TODO unittest
 	infoset_children_types = [None] * (levels - 1)
 	for level in range(levels - 1):
 		if level == 0:
-			infoset_children_types[0] = tf.expand_dims(node_types[1], axis=0, name="infoset_children_types_lvl0")
+			infoset_children_types[0] = tf.expand_dims(
+					node_types[1],
+					axis=0,
+					name="infoset_children_types_lvl0"
+			)
 		else:
 			infoset_children_types[level] = tf.scatter_nd_update(
 					ref=tf.Variable(
@@ -23,7 +27,8 @@ def get_infoset_children_types():  # TODO unittest
 					),
 					indices=tf.expand_dims(node_to_infoset[level], axis=-1),
 					updates=node_types[level + 1],
-					name="infoset_children_types_lvl{}".format(level))
+					name="infoset_children_types_lvl{}".format(level)
+			)
 	return infoset_children_types
 
 
@@ -37,7 +42,8 @@ def get_infoset_uniform_strategies():  # TODO unittest
 		infoset_uniform_strategies[level] = tf.divide(
 				infoset_uniform_strategies[level],
 				tf.reduce_sum(infoset_uniform_strategies[level], axis=-1, keepdims=True),
-				name="infoset_uniform_strategies_lvl{}".format(level))
+				name="infoset_uniform_strategies_lvl{}".format(level)
+		)
 	return infoset_uniform_strategies
 
 

af8b64df22a61fb089c4157b96422f9903db89da
Karel Ha <mathemage@gmail.com>
2018-04-19 12:52:08 +0200

    Set name scopes `infoset_children_types`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_domain01/uniform_strategies.py
+++ src/algorithms/tensorcfr_domain01/uniform_strategies.py
@@ -9,27 +9,28 @@ from src.utils.tensor_utils import print_tensors, masked_assign
 # custom-made game: see doc/domain01_via_drawing.png and doc/domain01_via_gambit.png
 
 def get_infoset_children_types():  # TODO unittest
-	infoset_children_types = [None] * (levels - 1)
-	for level in range(levels - 1):
-		if level == 0:
-			infoset_children_types[0] = tf.expand_dims(
-					node_types[1],
-					axis=0,
-					name="infoset_children_types_lvl0"
-			)
-		else:
-			infoset_children_types[level] = tf.scatter_nd_update(
-					ref=tf.Variable(
-							tf.zeros_like(
-									current_infoset_strategies[level],
-									dtype=node_types[level + 1].dtype
-							)
-					),
-					indices=tf.expand_dims(node_to_infoset[level], axis=-1),
-					updates=node_types[level + 1],
-					name="infoset_children_types_lvl{}".format(level)
-			)
-	return infoset_children_types
+	with tf.name_scope("infoset_children_types"):
+		infoset_children_types = [None] * (levels - 1)
+		for level in range(levels - 1):
+			if level == 0:
+				infoset_children_types[0] = tf.expand_dims(
+						node_types[1],
+						axis=0,
+						name="infoset_children_types_lvl0"
+				)
+			else:
+				infoset_children_types[level] = tf.scatter_nd_update(
+						ref=tf.Variable(
+								tf.zeros_like(
+										current_infoset_strategies[level],
+										dtype=node_types[level + 1].dtype
+								)
+						),
+						indices=tf.expand_dims(node_to_infoset[level], axis=-1),
+						updates=node_types[level + 1],
+						name="infoset_children_types_lvl{}".format(level)
+				)
+		return infoset_children_types
 
 
 def get_infoset_uniform_strategies():  # TODO unittest

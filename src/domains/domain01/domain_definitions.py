e27a3f4c028996b66179b264129baf3cc3fba051
Karel Ha <mathemage@gmail.com>
2018-04-23 01:59:55 +0200

    Replace `tf.Variable` with `tf.get_variable()`
    - in `domain_definitons.py`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/domain_definitions.py
+++ src/domains/domain01/domain_definitions.py
@@ -25,12 +25,12 @@ with tf.variable_scope("domain_definitions"):
 	########## Level 0 ##########
 	# I0,0 = { s } ... root node, the chance player acts here
 	# there are 5 actions in node s
-	node_to_infoset[0] = tf.Variable(0, name="node_to_infoset_lvl0")
-	reach_probability_of_root_node = tf.Variable(1.0, name="reach_probability_of_root_node")
+	node_to_infoset[0] = tf.get_variable("node_to_infoset_lvl0", initializer=0)
+	reach_probability_of_root_node = tf.get_variable("reach_probability_of_root_node", initializer=1.0)
 	shape[0] = actions_per_levels[:0]
-	node_types[0] = tf.Variable(INNER_NODE, name="node_types_lvl0")
+	node_types[0] = tf.get_variable("node_types_lvl0", initializer=INNER_NODE)
 	utilities[0] = tf.fill(value=NON_TERMINAL_UTILITY, dims=shape[0], name="utilities_lvl0")
-	infoset_acting_players[0] = tf.Variable([CHANCE_PLAYER], name="infoset_acting_players_lvl0")
+	infoset_acting_players[0] = tf.get_variable("infoset_acting_players_lvl0", initializer=[CHANCE_PLAYER])
 	initial_infoset_strategies[0] = tf.placeholder_with_default(
 			input=[[0.5, .25, 0.1, 0.1, .05]],  # of I0,0
 			shape=[infoset_acting_players[0].shape[0], actions_per_levels[0]],
@@ -43,16 +43,16 @@ with tf.variable_scope("domain_definitions"):
 	# I1,2 = { s2, s3 }
 	# I1,3 = { s4 } ... chance node
 	# each node has 3 actions
-	node_to_infoset[1] = tf.Variable([0, 1, 2, 2, 3], name="node_to_infoset_lvl1")
+	node_to_infoset[1] = tf.get_variable("node_to_infoset_lvl1", initializer=[0, 1, 2, 2, 3])
 	shape[1] = actions_per_levels[:1]
-	node_types[1] = tf.Variable([INNER_NODE] * 5, name="node_types_lvl1")
+	node_types[1] = tf.get_variable("node_types_lvl1", initializer=[INNER_NODE] * 5)
 	utilities[1] = tf.fill(value=NON_TERMINAL_UTILITY, dims=shape[1], name="utilities_lvl1")
-	infoset_acting_players[1] = tf.Variable(
-		[PLAYER1,         # I1,0
-		 PLAYER2,         # I1,1
-		 PLAYER2,         # I1,2
-		 CHANCE_PLAYER],  # I1,3
-		name="infoset_acting_players_lvl1"
+	infoset_acting_players[1] = tf.get_variable(
+			"infoset_acting_players_lvl1",
+			initializer=[PLAYER1,         # I1,0
+			             PLAYER2,         # I1,1
+			             PLAYER2,         # I1,2
+			             CHANCE_PLAYER],  # I1,3
 	)
 	initial_infoset_strategies[1] = tf.placeholder_with_default(
 			input=[[0.5, 0.4, 0.1],   # of I1,0
@@ -74,61 +74,55 @@ with tf.variable_scope("domain_definitions"):
 	# I2,7 = { s7, s17 } ... terminal nodes
 	# I2,8 = { s10, s13, s16 } ... imaginary nodes
 	# each node has 2 actions
-	node_to_infoset[2] = tf.Variable(
-		[[0, 1, 7],   # s5, s6, s7
-		 [2, 2, 8],   # s8, s9, s10
-		 [3, 4, 8],   # s11, s12, s1
-		 [3, 4, 8],   # s14, s15, s16
-		 [7, 5, 6]],  # s17, s18, s19
-		name="node_to_infoset_lvl2"
+	node_to_infoset[2] = tf.get_variable(
+			"node_to_infoset_lvl2",
+			initializer=[[0, 1, 7],   # s5, s6, s7
+			             [2, 2, 8],   # s8, s9, s10
+			             [3, 4, 8],   # s11, s12, s1
+			             [3, 4, 8],   # s14, s15, s16
+			             [7, 5, 6]],  # s17, s18, s19
 	)
 	shape[2] = actions_per_levels[:2]
-	node_types[2] = tf.Variable(
-		[[INNER_NODE, INNER_NODE, TERMINAL_NODE],   # s5, s6, s7
-		 [INNER_NODE, INNER_NODE, IMAGINARY_NODE],  # s8, s9, s10
-		 [INNER_NODE, INNER_NODE, IMAGINARY_NODE],  # s11, s12, s13
-		 [INNER_NODE, INNER_NODE, IMAGINARY_NODE],  # s14, s15, s16
-		 [TERMINAL_NODE, INNER_NODE, INNER_NODE]],  # s17, s18, s19
-		name="node_types_lvl2"
+	node_types[2] = tf.get_variable(
+			"node_types_lvl2",
+			initializer=[[INNER_NODE, INNER_NODE, TERMINAL_NODE],   # s5, s6, s7
+			             [INNER_NODE, INNER_NODE, IMAGINARY_NODE],  # s8, s9, s10
+			             [INNER_NODE, INNER_NODE, IMAGINARY_NODE],  # s11, s12, s13
+			             [INNER_NODE, INNER_NODE, IMAGINARY_NODE],  # s14, s15, s16
+			             [TERMINAL_NODE, INNER_NODE, INNER_NODE]],  # s17, s18, s19
 	)
-	utilities[2] = masked_assign(
-			ref=tf.Variable(
-					tf.fill(
-							value=NON_TERMINAL_UTILITY,
-							dims=shape[2]
-					)
+	utilities[2] = tf.get_variable(
+			"utilities_lvl2",
+			initializer=tf.where(
+					condition=tf.equal(node_types[2], TERMINAL_NODE),
+					x=tf.reshape(tf.range(10, 160, delta=10.0), shape[2]),
+					y=tf.fill(value=NON_TERMINAL_UTILITY, dims=shape[2]),
 			),
-			value=tf.reshape(
-					tf.range(10, 160, delta=10.0),
-					shape[2]
-			),
-			mask=tf.equal(node_types[2], TERMINAL_NODE),
-			name="utilities_lvl2"
 	)
-	infoset_acting_players[2] = tf.Variable(
-		[PLAYER1,            # of I2,0
-		 PLAYER2,            # of I2,1
-		 PLAYER1,            # of I2,2
-		 PLAYER2,            # of I2,3
-		 CHANCE_PLAYER,      # of I2,4
-		 PLAYER1,            # of I2,5
-		 PLAYER2,            # of I2,6
-		 NO_ACTING_PLAYER,   # of I2,7 ... pseudo-infoset of terminal nodes
-		 NO_ACTING_PLAYER],  # of I2,8 ... pseudo-infoset of imaginary nodes
-		name="infoset_acting_players_lvl2"
+	infoset_acting_players[2] = tf.get_variable(
+			"infoset_acting_players_lvl2",
+			initializer=[PLAYER1,            # of I2,0
+			             PLAYER2,            # of I2,1
+			             PLAYER1,            # of I2,2
+			             PLAYER2,            # of I2,3
+			             CHANCE_PLAYER,      # of I2,4
+			             PLAYER1,            # of I2,5
+			             PLAYER2,            # of I2,6
+			             NO_ACTING_PLAYER,   # of I2,7 ... pseudo-infoset of terminal nodes
+			             NO_ACTING_PLAYER],  # of I2,8 ... pseudo-infoset of imaginary nodes
 	)
 	initial_infoset_strategies[2] = tf.placeholder_with_default(
-		input=[[0.15, 0.85],   # of I2,0
-					 [0.70, 0.30],   # of I2,1
-					 [0.25, 0.75],   # of I2,2
-					 [0.50, 0.50],   # of I2,3
-					 [0.10, 0.90],   # of I2,4
-					 [0.45, 0.55],   # of I2,5
-					 [0.40, 0.60],   # of I2,6
-					 [0.00, 0.00],   # of I2,7 ... terminal nodes <- mock-up zero strategy
-					 [0.00, 0.00]],  # of I2,8 ... imaginary nodes <- mock-up zero strategy
-		shape=[infoset_acting_players[2].shape[0], actions_per_levels[2]],
-		name="initial_infoset_strategies_lvl{}".format(2)
+			input=[[0.15, 0.85],   # of I2,0
+			       [0.70, 0.30],   # of I2,1
+			       [0.25, 0.75],   # of I2,2
+			       [0.50, 0.50],   # of I2,3
+			       [0.10, 0.90],   # of I2,4
+			       [0.45, 0.55],   # of I2,5
+			       [0.40, 0.60],   # of I2,6
+			       [0.00, 0.00],   # of I2,7 ... terminal nodes <- mock-up zero strategy
+			       [0.00, 0.00]],  # of I2,8 ... imaginary nodes <- mock-up zero strategy
+			shape=[infoset_acting_players[2].shape[0], actions_per_levels[2]],
+			name="initial_infoset_strategies_lvl{}".format(2)
 	)
 
 	########## Level 3 ##########
@@ -142,55 +136,56 @@ with tf.variable_scope("domain_definitions"):
 		 [4, 0]],  # children of s17
 		name="indices_of_imaginary_nodes_lvl3"
 	)
-	node_types[3] = tf.scatter_nd_update(
-		ref=tf.Variable(
-					tf.fill(
-						value=TERMINAL_NODE,
-						dims=shape[3]
-					),
-		),
-		indices=indices_of_imaginary_nodes_lvl3,
-		updates=tf.fill(
-			value=IMAGINARY_NODE,
-			dims=indices_of_imaginary_nodes_lvl3.shape
-		),
-		name="node_types_lvl3"
+	node_types[3] = tf.get_variable(
+			"node_types_lvl3",
+			initializer=
+			[[[1, 1],
+			  [1, 1],
+			  [2, 2]],
+
+			 [[1, 1],
+			  [1, 1],
+			  [2, 2]],
+
+			 [[1, 1],
+			  [1, 1],
+			  [2, 2]],
+
+			 [[1, 1],
+			  [1, 1],
+			  [2, 2]],
+
+			 [[2, 2],
+			  [1, 1],
+			  [1, 1]]]
 	)
-	utilities[3] = masked_assign(
-		ref=tf.Variable(
-					tf.fill(
-						value=NON_TERMINAL_UTILITY,
-						dims=shape[3]
-					)
-		),
-		value=tf.reshape(tf.range(10, 310, delta=10.0), shape[3]),
-		mask=tf.equal(node_types[3], TERMINAL_NODE),
-		name="utilities_lvl3"
+	utilities[3] = tf.get_variable(
+			"utilities_lvl3",
+			initializer=tf.where(
+					condition=tf.equal(node_types[3], TERMINAL_NODE),
+					x=tf.reshape(tf.range(10, 310, delta=10.0), shape[3]),
+					y=tf.fill(value=NON_TERMINAL_UTILITY, dims=shape[3]),
+			),
 	)
 
 	########## miscellaneous tensors ##########
 	current_infoset_strategies = [
-		tf.Variable(
-				initial_value=initial_infoset_strategies[level],
-				name="current_infoset_strategies_lvl{}".format(level)
+		tf.get_variable(
+				"current_infoset_strategies_lvl{}".format(level),
+				initializer=initial_infoset_strategies[level],
 		) for level in range(acting_depth)
 	]
 	positive_cumulative_regrets = [
-		tf.Variable(
-				tf.zeros_like(
-						current_infoset_strategies[level]
-				),
-				name="positive_cumulative_regrets_lvl{}".format(level)
+		tf.get_variable(
+				"positive_cumulative_regrets_lvl{}".format(level),
+				initializer=tf.zeros_like(current_infoset_strategies[level]),
 		) for level in range(acting_depth)
 	]
 	cumulative_infoset_strategies = [
-		tf.Variable(
-				tf.zeros_like(
-						current_infoset_strategies[level]
-				),
-				name="cumulative_infoset_strategies_lvl{}".format(level)
-		)
-		for level in range(acting_depth)
+		tf.get_variable(
+				"cumulative_infoset_strategies_lvl{}".format(level),
+				initializer=tf.zeros_like(current_infoset_strategies[level]),
+		) for level in range(acting_depth)
 	]  # used for the final average strategy
 	infosets_of_non_chance_player = [
 		tf.reshape(
@@ -199,18 +194,24 @@ with tf.variable_scope("domain_definitions"):
 				name="infosets_of_acting_player_lvl{}".format(level)
 		) for level in range(acting_depth)
 	]
-	cfr_step = tf.Variable(initial_value=1, dtype=tf.int32, name="cfr_step")  # counter of CFR+ iterations
-	averaging_delay = tf.Variable(  # https://arxiv.org/pdf/1407.5042.pdf (Figure 2)
-			DEFAULT_AVERAGING_DELAY,
+	cfr_step = tf.get_variable("cfr_step", initializer=1, dtype=tf.int32)  # counter of CFR+ iterations
+	averaging_delay = tf.get_variable(  # https://arxiv.org/pdf/1407.5042.pdf (Figure 2)
+			"averaging_delay",
+			initializer=DEFAULT_AVERAGING_DELAY,
 			dtype=tf.int32,
-			name="averaging_delay"
 	)
 	player_owning_the_utilities = tf.constant(  # utilities defined...
 			PLAYER1,  # ...from this player's point of view
 			name="player_owning_the_utilities"
 	)
-	current_updating_player = tf.Variable(initial_value=PLAYER1, name="current_updating_player")
-	current_opponent = tf.Variable(initial_value=PLAYER2, name="current_opponent")
+	current_updating_player = tf.get_variable(
+			"current_updating_player",
+			initializer=PLAYER1,
+	)
+	current_opponent = tf.get_variable(
+			"current_opponent",
+			initializer=PLAYER2,
+	)
 	signum_of_current_player = tf.where(
 			condition=tf.equal(current_updating_player, player_owning_the_utilities),
 			x=1.0,

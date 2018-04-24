4fce3923770a60522e477dc8c3c461071400069e
Karel Ha <mathemage@gmail.com>
2018-04-24 22:24:36 +0200

    Extract `non_imaginary_children_lvl*`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_domain01/uniform_strategies.py
+++ src/algorithms/tensorcfr_domain01/uniform_strategies.py
@@ -48,17 +48,18 @@ def get_infoset_uniform_strategies():  # TODO unittest
 		infoset_uniform_strategies = [None] * (levels - 1)
 		for level in range(levels - 1):
 			with tf.control_dependencies(ops_set_infoset_children_types):
-				infoset_uniform_strategies[level] = tf.to_float(
+				non_imaginary_children = tf.to_float(
 						tf.not_equal(
 								infoset_children_types[level],
 								IMAGINARY_NODE
-						)
+						),
+						name="non_imaginary_children_lvl{}".format(level)
 				)
 			# Note: An all-0's row cannot be normalized. This is caused when IS has only imaginary children. As of now,
 			#  `tf.divide` produces `nan` in the entire row.
 			infoset_uniform_strategies[level] = tf.divide(
-					infoset_uniform_strategies[level],
-					tf.reduce_sum(infoset_uniform_strategies[level], axis=-1, keepdims=True),
+					non_imaginary_children,
+					tf.reduce_sum(non_imaginary_children, axis=-1, keepdims=True),
 			)
 		return [tf.where(
 				condition=infosets_of_non_chance_player[level],

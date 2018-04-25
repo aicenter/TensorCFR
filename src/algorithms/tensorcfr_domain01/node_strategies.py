b9c479a62929bb625d31f5d67b3fd2356c079fc0
Karel Ha <mathemage@gmail.com>
2018-04-26 01:00:36 +0200

    Compute `node_cf_strategies_` with update & read
    - Rename to `node_cf_strategies_`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_domain01/node_strategies.py
+++ src/algorithms/tensorcfr_domain01/node_strategies.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from src.domains.domain01.domain_definitions import node_to_infoset, current_infoset_strategies, \
-	infoset_acting_players, acting_depth, current_updating_player
+	infoset_acting_players, acting_depth, current_updating_player, shape
 from src.utils.tensor_utils import print_tensors
 
 
@@ -60,17 +60,39 @@ def get_node_strategies():
 
 
 def get_node_cf_strategies(updating_player=current_updating_player):
-	with tf.variable_scope("node_cf_strategies"):
+	"""
+	Here `cf` stands for counterfactual. Simply speaking, on each root->leaf path, the strategies of the updating player
+	are set to 1.
+	"""
+	with tf.variable_scope("node_cf_strategies", reuse=tf.AUTO_REUSE) as node_cf_strategies_scope:
+		node_cf_strategies = [
+			tf.get_variable(
+					name="node_cf_strategies_lvl{}".format(level),
+					shape=shape[level + 1],
+					dtype=current_infoset_strategies[level].dtype,
+			) for level in range(acting_depth)
+		]
+	# Note: no `reuse=tf.AUTO_REUSE` because these operations exists in 2 variants: 1 per each player
+	with tf.variable_scope("set_cf_strategies_to_nodes", reuse=tf.AUTO_REUSE):
 		# TODO generate node_cf_strategies_* with tf.where on node_strategies
-		return [
+		assigned_strategies = [
 			assign_strategies_to_nodes(
 					current_infoset_strategies[level],
 					node_to_infoset[level],
 					updating_player=updating_player,
 					acting_players=infoset_acting_players[level],
-					name="node_cf_strategies_lvl{}".format(level)
+					name="assign_cf_strategies_to_nodes_lvl{}".format(level),
 			) for level in range(acting_depth)
 		]
+		set_node_cf_strategies = [
+			tf.assign(
+					ref=node_cf_strategies[level],
+					value=assigned_strategies[level],
+			) for level in range(acting_depth)
+		]
+	with tf.variable_scope(node_cf_strategies_scope, reuse=True):
+		with tf.control_dependencies(set_node_cf_strategies):
+			return [node_cf_strategies[level].read_value() for level in range(acting_depth)]
 
 
 def show_strategies(session):
@@ -81,14 +103,14 @@ def show_strategies(session):
 			current_infoset_strategies[level_],
 			node_strategies[level_],
 			infoset_acting_players[level_],
-			node_cf_strategies[level_],
+			node_cf_strategies_[level_],
 		])
 
 
 if __name__ == '__main__':
 	from src.algorithms.tensorcfr_domain01.swap_players import swap_players
 	node_strategies = get_node_strategies()
-	node_cf_strategies = get_node_cf_strategies()
+	node_cf_strategies_ = get_node_cf_strategies()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		# TODO extract following lines to a UnitTest

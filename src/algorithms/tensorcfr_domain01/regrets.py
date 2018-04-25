ff80cac3cb9f0479e53185e7f8b2765f01510626
Karel Ha <mathemage@gmail.com>
2018-04-26 01:57:20 +0200

    Set variable scope `update_cumulative_regrets_lvl*`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_domain01/regrets.py
+++ src/algorithms/tensorcfr_domain01/regrets.py
@@ -28,22 +28,23 @@ def update_positive_cumulative_regrets(regrets=get_regrets()):  # TODO verify an
 	with tf.variable_scope("update_cumulative_regrets"):
 		update_regrets_ops = [None] * acting_depth
 		for level in range(acting_depth):
-			# TODO optimize by: pre-define `infosets_of_player1` and `infosets_of_player2` (in domain definitions) and switch
-			infosets_of_updating_player = tf.reshape(
-					tf.equal(infoset_acting_players[level], current_updating_player),
-					shape=[positive_cumulative_regrets[level].shape[0]],
-					name="infosets_of_updating_player_lvl{}".format(level),
-			)
-			# TODO implement and use `masked_assign_add` here
-			update_regrets_ops[level] = masked_assign(
-				ref=positive_cumulative_regrets[level],
-				mask=infosets_of_updating_player,
-				value=tf.maximum(
-						0.0,
-						positive_cumulative_regrets[level] + regrets[level]
-				),
-				name="update_regrets_lvl{}".format(level)
-			)
+			with tf.variable_scope("update_cumulative_regrets_lvl{}".format(level)):
+				# TODO optimize by: pre-define `infosets_of_player1` and `infosets_of_player2` (in domain definitions) and switch
+				infosets_of_updating_player = tf.reshape(
+						tf.equal(infoset_acting_players[level], current_updating_player),
+						shape=[positive_cumulative_regrets[level].shape[0]],
+						name="infosets_of_updating_player_lvl{}".format(level),
+				)
+				# TODO implement and use `masked_assign_add` here
+				update_regrets_ops[level] = masked_assign(
+					ref=positive_cumulative_regrets[level],
+					mask=infosets_of_updating_player,
+					value=tf.maximum(
+							0.0,
+							positive_cumulative_regrets[level] + regrets[level]
+					),
+					name="update_regrets_lvl{}".format(level)
+				)
 		return update_regrets_ops
 
 

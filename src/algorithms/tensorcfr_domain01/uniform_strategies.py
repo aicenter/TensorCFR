cfa9203092723f8581da8f9250d3eca33d1e7a52
Karel Ha <mathemage@gmail.com>
2018-04-26 05:05:49 +0200

    Add variable scopes describing levels in `uniform_strategies.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_domain01/uniform_strategies.py
+++ src/algorithms/tensorcfr_domain01/uniform_strategies.py
@@ -12,24 +12,25 @@ def get_infoset_children_types():  # TODO unittest
 	with tf.variable_scope("infoset_children_types"):
 		infoset_children_types = [None] * (levels - 1)
 		for level in range(levels - 1):
-			if level == 0:
-				infoset_children_types[0] = tf.expand_dims(
-						node_types[1],
-						axis=0,
-						name="infoset_children_types_lvl0"
-				)
-			else:
-				infoset_children_types[level] = tf.scatter_nd_update(
-						ref=tf.Variable(
-								tf.zeros_like(
-										current_infoset_strategies[level],
-										dtype=node_types[level + 1].dtype
-								)
-						),
-						indices=tf.expand_dims(node_to_infoset[level], axis=-1),
-						updates=node_types[level + 1],
-						name="infoset_children_types_lvl{}".format(level)
-				)
+			with tf.variable_scope("level{}".format(level)):
+				if level == 0:
+					infoset_children_types[0] = tf.expand_dims(
+							node_types[1],
+							axis=0,
+							name="infoset_children_types_lvl0"
+					)
+				else:
+					infoset_children_types[level] = tf.scatter_nd_update(
+							ref=tf.Variable(
+									tf.zeros_like(
+											current_infoset_strategies[level],
+											dtype=node_types[level + 1].dtype
+									)
+							),
+							indices=tf.expand_dims(node_to_infoset[level], axis=-1),
+							updates=node_types[level + 1],
+							name="infoset_children_types_lvl{}".format(level)
+					)
 		return infoset_children_types
 
 
@@ -37,20 +38,22 @@ def get_infoset_uniform_strategies():  # TODO unittest
 	with tf.variable_scope("infoset_uniform_strategies"):
 		infoset_children_types = get_infoset_children_types()
 		infoset_uniform_strategies = [None] * (levels - 1)
-		for level in range(levels - 1):
-			infoset_uniform_strategies[level] = tf.to_float(tf.not_equal(infoset_children_types[level], IMAGINARY_NODE))
-			# Note: An all-0's row cannot be normalized. This is caused when IS has only imaginary children. As of now,
-			#  `tf.divide` produces `nan` in the entire row.
-			infoset_uniform_strategies[level] = tf.divide(
-					infoset_uniform_strategies[level],
-					tf.reduce_sum(infoset_uniform_strategies[level], axis=-1, keepdims=True),
-			)
-		return [tf.where(
-				condition=infosets_of_non_chance_player[level],
-				x=infoset_uniform_strategies[level],
-				y=current_infoset_strategies[level],
-				name="infoset_uniform_strategies_lvl{}".format(level),
-		) for level in range(acting_depth)]
+		for level in range(acting_depth):
+			with tf.variable_scope("level{}".format(level)):
+				infoset_uniform_strategies[level] = tf.to_float(tf.not_equal(infoset_children_types[level], IMAGINARY_NODE))
+				# Note: An all-0's row cannot be normalized. This is caused when IS has only imaginary children. As of now,
+				#  `tf.divide` produces `nan` in the entire row.
+				infoset_uniform_strategies[level] = tf.divide(
+						infoset_uniform_strategies[level],
+						tf.reduce_sum(infoset_uniform_strategies[level], axis=-1, keepdims=True),
+				)
+				infoset_uniform_strategies[level] = tf.where(
+						condition=infosets_of_non_chance_player[level],
+						x=infoset_uniform_strategies[level],
+						y=current_infoset_strategies[level],
+						name="infoset_uniform_strategies_lvl{}".format(level),
+				)
+	return infoset_uniform_strategies
 
 
 if __name__ == '__main__':

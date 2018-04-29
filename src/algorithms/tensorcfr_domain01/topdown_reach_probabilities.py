b4dee5cfd38070bd321f4a71d863608b8c943b00
Karel Ha <mathemage@gmail.com>
2018-04-29 19:05:25 +0200

    Add docstring to `get_nodal_reach_probabilities()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_domain01/topdown_reach_probabilities.py
+++ src/algorithms/tensorcfr_domain01/topdown_reach_probabilities.py
@@ -2,13 +2,19 @@ import tensorflow as tf
 
 from src.algorithms.tensorcfr_domain01.node_strategies import get_node_cf_strategies
 from src.domains.domain01.domain_definitions import reach_probability_of_root_node, node_to_infoset, \
-	current_infoset_strategies, levels, infoset_acting_players, current_opponent
+	current_infoset_strategies, levels, infoset_acting_players, current_opponent, current_updating_player
 from src.utils.tensor_utils import print_tensors, expanded_multiply, scatter_nd_sum
 
 
 # custom-made game: see doc/domain01_via_drawing.png and doc/domain01_via_gambit.png
 
-def get_nodal_reach_probabilities():
+def get_nodal_reach_probabilities(for_player=current_updating_player):
+	"""
+	:param for_player: The player for which the reach probabilities are computed. These probabilities are usually computed
+	 for the updating player when counterfactual values are computed. Therefore, `for_player` is set to
+	  `current_updating_player` by default.
+	:return: The reach probabilities of nodes based on `current_infoset_strategies`.
+	"""
 	node_cf_strategies = get_node_cf_strategies()
 	with tf.variable_scope("nodal_reach_probabilities"):
 		nodal_reach_probabilities = [None] * levels
@@ -28,7 +34,7 @@ def get_infoset_reach_probabilities(for_player=current_opponent):
 	 for the opponent when his strategies are cumulated. Therefore, `for_player` is set to `current_opponent` by default.
 	:return: The reach probabilities of information sets based on `current_infoset_strategies`.
 	"""
-	nodal_reach_probabilities = get_nodal_reach_probabilities()
+	nodal_reach_probabilities = get_nodal_reach_probabilities(for_player)
 	with tf.variable_scope("infoset_reach_probabilities"):
 		infoset_reach_probabilities = [None] * levels
 		with tf.variable_scope("level0"):

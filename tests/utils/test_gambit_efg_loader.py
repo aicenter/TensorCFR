477b3c5e94bb948742eb01e8c054732a707d94c9
Karel Ha <mathemage@gmail.com>
2018-04-29 15:42:07 +0200

    Remove `test_parse_chance_node()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/utils/test_gambit_efg_loader.py
+++ tests/utils/test_gambit_efg_loader.py
@@ -10,30 +10,6 @@ class TestGambbitEFGLoaderParse(unittest.TestCase):
 	def setUp(self):
 		pass
 
-	def test_parse_chance_node(self):
-		input_str = 'c "" 1 "" { "Ea (0.05)" 0.05 "Da (0.1)" 0.1 "Ca (0.1)" 0.1 "Ba (0.25)" 0.25 "Aa (0.5)" 0.5 } 1 "" { 0, 0 }'
-
-		gambit_loader = GambitEFGLoader("dummy")
-
-		expected_output = {
-			'payoffs': [0, 0],
-			'outcome_name': '',
-			'outcome': '1',
-			'type': 'c',
-			'name': '',
-			'information_set_number': '1',
-			'information_set_name': '',
-			'actions': [
-				{'probability': 0.05, 'name': 'Ea (0.05)'},
-				{'probability': 0.1, 'name': 'Da (0.1)'},
-				{'probability': 0.1, 'name': 'Ca (0.1)'},
-				{'probability': 0.25, 'name': 'Ba (0.25)'},
-				{'probability': 0.5, 'name': 'Aa (0.5)'}
-			]
-		}
-
-		self.assertEqual(gambit_loader.parse_chance_node(input_str), expected_output)
-
 
 class TestGambitEFGLoaderDomain01(unittest.TestCase):
 	def setUp(self):

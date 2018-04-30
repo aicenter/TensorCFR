12d6a7c0667c6aa7af26a5224b30d6bad000cfef
Karel Ha <mathemage@gmail.com>
2018-04-30 23:00:07 +0200

    Copy `domain_as_numpy_values.py` from `domain_definitions.py`
    - in `domain01`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/__init__.py
+++ src/domains/domain01/__init__.py
@@ -2,4 +2,5 @@
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
 __all__ = ["domain01", "topdown_reach_probabilities", "bottomup_expected_values", "counterfactual_values",
-           "node_strategies", "regrets", "strategy_matched_to_regrets", "uniform_strategies"]
+           "node_strategies", "regrets", "strategy_matched_to_regrets", "uniform_strategies", "domain_definitions",
+           "domain_as_numpy_values"]

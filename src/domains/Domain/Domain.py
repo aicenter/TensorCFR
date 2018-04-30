9b48d94b3458e8ff62e4dbc37bd0d56244dcf7ee
Karel Ha <mathemage@gmail.com>
2018-05-01 01:17:21 +0200

    Define & print `matching_pennies` in `Domain.__main__`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/Domain/Domain.py
+++ src/domains/Domain/Domain.py
@@ -154,6 +154,17 @@ if __name__ == '__main__':
 			infoset_acting_players=d1.infoset_acting_players,
 			initial_infoset_strategies=d1.initial_infoset_strategies,
 	)
+	import src.domains.matching_pennies.matching_pennies_as_numpy_values as mp
+	matching_pennies = Domain(
+			domain_name="matching_pennies",
+			actions_per_levels=mp.actions_per_levels,
+			node_to_infoset=mp.node_to_infoset,
+			node_types=mp.node_types,
+			utilities=mp.utilities,
+			infoset_acting_players=mp.infoset_acting_players,
+			initial_infoset_strategies=mp.initial_infoset_strategies,
+	)
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		domain01.print_domain(session=sess)
+		domain01.print_domain(sess)
+		matching_pennies.print_domain(sess)

dd03a5cbfca64c5284986d4aeea5211cf733c73d
Karel Ha <mathemage@gmail.com>
2018-05-01 00:16:18 +0200

    Rename `domain_as_numpy_values.py` to `domain01_as_numpy_values.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/__init__.py
+++ src/domains/domain01/__init__.py
@@ -3,4 +3,4 @@
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
 __all__ = ["domain01", "topdown_reach_probabilities", "bottomup_expected_values", "counterfactual_values",
            "node_strategies", "regrets", "strategy_matched_to_regrets", "uniform_strategies", "domain_definitions",
-           "domain_as_numpy_values"]
+           "domain01_as_numpy_values"]

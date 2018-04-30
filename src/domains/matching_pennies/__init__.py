b0cbdc0ecaeb5c593cebc57cf7a608101749bda2
Karel Ha <mathemage@gmail.com>
2018-05-01 00:55:33 +0200

    Copy `matching_pennies_as_numpy_values.py` from `domain_definitions.py`
    - in `matching_pennies`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/matching_pennies/__init__.py
+++ src/domains/matching_pennies/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
-__all__ = ["domain_definitions"]
+__all__ = ["domain_definitions", "matching_pennies_as_numpy_values"]

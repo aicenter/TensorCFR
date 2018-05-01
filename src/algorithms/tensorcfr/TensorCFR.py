9d567ce11f9641c0053e696f1f399ce00c5c4a03
Karel Ha <mathemage@gmail.com>
2018-05-01 20:00:24 +0200

    Move functions of `topdown_reach_probabilities.py` to class `TensorCFR`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/TensorCFR.py
+++ src/algorithms/tensorcfr/TensorCFR.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from src.commons.constants import PLAYER1, PLAYER2, TERMINAL_NODE
 from src.domains.Domain import Domain
 from src.utils.distribute_strategies_to_nodes import distribute_strategies_to_nodes
-from src.utils.tensor_utils import print_tensors
+from src.utils.tensor_utils import print_tensors, expanded_multiply, scatter_nd_sum
 
 
 class TensorCFR:
@@ -119,6 +119,73 @@ class TensorCFR:
 				),
 				expected_values[level]
 			])
+
+	def get_nodal_reach_probabilities(self, for_player=None):
+		"""
+		:param for_player: The player for which the reach probabilities are computed. These probabilities are usually computed
+		 for the updating player when counterfactual values are computed. Therefore, `for_player` is set to
+			`current_updating_player` by default.
+		:return: The reach probabilities of nodes based on `current_infoset_strategies`.
+		"""
+		if for_player is None:
+			for_player = self.domain.current_updating_player
+		# TODO continue here
+		node_cf_strategies = self.get_node_cf_strategies(updating_player=for_player)
+		with tf.variable_scope("nodal_reach_probabilities"):
+			nodal_reach_probabilities = [None] * self.domain.levels
+			nodal_reach_probabilities[0] = self.domain.reach_probability_of_root_node
+			for level in range(1, self.domain.levels):
+				nodal_reach_probabilities[level] = expanded_multiply(
+						expandable_tensor=nodal_reach_probabilities[level - 1],
+						expanded_tensor=node_cf_strategies[level - 1],
+						name="nodal_reach_probabilities_lvl{}".format(level)
+				)
+			return nodal_reach_probabilities
+
+	def get_infoset_reach_probabilities(self, for_player=None):
+		"""
+		 :param for_player: The player for which the reach probabilities are computed. These probabilities are usually computed
+			for the opponent when his strategies are cumulated. Therefore, `for_player` is set to `current_opponent` by default.
+		 :return: The reach probabilities of information sets based on `current_infoset_strategies`.
+		 """
+		if for_player is None:
+			for_player = self.domain.current_opponent
+		nodal_reach_probabilities = self.get_nodal_reach_probabilities(for_player)
+		with tf.variable_scope("infoset_reach_probabilities"):
+			infoset_reach_probabilities = [None] * self.domain.levels
+			with tf.variable_scope("level0"):
+				infoset_reach_probabilities[0] = tf.identity(nodal_reach_probabilities[0],
+				                                             name="infoset_reach_probabilities_lvl0")
+			for level in range(1, self.domain.levels - 1):
+				with tf.variable_scope("level{}".format(level)):
+					scatter_nd_sum_indices = tf.expand_dims(
+							self.domain.node_to_infoset[level],
+							axis=-1,
+							name="expanded_node_to_infoset_lvl{}".format(level))
+					scatter_nd_sum_updates = nodal_reach_probabilities[level]
+					scatter_nd_sum_shape = self.domain.infoset_acting_players[level].shape
+					infoset_reach_probabilities[level] = scatter_nd_sum(
+							indices=scatter_nd_sum_indices,
+							updates=scatter_nd_sum_updates,
+							shape=scatter_nd_sum_shape,
+							name="infoset_reach_probabilities_lvl{}".format(level)
+					)
+		return infoset_reach_probabilities
+
+	def show_reach_probabilities(self, session):
+		node_cf_strategies = self.get_node_cf_strategies()
+		nodal_reach_probabilities = self.get_nodal_reach_probabilities()
+		infoset_reach_probabilities = self.get_infoset_reach_probabilities()
+		for level in range(self.domain.levels):
+			print("########## Level {} ##########".format(level))
+			print_tensors(session, [nodal_reach_probabilities[level]])
+			if level < self.domain.levels - 1:
+				print_tensors(session, [
+					self.domain.node_to_infoset[level],
+					infoset_reach_probabilities[level],
+					self.domain.current_infoset_strategies[level],
+					node_cf_strategies[level],
+				])
 
 
 if __name__ == '__main__':

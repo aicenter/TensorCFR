cec5bc5f4104c92b63ed224c50da9e46266c003a
Karel Ha <mathemage@gmail.com>
2018-05-01 17:05:38 +0200

    Showcase also `matching_pennies` in `swap_players.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/swap_players.py
+++ src/algorithms/tensorcfr/swap_players.py
@@ -3,17 +3,17 @@ import tensorflow as tf
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR
 from src.commons.constants import PLAYER1, PLAYER2
 from src.domains.domain01.Domain01 import domain01
+from src.domains.matching_pennies.MatchingPennies import matching_pennies
 
 if __name__ == '__main__':
-	tensorcfr_domain01 = TensorCFR(domain01)
-
-	domain01_cfr_step = tensorcfr_domain01.domain.cfr_step
-	increment_cfr_step_op = tf.assign_add(ref=domain01_cfr_step, value=1, name="increment_cfr_step_op")
-
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		print("PLAYER1 is {}, PLAYER2 is {}".format(PLAYER1, PLAYER2))
-		for _ in range(5):
-			print("########## CFR step {} ##########\n".format(domain01_cfr_step.eval()))
-			tensorcfr_domain01.domain.print_misc_variables(session=sess)
-			sess.run([tensorcfr_domain01.swap_players(), increment_cfr_step_op])
+	for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:
+		cfr_step = tensorcfr.domain.cfr_step
+		increment_cfr_step_op = tf.assign_add(ref=cfr_step, value=1, name="increment_cfr_step_op")
+		with tf.Session() as sess:
+			sess.run(tf.global_variables_initializer())
+			print(">>>>>>>>>> {} <<<<<<<<<<".format(tensorcfr.domain.domain_name))
+			print("PLAYER1 is {}, PLAYER2 is {}".format(PLAYER1, PLAYER2))
+			for _ in range(5):
+				print("########## CFR step {} ##########\n".format(cfr_step.eval()))
+				tensorcfr.domain.print_misc_variables(session=sess)
+				sess.run([tensorcfr.swap_players(), increment_cfr_step_op])

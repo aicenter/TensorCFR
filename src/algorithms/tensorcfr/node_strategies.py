b82e3a229fd8b65e9b6a4400032b653f45bcd362
Karel Ha <mathemage@gmail.com>
2018-05-01 17:12:15 +0200

    Move assign_strategies_to_nodes to `util.distribute_strategies_to_nodes`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/node_strategies.py
+++ src/algorithms/tensorcfr/node_strategies.py
@@ -2,45 +2,13 @@ import tensorflow as tf
 
 from src.domains.domain01.domain_definitions import node_to_infoset, current_infoset_strategies, \
 	infoset_acting_players, acting_depth, current_updating_player
+from src.utils.distribute_strategies_to_nodes import assign_strategies_to_nodes
 from src.utils.tensor_utils import print_tensors
 
 
 # custom-made game: see doc/domain01_via_drawing.png and doc/domain01_via_gambit.png
 
 # noinspection PyShadowingNames
-def assign_strategies_to_nodes(infoset_strategies, node_to_infoset, name, updating_player=None, acting_players=None):
-	"""
-  Translate 2-D tensor `infoset_strategies` of strategies per information sets to strategies per game states.
-  The translation is done based on N-D tensor `states_to_infosets`: each state (indexed by N-D coordinate)
-  stores the index of its information set.
-
-  If both `updating_player` and `acting_players` are `None` (default), no masking is used for strategies. Otherwise,
-  the `updating_player` acts with probabilities 1 everywhere (for the reach probability in the formula of
-  counterfactual values).
-
-  The corresponding TensorFlow operation (in the computation graph) outputs (N+1)-D tensor, which gives
-  for every states (indexed by N-D coordinate) the corresponding strategy of its information set. The strategy
-  can be read out in the final (N+1)th dimension.
-
-  Args:
-    :param infoset_strategies: A 2-D tensor of floats.
-    :param node_to_infoset: An N-D tensor of ints.
-    :param name: A string to name the resulting tensor operation.
-    :param updating_player: The index of the updating player to create for counterfactual probabilities.
-    :param acting_players: A tensor of the same shape as `node_to_infoset`, representing acting players per infosets.
-
-  Returns:
-    A corresponding TensorFlow operation (from the computation graph).
-  """
-	if (updating_player is not None) and (acting_players is not None):  # counterfactual reach probabilities
-		strategies = tf.where(
-				condition=tf.equal(acting_players, updating_player),
-				x=tf.ones_like(infoset_strategies),
-				y=infoset_strategies
-		)
-	else:
-		strategies = infoset_strategies
-	return tf.gather(params=strategies, indices=node_to_infoset, name=name)
 
 
 def get_node_strategies():

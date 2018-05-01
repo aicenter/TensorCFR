8b2d21fd93c0796dcebd3d998cd175a68b72c683
Karel Ha <mathemage@gmail.com>
2018-05-01 18:25:12 +0200

    Use for loops for examples of `TensorCFR(Domain)`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/TensorCFR.py
+++ src/algorithms/tensorcfr/TensorCFR.py
@@ -84,33 +84,9 @@ class TensorCFR:
 
 
 if __name__ == '__main__':
-	import src.domains.domain01.domain01_as_numpy_values as d1
-
-	domain01 = Domain(
-			domain_name="domain01",
-			actions_per_levels=d1.actions_per_levels,
-			node_to_infoset=d1.node_to_infoset,
-			node_types=d1.node_types,
-			utilities=d1.utilities,
-			infoset_acting_players=d1.infoset_acting_players,
-			initial_infoset_strategies=d1.initial_infoset_strategies,
-	)
-	import src.domains.matching_pennies.matching_pennies_as_numpy_values as mp
-
-	matching_pennies = Domain(
-			domain_name="matching_pennies",
-			actions_per_levels=mp.actions_per_levels,
-			node_to_infoset=mp.node_to_infoset,
-			node_types=mp.node_types,
-			utilities=mp.utilities,
-			infoset_acting_players=mp.infoset_acting_players,
-			initial_infoset_strategies=mp.initial_infoset_strategies,
-	)
-
-	tensorcfr_domain01 = TensorCFR(domain01)
-	tensorcfr_matching_pennies = TensorCFR(matching_pennies)
-
+	from src.domains.domain01.Domain01 import domain01
+	from src.domains.matching_pennies.MatchingPennies import matching_pennies
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		tensorcfr_domain01.domain.print_domain(sess)
-		tensorcfr_matching_pennies.domain.print_domain(sess)
+		for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:
+			tensorcfr.domain.print_domain(sess)

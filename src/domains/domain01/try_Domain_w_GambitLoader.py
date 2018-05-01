b343921c9412d98ccc86d7341e9d26cce5cdc51f
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-05-01 16:27:36 +0200

    [#64] Add an example of loading domain from Gambit file for domain01

---
+++ src/domains/domain01/try_Domain_w_GambitLoader.py
@@ -0,0 +1,23 @@
+import os
+import tensorflow as tf
+
+from src.domains.Domain import Domain
+
+if __name__ == '__main__':
+	path_to_domain_filename = os.path.join(
+		os.path.dirname(
+			os.path.abspath(
+				__file__)
+		),
+		'..',
+		'..',
+		'..',
+		'doc',
+		'domain01_via_gambit.efg'
+	)
+
+	domain_ = Domain.init_from_gambit_file(path_to_domain_filename)
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		domain_.print_domain(sess)

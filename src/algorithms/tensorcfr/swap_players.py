aa490da8249cdda6dce9ec2bbce1f6f164e8f8aa
Karel Ha <mathemage@gmail.com>
2018-05-01 16:59:23 +0200

    Showcase `swap_players.py` via class `TensorCFR`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/swap_players.py
+++ src/algorithms/tensorcfr/swap_players.py
@@ -1,20 +1,19 @@
 import tensorflow as tf
 
-from src.algorithms.tensorcfr.TensorCFR import swap_players
+from src.algorithms.tensorcfr.TensorCFR import TensorCFR
 from src.commons.constants import PLAYER1, PLAYER2
-
-
-# custom-made game: see doc/domain01_via_drawing.png and doc/domain01_via_gambit.png
-
+from src.domains.domain01.Domain01 import domain01
 
 if __name__ == '__main__':
-	from src.domains.domain01.domain_definitions import cfr_step, print_misc_variables
+	tensorcfr_domain01 = TensorCFR(domain01)
 
-	increment_cfr_step_op = tf.assign_add(ref=cfr_step, value=1, name="increment_cfr_step_op")
+	domain01_cfr_step = tensorcfr_domain01.domain.cfr_step
+	increment_cfr_step_op = tf.assign_add(ref=domain01_cfr_step, value=1, name="increment_cfr_step_op")
+
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		print("PLAYER1 is {}, PLAYER2 is {}".format(PLAYER1, PLAYER2))
 		for _ in range(5):
-			print("########## CFR step {} ##########\n".format(cfr_step.eval()))
-			print_misc_variables(session=sess)
-			sess.run([swap_players(), increment_cfr_step_op])
+			print("########## CFR step {} ##########\n".format(domain01_cfr_step.eval()))
+			tensorcfr_domain01.domain.print_misc_variables(session=sess)
+			sess.run([tensorcfr_domain01.swap_players(), increment_cfr_step_op])

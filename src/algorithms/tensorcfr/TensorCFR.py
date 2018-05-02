dc701fffbe87be33eaf00575753be543cf0c2d41
Karel Ha <mathemage@gmail.com>
2018-05-02 14:31:25 +0200

    Move functions of `uniform_strategies.py` to class `TensorCFR`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/TensorCFR.py
+++ src/algorithms/tensorcfr/TensorCFR.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 
-from src.commons.constants import PLAYER1, PLAYER2, TERMINAL_NODE
+from src.commons.constants import PLAYER1, PLAYER2, TERMINAL_NODE, IMAGINARY_NODE
 from src.domains.Domain import Domain
 from src.utils.distribute_strategies_to_nodes import distribute_strategies_to_nodes
 from src.utils.tensor_utils import print_tensors, expanded_multiply, scatter_nd_sum
@@ -224,6 +224,67 @@ class TensorCFR:
 						) for level in range(self.domain.levels - 1)
 			]
 		return infoset_cf_values, infoset_cf_values_per_actions
+
+	def get_infoset_children_types(self):  # TODO unittest
+		with tf.variable_scope("infoset_children_types"):
+			infoset_children_types = [None] * (self.domain.levels - 1)
+			for level in range(self.domain.levels - 1):
+				with tf.variable_scope("level{}".format(level)):
+					if level == 0:
+						infoset_children_types[0] = tf.expand_dims(
+								self.domain.node_types[1],
+								axis=0,
+								name="infoset_children_types_lvl0"
+						)
+					else:
+						infoset_children_types[level] = tf.scatter_nd_update(
+								ref=tf.Variable(
+										tf.zeros_like(
+												self.domain.current_infoset_strategies[level],
+												dtype=self.domain.node_types[level + 1].dtype
+										)
+								),
+								indices=tf.expand_dims(self.domain.node_to_infoset[level], axis=-1),
+								updates=self.domain.node_types[level + 1],
+								name="infoset_children_types_lvl{}".format(level)
+						)
+			return infoset_children_types
+
+	def get_infoset_uniform_strategies(self):  # TODO unittest
+		with tf.variable_scope("infoset_uniform_strategies"):
+			infoset_children_types = self.get_infoset_children_types()
+			infoset_uniform_strategies = [None] * (self.domain.levels - 1)
+			for level in range(self.domain.acting_depth):
+				with tf.variable_scope("level{}".format(level)):
+					infoset_uniform_strategies[level] = tf.to_float(tf.not_equal(infoset_children_types[level], IMAGINARY_NODE))
+					# Note: An all-0's row cannot be normalized. This is caused when an infoset has only imaginary children. As of
+					#       now, an all-0's row is kept without normalizing.
+					count_of_actions = tf.reduce_sum(
+							infoset_uniform_strategies[level],
+							axis=-1,
+							keepdims=True,
+							name="count_of_actions_lvl{}".format(level),
+					)
+					infosets_with_no_actions = tf.squeeze(
+							tf.equal(count_of_actions, 0.0),
+							name="rows_summing_to_zero_lvl{}".format(level)
+					)
+					infoset_uniform_strategies[level] = tf.where(
+							condition=infosets_with_no_actions,
+							x=infoset_uniform_strategies[level],
+							y=tf.divide(
+									infoset_uniform_strategies[level],
+									count_of_actions,
+							),
+							name="normalize_where_nonzero_sum_lvl{}".format(level),
+					)
+					infoset_uniform_strategies[level] = tf.where(
+							condition=self.domain.infosets_of_non_chance_player[level],
+							x=infoset_uniform_strategies[level],
+							y=self.domain.current_infoset_strategies[level],
+							name="infoset_uniform_strategies_lvl{}".format(level),
+					)
+		return infoset_uniform_strategies
 
 
 if __name__ == '__main__':

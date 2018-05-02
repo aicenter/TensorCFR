bf42789f432fb56b22acb706db9f977445991a0a
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-05-02 15:39:19 +0200

    [#64] Add domain file for GoofSpiel with 2 cards

---
+++ src/domains/goofspiel_2/domain_from_gambitloader.py
@@ -0,0 +1,24 @@
+import os
+import tensorflow as tf
+
+from src.domains.Domain import Domain
+
+if __name__ == '__main__':
+	path_to_domain_filename = os.path.join(
+		os.path.dirname(
+			os.path.abspath(
+				__file__)
+		),
+		'..',
+		'..',
+		'..',
+		'doc',
+		'goofspiel',
+		'II-GS2.gbt'
+	)
+
+	domain_ = Domain.init_from_gambit_file(path_to_domain_filename)
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		domain_.print_domain(sess)

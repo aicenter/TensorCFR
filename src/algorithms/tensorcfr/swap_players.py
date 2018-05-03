7f8f7db4aa652daf4c97125accdecdbc647edee8
Karel Ha <mathemage@gmail.com>
2018-05-03 02:47:16 +0200

    Rename `increment_cfr_step_op` to `increment_cfr_step`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/swap_players.py
+++ src/algorithms/tensorcfr/swap_players.py
@@ -8,7 +8,7 @@ from src.domains.matching_pennies.MatchingPennies import matching_pennies
 if __name__ == '__main__':
 	for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:
 		cfr_step = tensorcfr.domain.cfr_step
-		increment_cfr_step_op = tf.assign_add(ref=cfr_step, value=1, name="increment_cfr_step_op")
+		increment_cfr_step_op = tf.assign_add(ref=cfr_step, value=1, name="increment_cfr_step")
 		with tf.Session() as sess:
 			sess.run(tf.global_variables_initializer())
 			print(">>>>>>>>>> {} <<<<<<<<<<".format(tensorcfr.domain.domain_name))

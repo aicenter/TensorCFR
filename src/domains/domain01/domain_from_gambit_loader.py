38d0cfb0f4dfa50a59396f23fadd4fb0b6b99117
Karel Ha <mathemage@gmail.com>
2018-05-05 00:38:41 +0200

    Extract `get_domain01_from_gambit()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/domain01/domain_from_gambit_loader.py
+++ src/domains/domain01/domain_from_gambit_loader.py
@@ -3,21 +3,24 @@ import tensorflow as tf
 
 from src.domains.Domain import Domain
 
-if __name__ == '__main__':
+
+def get_domain01_from_gambit():
 	path_to_domain_filename = os.path.join(
-		os.path.dirname(
-			os.path.abspath(
-				__file__)
-		),
-		'..',
-		'..',
-		'..',
-		'doc',
-		'domain01_via_gambit.efg'
+			os.path.dirname(
+					os.path.abspath(
+							__file__)
+			),
+			'..',
+			'..',
+			'..',
+			'doc',
+			'domain01_via_gambit.efg'
 	)
+	return Domain.init_from_gambit_file(path_to_domain_filename)
 
-	domain_ = Domain.init_from_gambit_file(path_to_domain_filename)
 
+if __name__ == '__main__':
+	domain01_from_gambit = get_domain01_from_gambit()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		domain_.print_domain(sess)
+		domain01_from_gambit.print_domain(sess)

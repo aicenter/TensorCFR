3bc942571430792fac0385c046ae0f8b3dc2a018
Karel Ha <mathemage@gmail.com>
2018-05-04 21:10:49 +0200

    Use `get_domain_by_name` in `bottomup_expected_values.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/bottomup_expected_values.py
+++ src/algorithms/tensorcfr/bottomup_expected_values.py
@@ -3,10 +3,11 @@
 import tensorflow as tf
 
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR
-from src.domains.domain01.Domain01 import domain01
-from src.domains.matching_pennies.MatchingPennies import matching_pennies
+from src.domains.available_domains import get_domain_by_name
 
 if __name__ == '__main__':
+	domain01 = get_domain_by_name("domain01")
+	matching_pennies = get_domain_by_name("matching_pennies")
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:

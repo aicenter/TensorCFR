e318160e939cfed45a303693e7a3e08489278b2d
Karel Ha <mathemage@gmail.com>
2018-05-04 21:38:02 +0200

    Use `get_domain_by_name` in `node_strategies.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/node_strategies.py
+++ src/algorithms/tensorcfr/node_strategies.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR
-from src.domains.domain01.Domain01 import domain01
-from src.domains.matching_pennies.MatchingPennies import matching_pennies
+from src.domains.available_domains import get_domain_by_name
 
 if __name__ == '__main__':
+	domain01 = get_domain_by_name("domain01")
+	matching_pennies = get_domain_by_name("matching_pennies")
 	for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:
 		node_strategies = tensorcfr.get_node_strategies()
 		node_cf_strategies = tensorcfr.get_node_cf_strategies()

5806d73996e6ecaf007227fcdc5b2a0b277f7fc1
Karel Ha <mathemage@gmail.com>
2018-05-05 02:32:53 +0200

    Add TODO on wrong initialization of strategies
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/matching_pennies/domain_from_gambit_loader.py
+++ src/domains/matching_pennies/domain_from_gambit_loader.py
@@ -25,3 +25,7 @@ if __name__ == '__main__':
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		matching_pennies.print_domain(sess)
+		# TODO Why does GambitEFGLoader initialize strategies to:
+		# "from_gambit/initial_infoset_strategies_lvl1:0"
+    # [[0.25 0.25]]
+

7a47881653d47baa95f3ec54e84b1ca4bd20bfe5
Karel Ha <mathemage@gmail.com>
2018-05-06 17:53:06 +0200

    Get domains by name in `tensorcfr_on_matching_pennies.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/matching_pennies/tensorcfr_on_matching_pennies.py
+++ src/domains/matching_pennies/tensorcfr_on_matching_pennies.py
@@ -1,9 +1,9 @@
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR, run_cfr
-from src.domains.available_domains import get_domain_by_name, MATCHING_PENNIES, MATCHING_PENNIES_GAMBIT
+from src.domains.available_domains import get_domain_by_name
 
 if __name__ == '__main__':
-	domain = get_domain_by_name(MATCHING_PENNIES)
-	# domain = get_domain_by_name(MATCHING_PENNIES_GAMBIT)
+	# domain = get_domain_by_name("matching_pennies")
+	domain = get_domain_by_name("matching_pennies_via_gambit")
 	tensorcfr = TensorCFR(domain)
 	run_cfr(
 			# total_steps=10,

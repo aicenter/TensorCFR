c3c7c23a2fa20220acb9ed13cea7eba20417e955
Karel Ha <mathemage@gmail.com>
2018-05-08 18:53:24 +0200

    De-activate `tensorcfr_on_goofspiel5.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/tensorcfr_on_goofspiel5.py
+++ src/algorithms/tensorcfr/tensorcfr_on_goofspiel5.py
@@ -2,7 +2,7 @@ from src.algorithms.tensorcfr.TensorCFR import TensorCFR, run_cfr
 from src.domains.available_domains import get_domain_by_name
 
 # TODO: Get rid of `ACTIVATE_FILE` hotfix in "#74 Storage for large files"
-ACTIVATE_FILE = True
+ACTIVATE_FILE = False
 
 
 if __name__ == '__main__' and ACTIVATE_FILE:
@@ -12,6 +12,6 @@ if __name__ == '__main__' and ACTIVATE_FILE:
 			total_steps=10,
 			tensorcfr_instance=tensorcfr,
 			quiet=True,
-			profiling=True,
+			# profiling=True,
 			delay=0
 	)   # TODO verify the results (final average strategies) via `gtlibrary`

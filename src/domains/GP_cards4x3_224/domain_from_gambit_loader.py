dd974ca666995bf7b11034e0a260c8e6c7ffd220
Karel Ha <mathemage@gmail.com>
2018-05-11 00:03:46 +0200

    Create domain `GP_cards4x3_224`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/domains/GP_cards4x3_224/domain_from_gambit_loader.py
@@ -0,0 +1,28 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"   # off GPU
+import tensorflow as tf
+
+from src.domains.Domain import Domain
+
+
+def get_domain_GP_cards4x3_224():
+	path_to_domain_filename = os.path.join(
+			os.path.dirname(
+					os.path.abspath(
+							__file__)
+			),
+			'..',
+			'..',
+			'..',
+			'doc',
+			'poker',
+			'GP_cards4x3_224.gbt'
+	)
+	return Domain.init_from_gambit_file(path_to_domain_filename, domain_name="GP_cards4x3_224_via_gambit")
+
+
+if __name__ == '__main__':
+	domain = get_domain_GP_cards4x3_224()
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		domain.print_domain(sess)

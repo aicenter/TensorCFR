33162d83025c54f36fd2993803c7ee0b51086b37
Karel Ha <mathemage@gmail.com>
2018-05-10 03:58:55 +0200

    Run `tensorcfr_on_domain01.py` for 1000 iterations
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/tensorcfr_on_domain01.py
+++ src/algorithms/tensorcfr/tensorcfr_on_domain01.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
 	domain = get_domain_by_name("domain01_via_gambit")
 	tensorcfr = TensorCFR(domain)
 	run_cfr(
-			total_steps=10,
+			# total_steps=10,
 			tensorcfr_instance=tensorcfr,
 			quiet=True,
 			# profiling=True,
-			delay=0
+			# delay=0
 	)

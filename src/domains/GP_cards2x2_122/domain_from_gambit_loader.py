0eab3481d0e4795dba2d8895d7b36ee1cc6e9068
Karel Ha <mathemage@gmail.com>
2018-05-10 22:54:37 +0200

    Create domain `GP_cards2x2_122`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/domains/GP_cards2x2_122/domain_from_gambit_loader.py
@@ -0,0 +1,27 @@
+import os
+import tensorflow as tf
+
+from src.domains.Domain import Domain
+
+
+def get_domain_GP_cards2x2_122():
+	path_to_domain_filename = os.path.join(
+			os.path.dirname(
+					os.path.abspath(
+							__file__)
+			),
+			'..',
+			'..',
+			'..',
+			'doc',
+			'poker',
+			'GP_cards2x2_122.gbt'
+	)
+	return Domain.init_from_gambit_file(path_to_domain_filename, domain_name="GP_cards2x2_122_via_gambit")
+
+
+if __name__ == '__main__':
+	poker = get_domain_GP_cards2x2_122()
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		poker.print_domain(sess)

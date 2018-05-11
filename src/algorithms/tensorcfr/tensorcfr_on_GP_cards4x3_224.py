9587ced347ee65c120c3137d2b94912f06394688
Karel Ha <mathemage@gmail.com>
2018-05-11 12:59:28 +0200

    Disable `ACTIVATE_FILE` for TensorCFR on Poker domains
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/tensorcfr_on_GP_cards4x3_224.py
+++ src/algorithms/tensorcfr/tensorcfr_on_GP_cards4x3_224.py
@@ -1,7 +1,10 @@
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR, run_cfr
 from src.domains.available_domains import get_domain_by_name
 
-if __name__ == '__main__':
+# TODO: Get rid of `ACTIVATE_FILE` hotfix in "#74 Storage for large files"
+ACTIVATE_FILE = False
+
+if __name__ == '__main__' and ACTIVATE_FILE:
 	domain = get_domain_by_name("GP_cards4x3_224_via_gambit")
 	tensorcfr = TensorCFR(domain)
 	run_cfr(

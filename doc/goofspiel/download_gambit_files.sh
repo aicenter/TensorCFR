c941f6d321c8c9a65d2991ec045299b9e97fa541
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-05-18 16:24:25 +0200

    Add  for GoofSpiel

--- doc/goofspiel/download_gambit_files.sh
+++ doc/goofspiel/download_gambit_files.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Gambit files Google Drive from https://drive.google.com/drive/folders/1D3RomFdf9kPkyLQKdOX3hpi1dQEy5Ewt
-# Cesnet OwnCloud https://owncloud.cesnet.cz/index.php/s/rxMq2SUqwfhsjlE
 files=(
-"v0bbWtFuZHMzRAd" # IIGS5_s1_bf_ft.gbt
-"Ne4oZZXPsfXiV33" # IIGS5_s6_bf_ft.gbt
+"IIGS5_s1_bf_ft.gbt:74hraxIEuVV8M4j"
+"IIGS6_s1_bf_ft.gbt:HLFkymuC0gyh7ST"
 )

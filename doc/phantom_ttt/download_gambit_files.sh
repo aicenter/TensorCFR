10e3abc49d8adf1f7c29e8c9790d2dd9fe49155a
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-05-18 18:03:57 +0200

    Edit file for Phantom Tic Tac Toe

--- doc/phantom_ttt/download_gambit_files.sh
+++ doc/phantom_ttt/download_gambit_files.sh
@@ -2,5 +2,5 @@
 
 # Gambit files Google Drive from https://drive.google.com/drive/folders/1D3RomFdf9kPkyLQKdOX3hpi1dQEy5Ewt
 files=(
-"PTTT_ft.gbt:tgsZHIKxrvYbc5i"
+"phantom_ttt.efg:BfhbFanzMPxMMWb"
 )

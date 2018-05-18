67b4a17064daafd166767db834f69ae880174d2c
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-05-18 16:26:07 +0200

    Add  for PhantomTTT
    Signed-off-by: janrudolf <jan.rudolf.steeldog@gmail.com>

--- doc/phantom_ttt/download_gambit_files.sh
+++ doc/phantom_ttt/download_gambit_files.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Gambit files Google Drive from https://drive.google.com/drive/folders/1D3RomFdf9kPkyLQKdOX3hpi1dQEy5Ewt
-# Cesnet OwnCloud https://owncloud.cesnet.cz/index.php/s/NHK8MwFzCMun9FJ
 files=(
-"1f69HuVlJ9fMzLI" # PTTT_ft.gbt
+"PTTT_ft.gbt:tgsZHIKxrvYbc5i"
 )

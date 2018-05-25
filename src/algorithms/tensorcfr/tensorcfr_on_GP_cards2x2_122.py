6e413c81d3ddfbacc26670f81d106bd238f191c3
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-05-25 12:03:12 +0200

    Get rid of 'ACTIVATE_FILE' in algorithms/tensorcfr/tensorcfr_on_GP_cards2x2_122

--- src/algorithms/tensorcfr/tensorcfr_on_GP_cards2x2_122.py
+++ src/algorithms/tensorcfr/tensorcfr_on_GP_cards2x2_122.py
@@ -1,10 +1,8 @@
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR, run_cfr
 from src.domains.available_domains import get_domain_by_name
 
-# TODO: Get rid of `ACTIVATE_FILE` hotfix in "#74 Storage for large files"
-ACTIVATE_FILE = False
 
-if __name__ == '__main__' and ACTIVATE_FILE:
+if __name__ == '__main__':
 	domain = get_domain_by_name("GP_cards2x2_122_via_gambit")
 	tensorcfr = TensorCFR(domain)
 	run_cfr(

f745fa7df287b9471df2381d89916cf40e13357d
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-05-25 11:43:13 +0200

    Edit TODO comment in domains/goofspiel6 - we will not execute it during CI due to memory limitation

--- src/domains/goofspiel6/domain_from_gambit_loader.py
+++ src/domains/goofspiel6/domain_from_gambit_loader.py
@@ -20,7 +20,7 @@ def get_domain_goofspiel6():
 	return Domain.init_from_gambit_file(path_to_domain_filename, domain_name="IIGS6_s1_bf_ft_via_gambit")
 
 
-# TODO: Get rid of `ACTIVATE_FILE` hotfix in "#74 Storage for large files"
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
 ACTIVATE_FILE = False
 
 if __name__ == '__main__' and ACTIVATE_FILE:

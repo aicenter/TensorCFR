7d4fc8ab5dedce9d9c3f1c057caf77497c3fa72d
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-06-01 09:50:45 +0200

    Edit __init__.py reflecting deleted files

--- src/domains/domain01/__init__.py
+++ src/domains/domain01/__init__.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
-__all__ = ["domain01", "topdown_reach_probabilities", "bottomup_expected_values", "counterfactual_values",
-           "node_strategies", "regrets", "strategy_matched_to_regrets", "uniform_strategies", "domain_definitions",
-           "domain01_as_numpy_values"]
+__all__ = ["domain_definitions", "domain01_as_numpy_values"]

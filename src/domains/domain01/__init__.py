a26d0650a496785729affccb3d11291e31590f85
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-06-01 12:33:23 +0200

    Delete domain.domain01.domain_definitions.py

--- src/domains/domain01/__init__.py
+++ src/domains/domain01/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
-__all__ = ["domain_definitions", "domain01_as_numpy_values"]
+__all__ = ["Domain01", "domain01_as_numpy_values", "domain_from_gambit_loader"]

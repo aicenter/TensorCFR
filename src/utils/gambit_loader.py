b49ba50b1ea8feee8cfc404720b5c62dfff082bd
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-06-06 11:26:34 +0200

    Delete parse methods in gambit_loader.py

--- src/utils/gambit_loader.py
+++ src/utils/gambit_loader.py
@@ -9,6 +9,9 @@ import tensorflow as tf
 
 from src.commons import constants
 
+from src.utils.gambit import Parser
+
+
 TMP_NODE_TO_INFOSET_IMAGINARY_NODE = -1
 
 
@@ -150,169 +153,37 @@ class GambitEFGLoader:
 
 		with open(efg_file) as self.gambit_file:
 			game_header_line = self.gambit_file.readline()
-			game_header = self.parse_gambit_header(game_header_line)
+			game_header = Parser.parse_header(game_header_line)
 
 			if game_header['format'] == 'NFG':
 				raise NotImplementedFormatException
 
 			self.domain_name = game_header['name']
+			print(game_header)
 
-			self.load()
+			# self.load()
 
-		self.infoset_managers = [InformationSetManager(lvl) for lvl in range(len(self.actions_per_levels) + 1)]
+		# self.infoset_managers = [InformationSetManager(lvl) for lvl in range(len(self.actions_per_levels) + 1)]
+		#
+		# self.node_to_infoset = [None] * self.number_of_levels
+		# self.current_infoset_strategies = [None] * self.number_of_levels
+		# self.initial_infoset_strategies = [None] * self.number_of_levels  # TODO temporary because of TensorCFR.py
+		# self.infoset_acting_players = [None] * self.number_of_levels
+		# self.cumulative_regrets = [None] * self.number_of_levels
+		# self.positive_cumulative_regrets = [None] * self.number_of_levels
+		#
+		# self.node_types = [None] * (self.number_of_levels + 1)
+		# self.utilities = [None] * (self.number_of_levels + 1)
+		# self.node_to_infoset = [None] * (self.number_of_levels + 1)
+		#
+		# for idx in range(len(self.actions_per_levels) + 1):
+		# 	self.utilities[idx] = np.ones(self.actions_per_levels[:idx]) * constants.NON_TERMINAL_UTILITY
+		# 	self.node_types[idx] = np.ones(self.actions_per_levels[:idx], dtype=np.int) * constants.IMAGINARY_NODE
+		# 	self.node_to_infoset[idx] = np.ones(self.actions_per_levels[:idx], dtype=np.int) * TMP_NODE_TO_INFOSET_IMAGINARY_NODE
 
-		self.node_to_infoset = [None] * self.number_of_levels
-		self.current_infoset_strategies = [None] * self.number_of_levels
-		self.initial_infoset_strategies = [None] * self.number_of_levels  # TODO temporary because of TensorCFR.py
-		self.infoset_acting_players = [None] * self.number_of_levels
-		self.cumulative_regrets = [None] * self.number_of_levels
-		self.positive_cumulative_regrets = [None] * self.number_of_levels
+		# with open(efg_file) as self.gambit_file:
+		# 	self.load_post()
 
-		self.node_types = [None] * (self.number_of_levels + 1)
-		self.utilities = [None] * (self.number_of_levels + 1)
-		self.node_to_infoset = [None] * (self.number_of_levels + 1)
-
-		for idx in range(len(self.actions_per_levels) + 1):
-			self.utilities[idx] = np.ones(self.actions_per_levels[:idx]) * constants.NON_TERMINAL_UTILITY
-			self.node_types[idx] = np.ones(self.actions_per_levels[:idx], dtype=np.int) * constants.IMAGINARY_NODE
-			self.node_to_infoset[idx] = np.ones(self.actions_per_levels[:idx], dtype=np.int) * TMP_NODE_TO_INFOSET_IMAGINARY_NODE
-			# self.cumulative_regrets[idx] = np.zeros(self.actions_per_levels[:idx])
-			# self.positive_cumulative_regrets[idx] = np.zeros(self.actions_per_levels[:idx])
-
-		with open(efg_file) as self.gambit_file:
-			self.load_post()
-
-	@staticmethod
-	def parse_gambit_header(input_line):
-		results = re.search(r'^(?P<format>EFG|NFG) (?P<version>\d) R "(?P<name>[^"]+)" \{(?P<players_dirty>.*)\}',
-		                    input_line)
-		if results:
-			results_players = re.findall(r'"([^"]+)"', results.group('players_dirty'))
-			if results_players is None:
-				raise NotRecognizedPlayersException
-			return_dict = {
-				'format' : results.group('format'),
-				'version': int(results.group('version')),
-				'name'   : results.group('name'),
-				'players': results_players
-			}
-			return return_dict
-		else:
-			raise NotAcceptableFormatException
-
-	def parse_node(self, input_line):
-		# http://www.gambit-project.org/gambit13/formats.html
-		if len(input_line) == 0:
-			return False
-
-		node_type = input_line[0]
-
-		if node_type == constants.GAMBIT_NODE_TYPE_CHANCE:
-			return self.parse_chance_node(input_line)
-		elif node_type == constants.GAMBIT_NODE_TYPE_PLAYER:
-			return self.parse_player_node(input_line)
-		elif node_type == constants.GAMBIT_NODE_TYPE_TERMINAL:
-			return self.parse_terminal_node(input_line)
-		else:
-			return False
-
-	@staticmethod
-	def parse_probability(probability_str):
-		if '/' in probability_str:
-			probability_list = probability_str.split('/')
-			return float(int(probability_list[0]) / int(probability_list[1]))
-		else:
-			return float(probability_str)
-
-	def parse_actions_chance(self, input_actions_str):
-		parse_actions = re.findall(r'"(?P<name>[^"]*)" (?P<probability>[\d\./]+)', input_actions_str)
-		return [{'name': action[0], 'probability': self.parse_probability(action[1])} for action in parse_actions]
-
-	@staticmethod
-	def parse_actions_player(input_actions_str):
-		parse_actions = re.findall(r'"(?P<name>[^"]*)"', input_actions_str)
-		return [{'name': action[0]} for action in parse_actions]
-
-	@staticmethod
-	def parse_payoffs(input_payoffs_str):
-		parse_payoffs = re.findall(r'[\-]?[\d]+', input_payoffs_str)
-		return [int(payoff) for payoff in parse_payoffs]
-
-	def parse_chance_node(self, input_line):
-		parse_line = re.search(
-				r'^(?P<type>' + constants.GAMBIT_NODE_TYPE_CHANCE + ') "(?P<name>[^"]*)" (?P<information_set_number>\d+)\ ?"?(?P<information_set_name_optional>[^"]*)"?\ ?\{?(?P<actions_optional>[^\}]*)\}?\ ?(?P<outcome>\d+)\ ?"?(?P<outcome_name_optional>[^"]*)"?\ ?\{?(?P<payoffs_optional>.*)\}?',
-				input_line
-		)
-
-		actions = self.parse_actions_chance(parse_line.group('actions_optional'))
-		payoffs = self.parse_payoffs(parse_line.group('payoffs_optional'))
-		infoset_id = 'c-' + parse_line.group('information_set_number')
-
-		return {
-			'type'                  : parse_line.group('type'),
-			'name'                  : parse_line.group('name'),
-			'information_set_number': int(parse_line.group('information_set_number')),
-			'information_set_name'  : parse_line.group('information_set_name_optional'),
-			'actions'               : actions,
-			'outcome'               : parse_line.group('outcome'),
-			'outcome_name'          : parse_line.group('outcome_name_optional'),
-			'payoffs'               : payoffs,
-			'tensorcfr_id'          : constants.CHANCE_PLAYER,
-			'infoset_id'            : infoset_id
-		}
-
-	def parse_player_node(self, input_line):
-		parse_line = re.search(
-				r'^(?P<type>' + constants.GAMBIT_NODE_TYPE_PLAYER + ') "(?P<name>[^"]*)" (?P<player_number>\d+) (?P<information_set_number>\d+)\ ?"?(?P<information_set_name>[^"]*)"?\ ?\{?(?P<actions_optional>[^\}]*)\}?\ ?(?P<outcome>\d+)\ ?"?(?P<outcome_name>[^"]*)"?\ ?\{?(?P<payoffs_optional>[^\}]*)\}?',
-				input_line
-		)
-
-		actions = self.parse_actions_player(parse_line.group('actions_optional'))
-		payoffs = self.parse_payoffs(parse_line.group('payoffs_optional'))
-		infoset_id = 'p-' + parse_line.group('player_number') + '-' + parse_line.group('information_set_number')
-
-		return {
-			'type'                  : parse_line.group('type'),
-			'name'                  : parse_line.group('name'),
-			'player_number'         : int(parse_line.group('player_number')),
-			'information_set_number': int(parse_line.group('information_set_number')),
-			'information_set_name'  : parse_line.group('information_set_name'),
-			'actions'               : actions,
-			'outcome'               : parse_line.group('outcome'),
-			'outcome_name'          : parse_line.group('outcome_name'),
-			'payoffs'               : payoffs,
-			'tensorcfr_id'          : int(parse_line.group('player_number')),
-			'infoset_id'            : infoset_id
-		}
-
-	def parse_terminal_node(self, input_line):
-		parse_line = re.search(
-				r'^(?P<type>' + constants.GAMBIT_NODE_TYPE_TERMINAL + ') "(?P<name>[^"]*)" (?P<outcome>\d+)\ ?"?(?P<outcome_name_optional>[^"]*)"?\ ?\{(?P<payoffs>.*)\}',
-				input_line
-		)
-
-		payoffs = self.parse_payoffs(parse_line.group('payoffs'))
-		# infoset_id = 't-' + str(self.terminal_nodes_cnt)
-		infoset_id = 't'
-
-		self.terminal_nodes_cnt += 1
-
-		return {
-			'type'        : parse_line.group('type'),
-			'name'        : parse_line.group('name'),
-			'outcome'     : parse_line.group('outcome'),
-			'outcome_name': parse_line.group('outcome_name_optional'),
-			'payoffs'     : payoffs,
-			'tensorcfr_id': constants.NO_ACTING_PLAYER,
-			'infoset_id'  : infoset_id
-		}
-
-	@staticmethod
-	def is_gambit_node(line):
-		if line[0] == constants.GAMBIT_NODE_TYPE_CHANCE or line[0] == constants.GAMBIT_NODE_TYPE_PLAYER or line[
-			0] == constants.GAMBIT_NODE_TYPE_TERMINAL:
-			return True
-		return False
 
 	def load(self):
 		# determines the maximum number of actions per level
@@ -481,15 +352,17 @@ if __name__ == '__main__':
 		# poker_gbt,
 	]
 
-	for gbt_file in gbt_files:
-		domain = GambitEFGLoader(gbt_file)
-		print("\n>>>>>>>>>> {} <<<<<<<<<<".format(gbt_file))
-		for level in range(len(domain.actions_per_levels) + 1):
-			print("\n########## Level {} ##########".format(level))
-			print(domain.node_types[level])
-			print(domain.utilities[level])
-			if level < len(domain.actions_per_levels):
-				print(domain.node_to_infoset[level])
-				print(domain.infoset_acting_players[level])
-				print(domain.initial_infoset_strategies[level])
-				print(domain.current_infoset_strategies[level])
+	GambitEFGLoader(domain01_efg)
+
+	# for gbt_file in gbt_files:
+	# 	domain = GambitEFGLoader(gbt_file)
+	# 	print("\n>>>>>>>>>> {} <<<<<<<<<<".format(gbt_file))
+	# 	for level in range(len(domain.actions_per_levels) + 1):
+	# 		print("\n########## Level {} ##########".format(level))
+	# 		print(domain.node_types[level])
+	# 		print(domain.utilities[level])
+	# 		if level < len(domain.actions_per_levels):
+	# 			print(domain.node_to_infoset[level])
+	# 			print(domain.infoset_acting_players[level])
+	# 			print(domain.initial_infoset_strategies[level])
+	# 			print(domain.current_infoset_strategies[level])

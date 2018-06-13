c6c3caefa9ec0dbffce6a0f4e88f53956c9badd3
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-06-13 15:49:40 +0200

    Changes in gambit_loader.py, get right initial strategies, infosets, utilities

--- src/utils/gambit_loader.py
+++ src/utils/gambit_loader.py
@@ -5,7 +5,6 @@ import os
 import re
 
 import numpy as np
-import tensorflow as tf
 
 from src.commons import constants
 
@@ -51,54 +50,68 @@ class InformationSetManager:
 		self.flag_imaginary_node_present = False
 		self.flag_terminal_node_present = False
 
-	@staticmethod
-	def _is_imaginary_node_present(level, node_types_):
-		tensor = node_types_[level]
-		result = tensor[np.where(tensor == constants.IMAGINARY_NODE)]
-
-		if result.shape == (0,):
-			return False
-		return True
-
-	@staticmethod
-	def _is_terminal_node_present(level, node_types_):
-		tensor = node_types_[level]
-		result = tensor[np.where(tensor == constants.TERMINAL_NODE)]
-
-		if result.shape == (0,):
-			return False
-		return True
+	# @staticmethod
+	# def _is_imaginary_node_present(level, node_types_):
+	# 	tensor = node_types_[level]
+	# 	result = tensor[np.where(tensor == constants.IMAGINARY_NODE)]
+	#
+	# 	if result.shape == (0,):
+	# 		return False
+	# 	return True
+	#
+	# @staticmethod
+	# def _is_terminal_node_present(level, node_types_):
+	# 	tensor = node_types_[level]
+	# 	result = tensor[np.where(tensor == constants.TERMINAL_NODE)]
+	#
+	# 	if result.shape == (0,):
+	# 		return False
+	# 	return True
 
 	def add(self, node):
-		# node type due to _is_imaginary_node_present
-		self.infoset_node_to_infoset.append(node['infoset_id'])
-
-		if node['type'] == constants.GAMBIT_NODE_TYPE_PLAYER:
-			self.cnt_player_nodes += 1
+		if node['type'] == constants.GAMBIT_NODE_TYPE_TERMINAL:
+			self.flag_imaginary_node_present = True
+			return None
 
 		if node['infoset_id'] not in self.infoset_dict:
-			return_node_to_infoset_value = self.infoset_cnt
-			self.infoset_dict[node['infoset_id']] = [return_node_to_infoset_value, node['type'], node['tensorcfr_id'], node]
+			infoset_index = len(self.infoset_dict)
+			self.infoset_dict[node['infoset_id']] = [infoset_index, node['type'], node['tensorcfr_id'],
+													 node, len(node['actions'])]
 			self.infoset_acting_players_list.insert(0, node['infoset_id'])
-			self.infoset_cnt += 1
-			return return_node_to_infoset_value
+			return infoset_index
 		else:
-			return_node_to_infoset_value = self.infoset_dict[node['infoset_id']][0]
-			return return_node_to_infoset_value
+			# self.infoset_dict[node['infoset_id']][4] += len(node['actions'])
+			return self.infoset_dict[node['infoset_id']][0]
+		# # node type due to _is_imaginary_node_present
+		# self.infoset_node_to_infoset.append(node['infoset_id'])
+		#
+		# if node['infoset_id'] not in self.infoset_dict:
+		# 	return_node_to_infoset_value = self.infoset_cnt
+		# 	self.infoset_dict[node['infoset_id']] = [return_node_to_infoset_value, node['type'], node['tensorcfr_id'], node]
+		# 	self.infoset_acting_players_list.insert(0, node['infoset_id'])
+		# 	self.infoset_cnt += 1
+		# 	return return_node_to_infoset_value
+		# else:
+		# 	return_node_to_infoset_value = self.infoset_dict[node['infoset_id']][0]
+		# 	return return_node_to_infoset_value
 
-	def make_infoset_acting_players(self, next_level_max_no_actions, node_types_):
-		if self.flag_set is False and self.level > 0:
-			self.flag_imaginary_node_present = self._is_imaginary_node_present(self.level, node_types_)
-			self.flag_set = True
+	def make_infoset_acting_players(self, next_level_max_no_actions):
+		# if self.flag_set is False and self.level > 0:
+		# 	self.flag_imaginary_node_present = self._is_imaginary_node_present(self.level, node_types_)
+		# 	self.flag_set = True
 
 		infoset_acting_players_ = []
 		current_infoset_strategies_ = []
 
-		if self.flag_imaginary_node_present:
-			self.infoset_dict['imaginary-node'] = [self.infoset_cnt, 'tnode', -1]  # last element - imaginary
-			self.infoset_acting_players_list.append('imaginary-node')
+		# if self.flag_imaginary_node_present:
+		# 	self.infoset_dict['imaginary-node'] = [self.infoset_cnt, 'tnode', -1]  # last element - imaginary
+		# 	self.infoset_acting_players_list.append('imaginary-node')
 
-		for idx, infoset_id in enumerate(self.infoset_acting_players_list):
+		if self.flag_terminal_node_present:
+			self.infoset_dict['terminal-node'] = [len(self.infoset_cnt), 'tnode', -1]
+			self.infoset_acting_players_list.append('terminal-node')
+
+		for idx, infoset_id in enumerate(reversed(self.infoset_acting_players_list)):
 			infoset_acting_players_.append(self.infoset_dict[infoset_id][2])
 
 			if self.infoset_dict[infoset_id][1] == constants.GAMBIT_NODE_TYPE_PLAYER:
@@ -110,10 +123,17 @@ class InformationSetManager:
 				#
 				#  Check out the method `get_infoset_uniform_strategies()` located at line 266 of
 				#  `algorithms.tensorcfr.TensorCFR.py`.
-				current_infoset_strategies_.append(
-						# [float(1 / (next_level_max_no_actions * self.cnt_player_nodes))] * next_level_max_no_actions
-						[np.nan] * next_level_max_no_actions  # TODO This is a hotfix.
-				)
+				current_infoset_strategy = [np.nan] * next_level_max_no_actions
+
+				action = [float(1 / (self.infoset_dict[infoset_id][4]))] * len(self.infoset_dict[infoset_id][3]['actions'])
+
+				for index, action in enumerate(action):
+					current_infoset_strategy[index] = action
+
+				current_infoset_strategies_.append(current_infoset_strategy)
+
+			# [np.nan] * next_level_max_no_actions  # TODO This is a hotfix.
+
 			elif self.infoset_dict[infoset_id][1] == constants.GAMBIT_NODE_TYPE_CHANCE:
 				current_infoset_strategy = [np.nan] * next_level_max_no_actions
 
@@ -121,19 +141,15 @@ class InformationSetManager:
 					current_infoset_strategy[index] = action['probability']
 
 				current_infoset_strategies_.append(current_infoset_strategy)
-			else:
-				# current_infoset_strategies_.append([0] * next_level_max_no_actions)
-				# TODO Just to be sure, let's put NaNs everywhere.
-				current_infoset_strategies_.append([np.nan] * next_level_max_no_actions)
 
 		return [np.asarray(infoset_acting_players_, dtype=constants.INT_DTYPE_NUMPY),
 		        np.asarray(current_infoset_strategies_)]
 
-	def make_node_to_infoset(self, tensor):
-		tensor[np.where(tensor == TMP_NODE_TO_INFOSET_IMAGINARY_NODE)] = -self.infoset_cnt
-		tensor[np.where(tensor >= 0)] += -(self.infoset_cnt - 1)
-		tensor[np.where(tensor != 0)] *= -1
-		return tensor
+	# def make_node_to_infoset(self, tensor):
+	# 	tensor[np.where(tensor == TMP_NODE_TO_INFOSET_IMAGINARY_NODE)] = -self.infoset_cnt
+	# 	tensor[np.where(tensor >= 0)] += -(self.infoset_cnt - 1)
+	# 	tensor[np.where(tensor != 0)] *= -1
+	# 	return tensor
 
 
 class GambitEFGLoader:
@@ -145,6 +161,7 @@ class GambitEFGLoader:
 
 		self.domain_name = ""
 		self.actions_per_levels = []
+		self.max_actions_per_levels = []
 		# number of nodes per level starting from level 0
 		self.nodes_per_levels = [1]  # level 0 has always one node
 		self.number_of_levels = 0
@@ -165,25 +182,30 @@ class GambitEFGLoader:
 
 		print(self.actions_per_levels)
 		print(self.nodes_per_levels)
+		print(self.max_actions_per_levels)
 
 		self.number_of_levels = len(self.nodes_per_levels)
 		self.nodes_indexes = copy.deepcopy(self.nodes_per_levels)
 
+		# self.nodes_placement = [None] * self.number_of_levels
 		self.utilities = [None] * self.number_of_levels
+		self.node_to_infoset = [None] * self.number_of_levels
 
 		for level, number_of_nodes in enumerate(self.nodes_per_levels):
+			# self.nodes_placement[level] = [None] * number_of_nodes
 			self.utilities[level] = [0] * number_of_nodes
+			self.node_to_infoset[level] = [None] * number_of_nodes
 
-		# self.infoset_managers = [InformationSetManager(lvl) for lvl in range(len(self.actions_per_levels) + 1)]
+		self.infoset_managers = [InformationSetManager(lvl) for lvl in range(len(self.actions_per_levels) + 1)]
 		#
 		# self.node_to_infoset = [None] * self.number_of_levels
-		# self.current_infoset_strategies = [None] * self.number_of_levels
+		self.current_infoset_strategies = [None] * self.number_of_levels
 		# self.initial_infoset_strategies = [None] * self.number_of_levels  # TODO temporary because of TensorCFR.py
-		# self.infoset_acting_players = [None] * self.number_of_levels
+		self.infoset_acting_players = [None] * self.number_of_levels
 		# self.cumulative_regrets = [None] * self.number_of_levels
 		# self.positive_cumulative_regrets = [None] * self.number_of_levels
 		#
-		# self.node_types = [None] * (self.number_of_levels + 1)
+		self.node_types = [None] * (self.number_of_levels + 1)
 		# self.utilities = [None] * (self.number_of_levels + 1)
 		# self.node_to_infoset = [None] * (self.number_of_levels + 1)
 		#
@@ -212,6 +234,7 @@ class GambitEFGLoader:
 				if node['type'] != constants.GAMBIT_NODE_TYPE_TERMINAL:
 					if len(self.actions_per_levels) < (level + 1):
 						self.actions_per_levels.append(0)
+						self.max_actions_per_levels.append(0)
 
 					for action in node['actions']:
 						new_level = level + 1
@@ -222,6 +245,7 @@ class GambitEFGLoader:
 						)
 
 					self.actions_per_levels[level] += len(node['actions'])
+					self.max_actions_per_levels[level] = max(len(node['actions']), self.max_actions_per_levels[level])
 			self.number_of_levels = len(self.actions_per_levels)
 
 		self.nodes_per_levels.extend(self.actions_per_levels)
@@ -235,12 +259,8 @@ class GambitEFGLoader:
 		else:
 			self.node_types[level][tuple(coordinates)] = value
 
-	def update_node_to_infoset(self, level, coordinates, value):
-		if level == 0:
-			self.node_to_infoset[level] = value
-		else:
-			self.node_to_infoset[level][tuple(coordinates)] = value
-
+	def update_node_to_infoset(self, level, action_index, value):
+		self.node_to_infoset[level][self.placement_indices[level] + action_index] = value
 
 	def load_post(self):
 		# a vector of indices to filling vectors
@@ -254,8 +274,8 @@ class GambitEFGLoader:
 				current_node = Parser.parse_node(line)
 				current_tree_node = nodes_stack.pop()
 
-				# node_to_infoset_value = self.infoset_managers[level].add(node)
-				# self.update_node_to_infoset(level, coordinates, node_to_infoset_value)
+				node_to_infoset_value = self.infoset_managers[current_tree_node.level].add(current_node)
+				self.update_node_to_infoset(current_tree_node.level, current_tree_node.action_index, node_to_infoset_value)
 
 				if current_node['type'] != constants.GAMBIT_NODE_TYPE_TERMINAL:
 					# count the number of actions of the current node
@@ -271,12 +291,12 @@ class GambitEFGLoader:
 
 		# for level in range(1, self.number_of_levels):
 		# 	self.node_to_infoset[level] = self.infoset_managers[level].make_node_to_infoset(self.node_to_infoset[level])
-		#
-		# for level in range(self.number_of_levels):
-		# 	[infoset_acting_players_, infoset_strategies] = self.infoset_managers[level].make_infoset_acting_players(
-		# 			self.actions_per_levels[level], self.node_types)
-		# 	self.infoset_acting_players[level] = infoset_acting_players_
-		# 	self.current_infoset_strategies[level] = infoset_strategies
+		print("Number of levels", self.number_of_levels)
+		for level, max_action in enumerate(self.max_actions_per_levels):
+			[infoset_acting_players_, infoset_strategies] = self.infoset_managers[level].make_infoset_acting_players(
+				max_action)
+			self.infoset_acting_players[level] = infoset_acting_players_
+			self.current_infoset_strategies[level] = infoset_strategies
 		# 	self.initial_infoset_strategies[level] = np.array(copy.deepcopy(self.current_infoset_strategies[level]))
 		# 	self.cumulative_regrets[level] = np.zeros(infoset_strategies.shape)
 		# 	self.positive_cumulative_regrets[level] = np.zeros(infoset_strategies.shape)
@@ -307,7 +327,17 @@ if __name__ == '__main__':
 		# poker_gbt,
 	]
 
-	GambitEFGLoader(domain01_efg)
+	domain = GambitEFGLoader(domain01_efg)
+
+	for level in [0,1,2]:
+		print("LEVEL {}".format(level))
+		print("node_to_infoset")
+		print(domain.node_to_infoset[level])
+		print("infoset_acting_players")
+		print(domain.infoset_acting_players[level])
+		# print(domain.initial_infoset_strategies[level])
+		print("current_infoset_strategies")
+		print(domain.current_infoset_strategies[level])
 
 	# for gbt_file in gbt_files:
 	# 	domain = GambitEFGLoader(gbt_file)

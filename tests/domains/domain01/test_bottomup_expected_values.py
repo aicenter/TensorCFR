380989998fbed8f85a03d4b8839fa50f67b145e6
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-06-19 17:51:29 +0200

    Edit tests in bottom-up expected values
    Signed-off-by: janrudolf <jan.rudolf.steeldog@gmail.com>

--- tests/domains/domain01/test_bottomup_expected_values.py
+++ tests/domains/domain01/test_bottomup_expected_values.py
@@ -1,39 +1,30 @@
-import unittest
-
 import numpy as np
 import tensorflow as tf
 
+from src.algorithms.tensorcfr.TensorCFR import TensorCFR
 from src.commons.constants import SMALL_ERROR_TOLERANCE, LARGE_ERROR_TOLERANCE
-# TODO: fix old import
-# from src.domains.domain01.bottomup_expected_values import get_expected_values
+from src.domains.domain01.Domain01 import get_domain01
 
 
-@unittest.skip("Update for #92: account for the removed files")
 class TestBottomUpExpectedValues(tf.test.TestCase):
 	def setUp(self):
-		# TODO: Update for #92: account for the removed `get_expected_values()`
-		# self.expected_values = get_expected_values()
-		pass
+		self.domain01 = get_domain01()
 
 	def test_level_0(self):
 		expected_output = np.array(83.9625)
-
 		with self.test_session() as sess:
 			sess.run(tf.global_variables_initializer())
-
-			output = sess.run(self.expected_values[0])
-
-			self.assertNDArrayNear(output, expected_output, LARGE_ERROR_TOLERANCE)
+			tensorcfr = TensorCFR(self.domain01)
+			expected_values = sess.run(tensorcfr.get_expected_values())
+			self.assertNDArrayNear(expected_values[0], expected_output, LARGE_ERROR_TOLERANCE)
 
 	def test_level_1(self):
 		expected_output = np.array([25.45, 95.5, 154.2, 214.2, 210.45])
-
 		with self.test_session() as sess:
 			sess.run(tf.global_variables_initializer())
-
-			output = sess.run(self.expected_values[1])
-
-			self.assertNDArrayNear(output, expected_output, LARGE_ERROR_TOLERANCE)
+			tensorcfr = TensorCFR(self.domain01)
+			expected_values = sess.run(tensorcfr.get_expected_values())
+			self.assertNDArrayNear(expected_values[1], expected_output, LARGE_ERROR_TOLERANCE)
 
 	def test_level_2(self):
 		expected_output = np.array([[18.5, 33, 30],
@@ -41,13 +32,11 @@ class TestBottomUpExpectedValues(tf.test.TestCase):
 									[135, 159, 0],
 									[195, 219, 0],
 									[130, 275.5, 296]])
-
 		with self.test_session() as sess:
 			sess.run(tf.global_variables_initializer())
-
-			output = sess.run(self.expected_values[2])
-
-			self.assertNDArrayNear(output, expected_output, SMALL_ERROR_TOLERANCE)
+			tensorcfr = TensorCFR(self.domain01)
+			expected_values = sess.run(tensorcfr.get_expected_values())
+			self.assertNDArrayNear(expected_values[2], expected_output, SMALL_ERROR_TOLERANCE)
 
 	def test_level_3(self):
 		expected_output = np.array([[[10, 20],
@@ -69,10 +58,8 @@ class TestBottomUpExpectedValues(tf.test.TestCase):
 									[[0, 0],
 									 [270, 280],
 									 [290, 300]]])
-
 		with self.test_session() as sess:
 			sess.run(tf.global_variables_initializer())
-
-			output = sess.run(self.expected_values[3])
-
-			self.assertNDArrayNear(output, expected_output, SMALL_ERROR_TOLERANCE)
+			tensorcfr = TensorCFR(self.domain01)
+			expected_values = sess.run(tensorcfr.get_expected_values())
+			self.assertNDArrayNear(expected_values[3], expected_output, SMALL_ERROR_TOLERANCE)

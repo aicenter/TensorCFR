dc84d799af103a8827a037fefd0985b2d7fbfd59
Karel Ha <mathemage@gmail.com>
2018-06-20 09:24:16 +0200

    Add all files to `domains.matching_pennies.__init__.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/matching_pennies/__init__.py
+++ src/domains/matching_pennies/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
-__all__ = ["matching_pennies_as_numpy_values"]
+__all__ = ["MatchingPennies", "matching_pennies_as_numpy_values", "domain_from_gambit_loader"]

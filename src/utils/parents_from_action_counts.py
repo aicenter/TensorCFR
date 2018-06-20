3a4606931370c0e2e23842720cca78575adf948c
Karel Ha <mathemage@gmail.com>
2018-06-20 11:54:33 +0200

    Demonstrate in `parents_from_action_counts.__main__`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/utils/parents_from_action_counts.py
+++ src/utils/parents_from_action_counts.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import tensorflow as tf
 
 
@@ -27,4 +26,49 @@ def get_parents_from_action_counts(action_counts):
 
 
 if __name__ == '__main__':
-	pass
+	""" Demonstrate on `domains.hunger_games`:
+	
+	```
+	action_counts:
+	[[2],
+	 [1, 6],
+	 [4],
+	 [3, 3, 2, 2],
+	 [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]]
+	```
+	 
+	expected `parents`:
+	```
+	"hunger_games/parents_lvl0:0"
+	[0]
+
+	"hunger_games/parents_lvl1:0"
+	[0 0]
+
+	"hunger_games/parents_lvl2:0"
+	[0]
+
+	"hunger_games/parents_lvl3:0"
+	[0 0 0 0]
+
+	"hunger_games/parents_lvl4:0"
+	[0 0 0 0 0 0 0 0 0 0]
+	```
+	"""
+
+	action_counts = [
+		[2],
+		[1, 6],
+		[4],
+		[3, 3, 2, 2],
+		[2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	]
+	parents = get_parents_from_action_counts(action_counts)
+
+	from pprint import pprint
+	from src.utils.tensor_utils import print_tensors
+	print("action_counts:")
+	pprint(action_counts, indent=1, width=50)
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		print_tensors(sess, parents)

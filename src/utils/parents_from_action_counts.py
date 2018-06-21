facdf19f1c866e6cd296952aeef294f56f2af35d
Karel Ha <mathemage@gmail.com>
2018-06-21 19:34:36 +0200

    Switch to alternative computation of parents
    - via `tf.sequence_mask` and `tf.boolean_mask`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/utils/parents_from_action_counts.py
+++ src/utils/parents_from_action_counts.py
@@ -8,6 +8,20 @@ from pprint import pprint
 from src.utils.tensor_utils import print_tensors, print_tensor, scatter_nd_sum
 
 
+def get_parents_from_action_counts_alternative(action_counts):
+	levels = len(action_counts)
+	mask_children = [
+		tf.sequence_mask(
+				action_counts[level],
+				name="mask_children_lvl{}".format(level)
+		)
+		for level in range(levels)
+	]
+	with tf.Session() as tmp_sess:
+		tmp_sess.run(tf.global_variables_initializer())
+		print_tensors(tmp_sess, mask_children)
+
+
 def get_parents_from_action_counts(action_counts):
 	"""
 	Compute tensor `parents` containing the indices to each node's parent in the previous level.
@@ -69,10 +83,13 @@ if __name__ == '__main__':
 		[2] * 10,
 		[0] * 20
 	]
-	parents_ = get_parents_from_action_counts(action_counts_)
-
 	print("action_counts:")
 	pprint(action_counts_, indent=1, width=80)
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		print_tensors(sess, parents_)
+
+	get_parents_from_action_counts_alternative(action_counts_)
+
+	# parents_ = get_parents_from_action_counts(action_counts_)
+	#
+	# with tf.Session() as sess:
+	# 	sess.run(tf.global_variables_initializer())
+	# 	print_tensors(sess, parents_)

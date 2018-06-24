9078e5401002e5ea543ea8f33ed0cf8d12ffe012
Karel Ha <mathemage@gmail.com>
2018-06-24 04:44:17 +0200

    Remove (the original) `get_parents_from_action_counts()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/utils/parents_from_action_counts.py
+++ src/utils/parents_from_action_counts.py
@@ -9,6 +9,16 @@ from src.utils.tensor_utils import print_tensors, print_tensor, scatter_nd_sum
 
 
 def get_parents_from_action_counts_alternative(action_counts):
+	"""
+	Compute tensor `parents` containing the indices to each node's parent in the previous level.
+
+  Args:
+    :param action_counts: A 2-D numpy array containing number of actions of each node.
+
+  Returns:
+    A corresponding TensorFlow operation (from the computation graph) that contain the index to each node's parent
+     (in the level above).
+  """
 	levels = len(action_counts)
 	max_actions = list(map(np.amax, action_counts))
 	print("levels:")
@@ -55,52 +65,6 @@ def get_parents_from_action_counts_alternative(action_counts):
 		print_tensors(tmp_sess, mask_children)
 		print_tensors(tmp_sess, broadcast_ranges)
 		print_tensors(tmp_sess, parents)
-
-
-def get_parents_from_action_counts(action_counts):
-	"""
-	Compute tensor `parents` containing the indices to each node's parent in the previous level.
-
-  Args:
-    :param action_counts: A 2-D numpy array containing number of actions of each node.
-
-  Returns:
-    A corresponding TensorFlow operation (from the computation graph) that contain the index to each node's parent
-     (in the level above).
-  """
-
-	# the 1st size is `1` as there's only 1 root, the last `action_count` is skipped as there are only terminal nodes
-	sizes = [1] + list(map(np.sum, action_counts[:-1]))
-	print("sizes:")
-	pprint(sizes, indent=1, width=50)
-
-	leftmost_child = [    # indices of each node's leftmost child
-		tf.cast(
-				tf.cumsum(
-						action_counts[level],
-						exclusive=False,
-						name="leftmost_child_lvl{}".format(level)
-				),
-				dtype=INT_DTYPE,
-		)
-		for level in range(len(action_counts))
-	]
-
-	parents = [
-		tf.sparse_to_dense(
-				sparse_indices=tf.cast(leftmost_child[level - 1] if level > 0 else [], dtype=INT_DTYPE),
-				output_shape=[sizes[level]],
-				sparse_values=1,
-				default_value=0,
-				name="parents_lvl{}".format(level),
-		)
-		for level in range(len(sizes))
-	]
-
-	with tf.Session() as tmp_sess:
-		tmp_sess.run(tf.global_variables_initializer())
-		print_tensors(tmp_sess, leftmost_child)
-
 	return parents
 
 

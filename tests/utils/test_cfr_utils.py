28476aa2548f4fb6f18fe55201e43a0169e4c4ca
Karel Ha <mathemage@gmail.com>
2018-06-26 22:46:57 +0200

    Move to `setUp()`
    - in `test_cfr_utils.py`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/utils/test_cfr_utils.py
+++ tests/utils/test_cfr_utils.py
@@ -5,11 +5,11 @@ from src.utils.cfr_utils import get_parents_from_action_counts
 
 
 class TestCFRUtils(tf.test.TestCase):
-	def test_get_parents_from_action_counts(self):
+	def setUp(self):
 		"""
-		Test on `domains.hunger_games`:
+		Test on `domains.hunger_games`
 		"""
-		action_counts = [
+		self.action_counts = [
 			[2],
 			[1, 6],
 			[4, 0, 0, 0, 0, 0, 0],
@@ -17,7 +17,7 @@ class TestCFRUtils(tf.test.TestCase):
 			[2] * 10,
 			[0] * 20
 		]
-		expected_parents = [
+		self.expected_parents = [
 			[np.nan],
 			[0, 0],
 			[0, 1, 1, 1, 1, 1, 1],
@@ -25,9 +25,11 @@ class TestCFRUtils(tf.test.TestCase):
 			[0, 0, 0, 1, 1, 1, 2, 2, 3, 3],
 			[0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
 		]
-		parents = get_parents_from_action_counts(action_counts)
+		self.parents = get_parents_from_action_counts(self.action_counts)
+
+	def test_get_parents_from_action_counts(self):
 		with self.test_session() as sess:
 			sess.run(tf.global_variables_initializer())
-			assert (len(parents) == len(expected_parents))
-			for i in range(len(parents)):
-				tf.assert_equal(parents[i], expected_parents[i])
+			self.assertEquals(len(self.parents), len(self.expected_parents))
+			for i in range(len(self.parents)):
+				tf.assert_equal(self.parents[i], self.expected_parents[i])

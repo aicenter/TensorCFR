ae8e5ce1bf0c55becd2ebc48bc12679bac58ecd5
Karel Ha <mathemage@gmail.com>
2018-07-02 20:58:58 +0200

    Create `test_distribute_strategies_to_nodes()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/utils/test_cfr_utils.py
+++ tests/utils/test_cfr_utils.py
@@ -1,7 +1,9 @@
 import numpy as np
 import tensorflow as tf
 
-from src.utils.cfr_utils import get_parents_from_action_counts, get_node_types_from_action_counts
+from src.utils.cfr_utils import get_parents_from_action_counts, get_node_types_from_action_counts, \
+	distribute_strategies_to_nodes
+from src.utils.tensor_utils import print_tensors
 
 
 class TestCFRUtils(tf.test.TestCase):
@@ -58,3 +60,89 @@ class TestCFRUtils(tf.test.TestCase):
 			self.assertEquals(len(node_types), len(expected_node_types))
 			for i in range(len(node_types)):
 				tf.assert_equal(node_types[i], expected_node_types[i])
+
+	def test_distribute_strategies_to_nodes(self):
+		"""
+		Test on `domains.hunger_games`
+		"""
+		# taken from hunger_games.initial_infoset_strategies, see `doc/hunger_games/hunger_games_via_gambit.png`
+		infoset_strategies = [
+			tf.Variable([[0.1, 0.9]],
+			            name="infoset_uniform_strategies_lvl0"),
+			tf.Variable([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+			             [0.1, 0.1, 0.1, 0.0, 0.2, 0.5]],
+			            name="infoset_uniform_strategies_lvl1"),
+			tf.Variable([[0.1, 0.2, 0.0, 0.7]],
+			            name="infoset_uniform_strategies_lvl2"),
+			tf.Variable([[0.1, 0.0, 0.9],
+			             [0.2, 0.8, 0.0]],
+			            name="infoset_uniform_strategies_lvl3"),
+			tf.Variable([[0.1, 0.9],
+			             [0.2, 0.8],
+			             [0.3, 0.7],
+			             [0.4, 0.6],
+			             [0.5, 0.5],
+			             [0.6, 0.4],
+			             [0.7, 0.3],
+			             [0.8, 0.2],
+			             [0.9, 0.1],
+			             [1.0, 0.0]],
+			            name="infoset_uniform_strategies_lvl4")
+		]
+		node_to_infoset = [
+			tf.Variable(0,
+			            name="node_to_infoset_lvl0"),
+			tf.Variable([0, 1],
+			            name="node_to_infoset_lvl1"),
+			tf.Variable([0, 1, 1, 1, 1, 1, 1],
+			            name="node_to_infoset_lvl2"),
+			tf.Variable([0, 0, 1, 1],
+			            name="node_to_infoset_lvl3"),
+			tf.Variable([0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+			            name="node_to_infoset_lvl4")
+		]
+		nodal_strategies = [
+			distribute_strategies_to_nodes(
+					infoset_strategies[level],
+					node_to_infoset[level],
+					"nodal_strategies_lvl{}".format(level)
+			)
+			for level in range(len(infoset_strategies))
+		]
+		expected_nodal_strategies = [
+			[[0.1, 0.9]],   # level 0
+			[[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],   # level 1
+			 [0.1, 0.1, 0.1, 0.0, 0.2, 0.5]],
+			[[0.1, 0.2, 0.,  0.7],   # level 2
+			 [0.,  0.,  0.,  0., ],
+			 [0.,  0.,  0.,  0., ],
+			 [0.,  0.,  0.,  0., ],
+			 [0.,  0.,  0.,  0., ],
+			 [0.,  0.,  0.,  0., ],
+			 [0.,  0.,  0.,  0., ]],
+			[[0.1, 0.,  0.9],   # level 3
+			 [0.1, 0.,  0.9],
+			 [0.2, 0.8, 0.],
+			 [0.2, 0.8, 0.]],
+			[[0.1, 0.9],   # level 4
+			 [0.2, 0.8],
+			 [0.3, 0.7],
+			 [0.4, 0.6],
+			 [0.5, 0.5],
+			 [0.6, 0.4],
+			 [0.7, 0.3],
+			 [0.8, 0.2],
+			 [0.9, 0.1],
+			 [1.,  0.]]
+		]
+
+		# TODO here
+		# updating_player =
+		# acting_players =
+
+		with self.test_session() as sess:
+			sess.run(tf.global_variables_initializer())
+			self.assertEquals(len(nodal_strategies), len(expected_nodal_strategies))
+			for i in range(len(nodal_strategies)):
+				tf.assert_equal(nodal_strategies[i], expected_nodal_strategies[i])
+			print_tensors(sess, nodal_strategies)

c8b74064f93ccfb255c526e1cb8e69ee9f93bc0d
Karel Ha <mathemage@gmail.com>
2018-07-02 19:00:14 +0200

    Remove `node_types` from `hunger_games`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/flattened_hunger_games/flattened_hunger_games_as_numpy_values.py
+++ src/domains/flattened_hunger_games/flattened_hunger_games_as_numpy_values.py
@@ -21,21 +21,18 @@ acting_depth = len(action_counts)
 
 # allocate Python arrays
 node_to_infoset: List[int] = [None] * acting_depth
-node_types: List[int] = [None] * levels
 utilities: List[int] = [None] * levels
 infoset_acting_players: List[int] = [None] * acting_depth
 initial_infoset_strategies: List[int] = [None] * acting_depth
 
 ########## Level 0 ##########
 node_to_infoset[0] = 0
-node_types[0] = INNER_NODE
 utilities[0] = NON_TERMINAL_UTILITY
 infoset_acting_players[0] = [PLAYER1]
 initial_infoset_strategies[0] = [[0.1, 0.9]]
 
 ########## Level 1 ##########
 node_to_infoset[1] = [0, 1]
-node_types[1] = [INNER_NODE] * 2
 utilities[1] = [NON_TERMINAL_UTILITY] * 2
 infoset_acting_players[1] = [PLAYER2, PLAYER2]
 initial_infoset_strategies[1] = [
@@ -46,10 +43,6 @@ initial_infoset_strategies[1] = [
 
 ########## Level 2 ##########
 node_to_infoset[2] = [0, 1, 1, 1, 1, 1, 1]    # `1` for the infoset of terminal nodes
-node_types[2] = [
-	[INNER_NODE, IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-	[TERMINAL_NODE, TERMINAL_NODE, TERMINAL_NODE, TERMINAL_NODE, TERMINAL_NODE, TERMINAL_NODE]
-]
 utilities[2] = [NON_TERMINAL_UTILITY, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]
 infoset_acting_players[2] = [PLAYER1]
 initial_infoset_strategies[2] = [
@@ -58,24 +51,6 @@ initial_infoset_strategies[2] = [
 
 ########## Level 3 ##########
 node_to_infoset[3] = [0, 0, 1, 1]
-node_types[3] = [
-	[
-		[TERMINAL_NODE] * 4,
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4
-	],
-	[
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4,
-		[IMAGINARY_NODE] * 4
-	]
-]
 utilities[3] = [NON_TERMINAL_UTILITY] * 4
 infoset_acting_players[3] = [PLAYER2, PLAYER2]
 initial_infoset_strategies[3] = [
@@ -85,84 +60,6 @@ initial_infoset_strategies[3] = [
 
 ########## Level 4 ##########
 node_to_infoset[4] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-node_types[4] = [
-	[
-		[
-			[INNER_NODE, INNER_NODE,     INNER_NODE],
-			[INNER_NODE, INNER_NODE,     INNER_NODE],
-			[INNER_NODE, INNER_NODE, IMAGINARY_NODE],
-			[INNER_NODE, INNER_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		]
-	],
-	[
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		],
-		[
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-			[IMAGINARY_NODE, IMAGINARY_NODE, IMAGINARY_NODE],
-		]
-	]
-]
 utilities[4] = [NON_TERMINAL_UTILITY] * 10,
 infoset_acting_players[4] = [
 	PLAYER1,
@@ -191,274 +88,4 @@ initial_infoset_strategies[4] = [
 
 ########## Level 5 ##########
 # There are never any infosets in the final layer, only terminal / imaginary nodes.
-node_types[5] = [
-	[
-		[
-			[
-				[INNER_NODE] * 2,
-				[INNER_NODE] * 2,
-				[INNER_NODE] * 2
-			],
-			[
-				[INNER_NODE] * 2,
-				[INNER_NODE] * 2,
-				[INNER_NODE] * 2
-			],
-			[
-				[INNER_NODE] * 2,
-				[INNER_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[INNER_NODE] * 2,
-				[INNER_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-	],
-	[
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-		[
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-			[
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2,
-				[IMAGINARY_NODE] * 2
-			],
-		],
-	],
-]
 utilities[5] = [1.0] * 20

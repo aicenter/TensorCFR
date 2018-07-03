b41a75ad6c108db89c32f35596d57e8c8fa8cb1b
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-07-03 17:58:27 +0200

    Use better names for variables
    Signed-off-by: janrudolf <jan.rudolf.steeldog@gmail.com>

--- src/utils/gambit/gambit_loader.py
+++ src/utils/gambit/gambit_loader.py
@@ -26,51 +26,55 @@ class InformationSetManager:
 			self.__terminal_node_information_set_index = None
 
 		self.information_sets = {}
-		self.infoset_acting_players_list = []
+		self.infotmation_set_acting_players_list = []
 
 	def add_node(self, node):
 		if node.type == constants.GAMBIT_NODE_TYPE_TERMINAL:
 			return self.__terminal_node_information_set_index
 
 		if node.information_set_id not in self.information_sets:
-			infoset_index = len(self.information_sets)
-			self.information_sets[node.information_set_id] = [infoset_index, node.type, node.tensorcfr_id,
-													 node, len(node.actions)] # TODO upravit
-			self.infoset_acting_players_list.insert(0, node.information_set_id)
-			return infoset_index
+			information_set_index = len(self.information_sets)
+			# self.information_sets[node.information_set_id] = [infoset_index, node.type, node.tensorcfr_id,
+			# 										 node, len(node.actions)] # TODO upravit
+			self.information_sets[node.information_set_id] = [information_set_index, node]
+			self.infotmation_set_acting_players_list.insert(0, node.information_set_id)
+			return information_set_index
 		else:
 			return self.information_sets[node.information_set_id][0]
 
 	def get_tensors(self, next_level_max_no_actions):
-		infoset_acting_players = []
-		initial_infoset_strategies = []
+		information_set_acting_players = []
+		initial_information_set_strategies = []
 
-		for infoset_id in reversed(self.infoset_acting_players_list):
-			infoset_acting_players.insert(0, self.information_sets[infoset_id][2])
+		for information_set_id in reversed(self.infotmation_set_acting_players_list):
+			node = self.information_sets[information_set_id][1]
 
-			if self.information_sets[infoset_id][1] == constants.GAMBIT_NODE_TYPE_PLAYER:
+			information_set_acting_players.insert(0, node.tensorcfr_id)
+
+			if node.is_player():
 				current_infoset_strategy = [np.nan] * next_level_max_no_actions
 
-				action = [float(1 / (self.information_sets[infoset_id][4]))] * len(self.information_sets[infoset_id][3].actions)
+				actions = [float(1 / len(node.actions))] * len(node.actions)
 
-				for index, action in enumerate(action):
+				for index, action in enumerate(actions):
 					current_infoset_strategy[index] = action
 
-				initial_infoset_strategies.append(current_infoset_strategy)
-			elif self.information_sets[infoset_id][1] == constants.GAMBIT_NODE_TYPE_CHANCE:
+				initial_information_set_strategies.append(current_infoset_strategy)
+			elif node.is_chance():
 				initial_infoset_strategy = [np.nan] * next_level_max_no_actions
 
-				for index, action in enumerate(reversed(self.information_sets[infoset_id][3].actions)):
+				for index, action in enumerate(reversed(node.actions)):
 					initial_infoset_strategy[index] = action['probability']
 
-				initial_infoset_strategies.append(initial_infoset_strategy)
+				initial_information_set_strategies.append(initial_infoset_strategy)
 
 		if self.is_terminal_node_present:
-			initial_infoset_strategies.append([0] * next_level_max_no_actions)
+			# if is a terminal node present in information set, add null strategy
+			initial_information_set_strategies.append([0] * next_level_max_no_actions)
 
 		return [
-			np.asarray(infoset_acting_players, dtype=constants.INT_DTYPE_NUMPY),
-			np.asarray(initial_infoset_strategies)
+			np.asarray(information_set_acting_players, dtype=constants.INT_DTYPE_NUMPY),
+			np.asarray(initial_information_set_strategies)
 		]
 
 
@@ -94,12 +98,7 @@ class GambitLoader:
 		self.nodes_per_levels = [1]  # level 0 has always one node
 		self.number_of_levels = 0
 
-
-
-		# game_header = Parser.parse_header(f.readline())
-		#
-		# self.domain_name = game_header['name']
-
+		# load meta information - number of levels, max actions per level...
 		self.__load_meta_information(file)
 
 		self.number_of_levels = len(self.nodes_per_levels)
@@ -212,6 +211,7 @@ class GambitLoader:
 			[infoset_acting_players, initial_infoset_strategy] = self.__infoset_managers[level].get_tensors(max_number_of_actions)
 			self.infoset_acting_players[level] = infoset_acting_players
 			self.initial_infoset_strategies[level] = initial_infoset_strategy
+
 
 if __name__ == '__main__':
 	domain01_efg = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..', 'doc', 'domain01_via_gambit.efg')

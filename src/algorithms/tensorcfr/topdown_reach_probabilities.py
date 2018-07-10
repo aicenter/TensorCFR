3ce0f21ddb7a2ac30243abc5d1a385d9338891cb
Karel Ha <mathemage@gmail.com>
2018-07-10 15:33:50 +0200

    Extract `domains`
    - in (the original) `topdown_reach_probabilities.py`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/topdown_reach_probabilities.py
+++ src/algorithms/tensorcfr/topdown_reach_probabilities.py
@@ -1,14 +1,20 @@
+#!/usr/bin/env python3
+
 import tensorflow as tf
 
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR
 from src.domains.available_domains import get_domain_by_name
 
 if __name__ == '__main__':
-	domain01 = get_domain_by_name("domain01")
-	matching_pennies = get_domain_by_name("matching_pennies")
+	domains = [
+		get_domain_by_name("domain01"),
+		get_domain_by_name("matching_pennies"),
+		get_domain_by_name("hunger_games")
+	]
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:
+		for tensorcfr in map(TensorCFR, domains):
+			print(">>>>>>>>>> {} <<<<<<<<<<".format(tensorcfr.domain.domain_name))
 			# TODO extract following lines to a UnitTest
 			tensorcfr.show_reach_probabilities(sess)
 			print("-----------Swap players-----------\n")

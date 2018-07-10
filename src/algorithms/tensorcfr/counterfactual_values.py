025398d85cb386c8ec353f83ed0f11df0785b7bb
Karel Ha <mathemage@gmail.com>
2018-07-10 18:49:31 +0200

    Extract `domains`
    - in (the original) `counterfactual_values.py`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/counterfactual_values.py
+++ src/algorithms/tensorcfr/counterfactual_values.py
@@ -6,13 +6,15 @@ from src.algorithms.tensorcfr.TensorCFR import TensorCFR
 from src.domains.available_domains import get_domain_by_name
 from src.utils.tensor_utils import print_tensors
 
-
 if __name__ == '__main__':
-	domain01 = get_domain_by_name("domain01")
-	matching_pennies = get_domain_by_name("matching_pennies")
+	domains = [
+		get_domain_by_name("domain01"),
+		get_domain_by_name("matching_pennies"),
+		get_domain_by_name("hunger_games")
+	]
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:
+		for tensorcfr in map(TensorCFR, domains):
 			print(">>>>>>>>>> {} <<<<<<<<<<".format(tensorcfr.domain.domain_name))
 			nodal_reach_probabilities = tensorcfr.get_nodal_reach_probabilities()
 			expected_values = tensorcfr.get_expected_values()

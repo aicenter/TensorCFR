b215310e9a9970130cea47c81b07499efef9c3f7
Karel Ha <mathemage@gmail.com>
2018-07-12 23:31:17 +0200

    Move the 2 methods from `flattened_to_2D.py` to `cfr_utils.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/utils/cfr_utils.py
+++ src/utils/cfr_utils.py
@@ -4,8 +4,8 @@ from pprint import pprint
 import numpy as np
 import tensorflow as tf
 
-from src.commons.constants import INT_DTYPE, TERMINAL_NODE, INNER_NODE, REACH_PROBABILITY_OF_ROOT
-from src.utils.tensor_utils import print_tensors
+from src.commons.constants import INT_DTYPE, TERMINAL_NODE, INNER_NODE, REACH_PROBABILITY_OF_ROOT, FLOAT_DTYPE
+from src.utils.tensor_utils import print_tensors, scatter_nd_sum
 
 
 def distribute_strategies_to_nodes(infoset_strategies, node_to_infoset, name, updating_player=None,
@@ -144,3 +144,80 @@ if __name__ == '__main__':
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		print_tensors(sess, parents_)
+
+
+def get_parent_x_actions_from_action_counts(action_counts, values_in_children, name="reshape_CFVs"):
+	"""
+  Reshape data related to children (e.g., CFVs) to a 2D tensor of shape (parent x action).
+
+  Args:
+    :param action_counts: A 1-D array containing number of actions of each node.
+    :param values_in_children: Data for the children to reshape.
+		:param name: A string to name the resulting tensor operation.
+
+  Returns: A corresponding TensorFlow operation (from the computation graph) that computes the (parent x action)
+  tensor with the provided data distributed to the correct positions.
+	"""
+	mask_children = tf.sequence_mask(
+			action_counts,
+			dtype=INT_DTYPE,
+			name="initial_mask_in_{}".format(name)
+	)
+	first_column = tf.expand_dims(
+			tf.cumsum(action_counts,
+			          exclusive=True,
+			          name="first_column_indices_in_{}".format(name)),
+			dim=1
+	)
+	mask_with_replaced_first_column = tf.concat(
+			[first_column, mask_children[:, 1:]], 1,
+			name="replacing_col0_in_{}".format(name)
+	)
+	indices_2D_into_1D = tf.expand_dims(
+			tf.cumsum(
+					mask_with_replaced_first_column,
+					axis=1
+			),
+			dim=2,
+			name="computing_indices_in_{}".format(name)
+	)
+	final = tf.multiply(
+			tf.cast(
+					mask_children,
+					dtype=FLOAT_DTYPE
+			),
+			tf.gather_nd(
+					values_in_children,
+					indices_2D_into_1D
+			),
+			name="cleaned_up_result_in_{}".format(name)
+	)
+	return final
+
+
+def get_action_and_IS_cfvs(children_values, action_counts, parent_IS_map, strategy):
+	"""
+  Compute counterfactual values of actions and information sets for one level.
+
+  Args:
+    :param children_values: A 1-D tensor of counterfactual values for the children.
+    :param action_counts: A 1-D array containing number of actions of each node.
+    :param parent_IS_map: A 1-D array indicating the index if the IS for each parent of the children.
+    :param strategy: A 2-D representation of probability of playing an action in an IS.
+
+  Returns: A pair of counterfactual values for all actions in ISs (2-D) and the cfvs of ISs expanded to (2-D) to be
+  able to subtract for the first return value.
+	"""
+	parent_x_action = get_parent_x_actions_from_action_counts(action_counts, children_values)
+	cfvs_IS_action = scatter_nd_sum(
+			tf.expand_dims(parent_IS_map, axis=1),
+			parent_x_action,
+			tf.shape(strategy),
+			name="sums_cfvs_over_IS"
+	)
+	cfvs_IS = tf.reduce_sum(
+			tf.multiply(cfvs_IS_action, strategy),
+			axis=1,
+			name="IS_cfvs_from_action_cfvs"
+	)
+	return cfvs_IS_action, tf.expand_dims(cfvs_IS, dim=1)

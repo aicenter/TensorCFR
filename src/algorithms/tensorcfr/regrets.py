e2c460ed9c58dc9ed49480d60bc6951e111e8ab2
Karel Ha <mathemage@gmail.com>
2018-07-15 15:37:15 +0200

    Extract `domains`
    - in `algorithms.tensorcfr.regrets`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/regrets.py
+++ src/algorithms/tensorcfr/regrets.py
@@ -7,9 +7,12 @@ from src.domains.available_domains import get_domain_by_name
 from src.utils.tensor_utils import print_tensors
 
 if __name__ == '__main__':
-	domain01 = get_domain_by_name("domain01")
-	matching_pennies = get_domain_by_name("matching_pennies")
-	for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:
+	domains = [
+		get_domain_by_name("domain01"),
+		get_domain_by_name("matching_pennies"),
+		get_domain_by_name("hunger_games")
+	]
+	for tensorcfr in map(TensorCFR, domains):
 		print(">>>>>>>>>> {} <<<<<<<<<<".format(tensorcfr.domain.domain_name))
 		infoset_cf_values, infoset_cf_values_per_actions = tensorcfr.get_infoset_cf_values()
 		regrets = tensorcfr.get_regrets()

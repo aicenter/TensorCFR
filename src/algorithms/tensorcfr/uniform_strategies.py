6581c278deabdede6fb3f3ce5cc391c38a3139d5
Karel Ha <mathemage@gmail.com>
2018-07-16 15:15:18 +0200

    Extract `domains`
    - in `algorithms.tensorcfr.uniform_strategies`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr/uniform_strategies.py
+++ src/algorithms/tensorcfr/uniform_strategies.py
@@ -5,9 +5,12 @@ from src.domains.available_domains import get_domain_by_name
 from src.utils.tensor_utils import print_tensors
 
 if __name__ == '__main__':
-	domain01 = get_domain_by_name("domain01")
-	matching_pennies = get_domain_by_name("matching_pennies")
-	for tensorcfr in [TensorCFR(domain01), TensorCFR(matching_pennies)]:
+	domains = [
+		get_domain_by_name("domain01"),
+		get_domain_by_name("matching_pennies"),
+		get_domain_by_name("hunger_games")
+	]
+	for tensorcfr in map(TensorCFR, domains):
 		print(">>>>>>>>>> {} <<<<<<<<<<".format(tensorcfr.domain.domain_name))
 		infoset_uniform_strategies = tensorcfr.get_infoset_uniform_strategies()
 		infoset_children_types = tensorcfr.get_infoset_children_types()

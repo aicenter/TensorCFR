00cdec592dbeea68714b120b4c646b2e6baab890
Karel Ha <mathemage@gmail.com>
2018-07-18 12:01:32 +0200

    Rename `flattened_hunger_games_as_numpy_values_2.py`
    - to `flattened_hunger_games_2_as_numpy_values.py`
    - in `domains`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/flattened_hunger_games_2/__init__.py
+++ src/domains/flattened_hunger_games_2/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
-__all__ = ["flattened_hunger_games_as_numpy_values_2", "FlattenedHungerGames_2"]
+__all__ = ["flattened_hunger_games_2_as_numpy_values", "FlattenedHungerGames_2"]

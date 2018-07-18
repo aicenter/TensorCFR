f28facfa4b478c3a60dc395894c7a456878692dd
Karel Ha <mathemage@gmail.com>
2018-07-18 12:03:20 +0200

    Update `FlattenedHungerGames_2.py`
    - for `flattened_hunger_games_as_numpy_values_2`
    - in `domains`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/flattened_hunger_games_2/FlattenedHungerGames_2.py
+++ src/domains/flattened_hunger_games_2/FlattenedHungerGames_2.py
@@ -2,25 +2,25 @@
 
 import tensorflow as tf
 
-import src.domains.flattened_hunger_games.flattened_hunger_games_as_numpy_values as fhg
+import src.domains.flattened_hunger_games_2.flattened_hunger_games_2_as_numpy_values as fhg_2
 from src.domains.FlattenedDomain import FlattenedDomain
 
 
 # flattened domain `hunger_games`: see `doc/hunger_games_via_drawing.png` and `doc/hunger_games_2/`
 
-def get_flattened_domain_hunger_games():
+def get_flattened_domain_hunger_games_2():
 	return FlattenedDomain(
-			domain_name="flattened_hunger_games",
-			action_counts=fhg.action_counts,
-			node_to_infoset=fhg.node_to_infoset,
-			utilities=fhg.utilities,
-			infoset_acting_players=fhg.infoset_acting_players,
-			initial_infoset_strategies=fhg.initial_infoset_strategies,
+			domain_name="flattened_hunger_games_2",
+			action_counts=fhg_2.action_counts,
+			node_to_infoset=fhg_2.node_to_infoset,
+			utilities=fhg_2.utilities,
+			infoset_acting_players=fhg_2.infoset_acting_players,
+			initial_infoset_strategies=fhg_2.initial_infoset_strategies,
 	)
 
 
 if __name__ == '__main__':
-	flattened_hunger_games = get_flattened_domain_hunger_games()
+	flattened_hunger_games_2 = get_flattened_domain_hunger_games_2()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		flattened_hunger_games.print_domain(sess)
+		flattened_hunger_games_2.print_domain(sess)

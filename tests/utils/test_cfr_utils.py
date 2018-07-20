a35f5b58793216b6c7d666bb212b6931705739d2
Karel Ha <mathemage@gmail.com>
2018-07-20 11:27:55 +0200

    Create `test_distribute_strategies_to_inner_nodes`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/utils/test_cfr_utils.py
+++ tests/utils/test_cfr_utils.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 from src.commons.constants import INFOSET_FOR_TERMINAL_NODES
 from src.utils.cfr_utils import get_parents_from_action_counts, get_node_types_from_action_counts, \
-	distribute_strategies_to_nodes
+	distribute_strategies_to_nodes, distribute_strategies_to_inner_nodes
 from src.utils.tensor_utils import print_tensors
 
 
@@ -168,6 +168,81 @@ class TestCFRUtils(tf.test.TestCase):
 					mask_of_inner_nodes[level],
 					self.node_to_infoset[level],
 					inner_node_to_infoset[level],
+					nodal_strategy
+				])
+				print("\nexpected_nodal_strategies[{}]:".format(level))
+				pprint(expected_nodal_strategies[level], width=50)
+				np.testing.assert_array_almost_equal(
+					sess.run(nodal_strategy),
+					expected_nodal_strategies[level],
+					err_msg="Nodal strategies differ at level {}!".format(level)
+				)
+
+	def test_distribute_strategies_to_inner_nodes(self):
+		"""
+		Test on `domains.hunger_games`
+
+		The test `test_distribute_strategies_to_nodes` failed work on the CPU version of TensorFlow due to different
+		 behavior of `tf.gather` on GPU and CPU:
+
+		> Note that on CPU, if an out of bound index is found, an error is returned. On GPU, if an out of bound index is
+		 found, a 0 is stored in the corresponding output value.
+
+		(quoted from https://www.tensorflow.org/api_docs/python/tf/gather)
+		"""
+		mask_of_inner_nodes = [
+			tf.greater(
+				action_count,
+				0,
+				name="mask_of_inner_nodes_lvl{}".format(level)
+			)
+			for level, action_count in enumerate(self.action_counts)
+		]
+		nodal_strategies = [
+			distribute_strategies_to_inner_nodes(
+				self.infoset_strategies[level],
+				self.node_to_infoset[level],
+				mask_of_inner_nodes[level],
+				"nodal_strategies_lvl{}".format(level)
+			)
+			for level in range(len(self.infoset_strategies))
+		]
+		expected_nodal_strategies = [
+			[[0.1, 0.9]],   # level 0
+			[[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],   # level 1
+			 [0.1, 0.1, 0.1, 0.0, 0.2, 0.5]],
+			[[0.1, 0.2, 0.,  0.7]],   # level 2
+			[[0.1, 0.,  0.9],   # level 3
+			 [0.1, 0.,  0.9],
+			 [0.2, 0.8, 0.],
+			 [0.2, 0.8, 0.]],
+			[[0.1, 0.9],   # level 4
+			 [0.2, 0.8],
+			 [0.3, 0.7],
+			 [0.4, 0.6],
+			 [0.5, 0.5],
+			 [0.6, 0.4],
+			 [0.7, 0.3],
+			 [0.8, 0.2],
+			 [0.9, 0.1],
+			 [1.,  0.]]
+		]
+
+		# TODO here
+		# updating_player =
+		# acting_players =
+
+		with self.test_session(
+			# config=tf.ConfigProto(device_count={'GPU': 0})  # uncomment to test on CPUs
+		) as sess:
+			sess.run(tf.global_variables_initializer())
+			self.assertEquals(len(nodal_strategies), len(expected_nodal_strategies))
+			for level, nodal_strategy in enumerate(nodal_strategies):
+				print("\n>>>>>>>>>>>>>>>>>>Level {}<<<<<<<<<<<<<<<<<<".format(level))
+				print_tensors(sess, [
+					self.infoset_strategies[level],
+					mask_of_inner_nodes[level],
+					self.node_to_infoset[level],
 					nodal_strategy
 				])
 				print("\nexpected_nodal_strategies[{}]:".format(level))

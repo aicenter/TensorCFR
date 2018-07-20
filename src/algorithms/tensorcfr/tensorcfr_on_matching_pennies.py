4c5b3ff65b14d59519e5417f2a0709404346be95
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-07-20 12:03:04 +0200

    Add generating .json with average strategies in Matching pennies for BR in GTLibrary
    Signed-off-by: janrudolf <jan.rudolf.steeldog@gmail.com>

--- src/algorithms/tensorcfr/tensorcfr_on_matching_pennies.py
+++ src/algorithms/tensorcfr/tensorcfr_on_matching_pennies.py
@@ -1,14 +1,23 @@
+import os
+
+from src import utils
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR, get_cfr_strategies
+from src.commons.constants import PROJECT_ROOT
 from src.domains.available_domains import get_domain_by_name
 
 if __name__ == '__main__':
 	# domain = get_domain_by_name("matching_pennies")
 	domain = get_domain_by_name("matching_pennies_via_gambit")
 	tensorcfr = TensorCFR(domain)
-	get_cfr_strategies(
+	average_strategies = get_cfr_strategies(
 			total_steps=10,
 			tensorcfr_instance=tensorcfr,
 			quiet=True,
 			# profiling=True,
 			delay=0
 	)
+
+	utils.gtlibrary.export_average_strategies_to_json(
+		domain.information_set_mapping_to_gtlibrary,
+		average_strategies,
+		os.path.join(PROJECT_ROOT, 'matching_pennies_average_strategy.json'))

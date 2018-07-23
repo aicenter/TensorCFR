8c7b24f0864dc447a98b2300bb70ec66df3b0263
Karel Ha <mathemage@gmail.com>
2018-07-23 15:09:38 +0200

    Remove commented code
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/utils/gambit_flattened_domains/loader.py
+++ src/utils/gambit_flattened_domains/loader.py
@@ -5,7 +5,6 @@ import os
 import numpy as np
 
 from src.commons import constants as common_constants
-
 from ..gambit_flattened_domains import constants
 from ..gambit_flattened_domains.parser import Parser
 
@@ -20,19 +19,11 @@ class InformationSetManager:
 	def __init__(self, level, number_of_information_sets, is_terminal_node_present=False):
 		self.level = level
 		self.number_of_information_sets = number_of_information_sets
-		# self.is_terminal_node_present = is_terminal_node_present
-		#
-		# if self.is_terminal_node_present:
-		# 	self.__terminal_node_information_set_index = self.number_of_information_sets
-		# else:
-		# 	self.__terminal_node_information_set_index = None
-
 		self.information_sets = {}
 		self.information_set_acting_players_list = []
 
 	def add_node(self, node):
 		if node.type == constants.TERMINAL_NODE:
-			# return self.__terminal_node_information_set_index
 			return common_constants.INFOSET_FOR_TERMINAL_NODES
 
 		if node.information_set_id not in self.information_sets:
@@ -68,11 +59,6 @@ class InformationSetManager:
 					initial_infoset_strategy[index] = action['probability']
 
 				initial_information_set_strategies.append(initial_infoset_strategy)
-
-		# if self.is_terminal_node_present:
-		# 	# if is a terminal node present in information set, add null strategy
-		# 	initial_information_set_strategies.append([0] * next_level_max_no_actions)
-
 		return [
 			np.asarray(information_set_acting_players, dtype=common_constants.INT_DTYPE_NUMPY),
 			np.asarray(initial_information_set_strategies)

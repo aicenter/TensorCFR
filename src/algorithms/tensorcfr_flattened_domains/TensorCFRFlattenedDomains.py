3234fb53c83a499a6b145667256f9a4ba441dcf2
Karel Ha <mathemage@gmail.com>
2018-07-25 19:01:26 +0200

    Add `flattened_domain01_via_gambit` to `__main__`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_flattened_domains/TensorCFRFlattenedDomains.py
+++ src/algorithms/tensorcfr_flattened_domains/TensorCFRFlattenedDomains.py
@@ -742,6 +742,7 @@ if __name__ == '__main__':
 	# domain = get_domain_by_name("invalid domain name test")
 	# domain = get_domain_by_name("flattened_hunger_games")
 	domain = get_domain_by_name("flattened_hunger_games_2")
+	# domain = get_domain_by_name("flattened_domain01_via_gambit")    # TODO fix for wrong IS 6, 7 (lvl last)
 	tensorcfr = TensorCFRFlattenedDomains(domain)
 
 	# infoset_action_cf_values_, infoset_cf_values_ = tensorcfr.get_infoset_cf_values()

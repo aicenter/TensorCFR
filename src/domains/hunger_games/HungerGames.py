758da81eb3acb6351bd6f39aa0bc4d93a05b93f7
Karel Ha <mathemage@gmail.com>
2018-07-26 14:23:27 +0200

    Indent with 2 spaces
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/hunger_games/HungerGames.py
+++ src/domains/hunger_games/HungerGames.py
@@ -5,18 +5,19 @@ import tensorflow as tf
 import src.domains.hunger_games.hunger_games_as_numpy_values as hg
 from src.domains.Domain import Domain
 
+
 # domain `hunger_games`: see doc/hunger_games_via_drawing.png
 
 
 def get_domain_hunger_games():
 	return Domain(
-			domain_name="hunger_games",
-			actions_per_levels=hg.actions_per_levels,
-			node_to_infoset=hg.node_to_infoset,
-			node_types=hg.node_types,
-			utilities=hg.utilities,
-			infoset_acting_players=hg.infoset_acting_players,
-			initial_infoset_strategies=hg.initial_infoset_strategies,
+		domain_name="hunger_games",
+		actions_per_levels=hg.actions_per_levels,
+		node_to_infoset=hg.node_to_infoset,
+		node_types=hg.node_types,
+		utilities=hg.utilities,
+		infoset_acting_players=hg.infoset_acting_players,
+		initial_infoset_strategies=hg.initial_infoset_strategies,
 	)
 
 

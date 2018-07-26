9ccc78e48780ef702dfcdc30cb2683a422d110d8
Karel Ha <mathemage@gmail.com>
2018-07-26 16:46:54 +0200

    Rename `domain_name` to `II-GS2_gambit_flattened`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/flattened_goofspiel2/domain_from_gambit_loader.py
+++ src/domains/flattened_goofspiel2/domain_from_gambit_loader.py
@@ -18,7 +18,7 @@ def get_flattened_goofspiel2():
 			'goofspiel',
 			'II-GS2.gbt'
 	)
-	return FlattenedDomain.init_from_gambit_file(path_to_domain_filename, domain_name="II-GS2_via_gambit")
+	return FlattenedDomain.init_from_gambit_file(path_to_domain_filename, domain_name="II-GS2_gambit_flattened")
 
 
 if __name__ == '__main__':

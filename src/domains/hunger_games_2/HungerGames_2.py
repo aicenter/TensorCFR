826cfcd14b4e1c9195b4d5b8d5e683c87b3a21fa
Karel Ha <mathemage@gmail.com>
2018-07-26 11:53:43 +0200

    Copy `hunger_games_2` (dense) from `hunger_games`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/domains/hunger_games_2/HungerGames_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+
+import src.domains.hunger_games.hunger_games_as_numpy_values as hg
+from src.domains.Domain import Domain
+
+
+# domain `hunger_games`: see doc/hunger_games_via_drawing.png
+
+
+def get_domain_hunger_games():
+	return Domain(
+			domain_name="hunger_games",
+			actions_per_levels=hg.actions_per_levels,
+			node_to_infoset=hg.node_to_infoset,
+			node_types=hg.node_types,
+			utilities=hg.utilities,
+			infoset_acting_players=hg.infoset_acting_players,
+			initial_infoset_strategies=hg.initial_infoset_strategies,
+	)
+
+
+if __name__ == '__main__':
+	hunger_games = get_domain_hunger_games()
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		hunger_games.print_domain(sess)
---
+++ src/domains/hunger_games_2/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+# https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
+__all__ = ["hunger_games_2_as_numpy_values", "HungerGames_2"]

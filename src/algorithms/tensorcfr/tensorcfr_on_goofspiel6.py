0efb1e7e292337debb26baebf778cf235b46571c
Karel Ha <mathemage@gmail.com>
2018-07-29 01:28:57 +0200

    Run (dense) TensorCFR on II-GS6
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/algorithms/tensorcfr/tensorcfr_on_goofspiel6.py
@@ -0,0 +1,17 @@
+from src.algorithms.tensorcfr.TensorCFR import TensorCFR, run_cfr
+from src.domains.available_domains import get_domain_by_name
+
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
+ACTIVATE_FILE = False
+
+
+if __name__ == '__main__' and ACTIVATE_FILE:
+	domain = get_domain_by_name("IIGS6_s1_bf_ft_via_gambit")
+	tensorcfr = TensorCFR(domain)
+	run_cfr(
+			total_steps=10,
+			tensorcfr_instance=tensorcfr,
+			quiet=True,
+			# profiling=True,
+			delay=0
+	)   # TODO verify the results (final average strategies) via `gtlibrary`

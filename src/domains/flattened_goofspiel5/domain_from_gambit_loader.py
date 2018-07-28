e22d23d47613aaa570eeef7dfb2a3e03ba264df3
Karel Ha <mathemage@gmail.com>
2018-07-28 12:48:19 +0200

    Define `domains.flattened_goofspiel5`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/domains/flattened_goofspiel5/domain_from_gambit_loader.py
@@ -0,0 +1,28 @@
+import os
+
+import tensorflow as tf
+
+from src.domains.FlattenedDomain import FlattenedDomain
+
+
+def get_flattened_goofspiel5():
+	path_to_domain_filename = os.path.join(
+			os.path.dirname(
+					os.path.abspath(
+							__file__)
+			),
+			'..',
+			'..',
+			'..',
+			'doc',
+			'goofspiel',
+			'IIGS5_s1_bf_ft.gbt'
+	)
+	return FlattenedDomain.init_from_gambit_file(path_to_domain_filename, domain_name="IIGS5_s1_bf_ft_gambit_flattened")
+
+
+if __name__ == '__main__':
+	goofspiel5 = get_flattened_goofspiel5()
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		goofspiel5.print_domain(sess)

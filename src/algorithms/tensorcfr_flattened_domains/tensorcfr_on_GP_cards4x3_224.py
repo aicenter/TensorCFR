779422a23e276cccc7d4d954fc1b6310e26480ae
Karel Ha <mathemage@gmail.com>
2018-07-29 19:24:53 +0200

    Run flattened TensorCFR on GP_4x3_224
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/algorithms/tensorcfr_flattened_domains/tensorcfr_on_GP_cards4x3_224.py
@@ -0,0 +1,14 @@
+from src.algorithms.tensorcfr_flattened_domains.TensorCFRFlattenedDomains import TensorCFRFlattenedDomains, run_cfr
+from src.domains.available_domains import get_domain_by_name
+
+
+if __name__ == '__main__':
+	domain = get_domain_by_name("GP_cards4x3_224_gambit_flattened")
+	tensorcfr = TensorCFRFlattenedDomains(domain)
+	run_cfr(
+			total_steps=10,
+			tensorcfr_instance=tensorcfr,
+			quiet=True,
+			# profiling=True,
+			delay=0
+	)   # TODO verify the results (final average strategies) via `gtlibrary`

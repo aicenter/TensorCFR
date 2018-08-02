fbbe4e4278b65ceda13be1c6aa691a456842e623
Karel Ha <mathemage@gmail.com>
2018-08-02 14:43:45 +0200

    Replace TODO with section description
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_fixed_trunk_strategies/TensorCFRFixedTrunkStrategies.py
+++ src/algorithms/tensorcfr_fixed_trunk_strategies/TensorCFRFixedTrunkStrategies.py
@@ -735,7 +735,7 @@ def log_after_all_steps(tensorcfr_instance, session, average_infoset_strategies,
 		print("___________________________________\n")
 		print_tensors(session, tensorcfr_instance.domain.current_infoset_strategies)
 
-		# TODO remove this section
+		# compute trunk
 		trunk_depth_infoset_cfvs = {}
 		for player in [PLAYER1, PLAYER2]:
 			print("___________________________________\n")

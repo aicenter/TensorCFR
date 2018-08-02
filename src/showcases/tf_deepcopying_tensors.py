6738469ee7ba76de9fb9471acc8e305b343ed428
Karel Ha <mathemage@gmail.com>
2018-08-02 11:32:52 +0200

    Showcase (deep)copying variables in TensorFlow
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/showcases/tf_deepcopying_tensors.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# from https://stackoverflow.com/questions/33717772/how-can-i-copy-a-variable-in-tensorflow
+import tensorflow as tf
+
+from src.utils.tensor_utils import print_tensor
+
+var = tf.Variable(0.9)
+var2 = tf.Variable(0.0)
+deepcopy_first_variable = var2.assign(var)
+
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	print_tensor(sess, var2)
+	print_tensor(sess, var2)
+	sess.run(deepcopy_first_variable)
+	print_tensor(sess, var2)
+	print_tensor(sess, var2)

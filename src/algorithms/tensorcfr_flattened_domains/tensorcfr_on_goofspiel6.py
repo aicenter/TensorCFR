d7400de0d2b63ccef2be386bfec025546dd2eded
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-08-12 20:16:15 +0200

    Add registering average strategies for IIGS6
    Signed-off-by: janrudolf <jan.rudolf.steeldog@gmail.com>

--- src/algorithms/tensorcfr_flattened_domains/tensorcfr_on_goofspiel6.py
+++ src/algorithms/tensorcfr_flattened_domains/tensorcfr_on_goofspiel6.py
@@ -1,3 +1,4 @@
+from src import utils
 from src.algorithms.tensorcfr_flattened_domains.TensorCFRFlattenedDomains import TensorCFRFlattenedDomains, get_cfr_strategies
 from src.domains.available_domains import get_domain_by_name
 
@@ -8,10 +9,16 @@ ACTIVATE_FILE = False
 if __name__ == '__main__' and ACTIVATE_FILE:
 	domain = get_domain_by_name("IIGS6_gambit_flattened")
 	tensorcfr = TensorCFRFlattenedDomains(domain)
-	get_cfr_strategies(
-			total_steps=10,
+	average_strategies = get_cfr_strategies(
+			total_steps=1000,
 			tensorcfr_instance=tensorcfr,
 			quiet=True,
 			# profiling=True,
-			delay=0
+			delay=0,
+			register_strategies_on_step=[0, 250, 500, 750, 999]
 	)   # TODO verify the results (final average strategies) via `gtlibrary`
+
+	utils.gtlibrary.export_average_strategies_to_json(
+		domain,
+		average_strategies,
+		"GS3_average_strategies")

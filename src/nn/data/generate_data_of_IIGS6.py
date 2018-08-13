c8d3e5e0dd33bac0c6c3e6409b9f71c6bf5a8665
Karel Ha <mathemage@gmail.com>
2018-08-13 15:19:00 +0200

    Create `generate_data_of_IIGS6.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/data/generate_data_of_IIGS6.py
@@ -0,0 +1,18 @@
+import os
+
+from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
+from src.commons.constants import SEED_FOR_TESTING
+from src.domains.available_domains import get_domain_by_name
+
+if __name__ == '__main__':
+	domain = get_domain_by_name("IIGS6_gambit_flattened")
+	tensorcfr = TensorCFRFixedTrunkStrategies(
+		domain,
+		trunk_depth=10
+	)
+	script_directory = os.path.dirname(os.path.abspath(__file__))
+	tensorcfr.generate_dataset_at_trunk_depth(
+		dataset_size=2,
+		dataset_directory=script_directory + "/out",
+		seed=SEED_FOR_TESTING
+	)

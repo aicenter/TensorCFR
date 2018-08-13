f67a4698141292523239a75ddaffe6d3126c2888
Karel Ha <mathemage@gmail.com>
2018-08-13 16:37:09 +0200

    Create `generate_data_of_GP4x3_224.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/data/generate_data_of_GP4x3_224.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+
+from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
+from src.commons.constants import SEED_FOR_TESTING
+from src.domains.available_domains import get_domain_by_name
+from src.utils.other_utils import get_current_timestamp
+
+if __name__ == '__main__':
+	domain = get_domain_by_name("GP_cards4x3_224_gambit_flattened")
+	tensorcfr = TensorCFRFixedTrunkStrategies(
+		domain,
+		trunk_depth=8
+	)
+	script_directory = os.path.dirname(os.path.abspath(__file__))
+	print(get_current_timestamp())
+	tensorcfr.generate_dataset_at_trunk_depth(
+		dataset_size=2,
+		dataset_directory=script_directory + "/out",
+		seed=SEED_FOR_TESTING
+	)
+	print(get_current_timestamp())

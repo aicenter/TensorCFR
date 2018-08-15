7f336a6efaef20d4532ef513ccaabaa90f02471a
Karel Ha <mathemage@gmail.com>
2018-08-15 19:11:57 +0200

    Store only `data_id_column` and `node_to_infoset`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_fixed_trunk_strategies/TensorCFRFixedTrunkStrategies.py
+++ src/algorithms/tensorcfr_fixed_trunk_strategies/TensorCFRFixedTrunkStrategies.py
@@ -228,13 +228,14 @@ class TensorCFRFixedTrunkStrategies:
 		:return: The reach probabilities of nodes based on `current_infoset_strategies`.
 		"""
 		node_cf_strategies = self.get_node_cf_strategies(for_player=for_player)
-		if for_player is None:
-			player_name = "current_player"
-		elif for_player == ALL_PLAYERS:
+		if for_player == ALL_PLAYERS:
 			node_cf_strategies = self.get_node_strategies()
 			player_name = "all_players"
-		else:
+		elif for_player in [PLAYER1, PLAYER2]:
 			player_name = "player{}".format(for_player)
+		else:
+			player_name = "current_player"
+
 		with tf.variable_scope("nodal_reach_probabilities_for_{}".format(player_name)):
 			nodal_reach_probabilities = [None] * self.domain.levels
 			nodal_reach_probabilities[0] = tf.expand_dims(

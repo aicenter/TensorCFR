a2fd08b21abb3b0a4fc459d3a090909805857a5f
Karel Ha <mathemage@gmail.com>
2018-08-15 18:04:49 +0200

    Rename `test_level_*` to `test_level_*_via_dense_tensorcfr`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/domains/domain01/test_topdown_reach_probabilities.py
+++ tests/domains/domain01/test_topdown_reach_probabilities.py
@@ -1,8 +1,8 @@
 import numpy as np
 import tensorflow as tf
 
-from src.commons.constants import SMALL_ERROR_TOLERANCE
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR
+from src.commons.constants import SMALL_ERROR_TOLERANCE
 from src.domains.domain01.Domain01 import get_domain01
 
 
@@ -11,7 +11,7 @@ class TestTopDownReachProbabilities(tf.test.TestCase):
 		self.error_tolerance = SMALL_ERROR_TOLERANCE
 		self.domain01 = get_domain01()
 
-	def test_level_0(self):
+	def test_level_0_via_dense_tensorcfr(self):
 		expected_output = np.array([1])
 		with self.test_session() as sess:
 			sess.run(tf.global_variables_initializer())
@@ -19,7 +19,7 @@ class TestTopDownReachProbabilities(tf.test.TestCase):
 			reach_probabilities = sess.run(tensorcfr.get_nodal_reach_probabilities())
 			self.assertNDArrayNear(reach_probabilities[0], expected_output, self.error_tolerance)
 
-	def test_level_1(self):
+	def test_level_1_via_dense_tensorcfr(self):
 		expected_output = np.array([0.5, 0.25, 0.1, 0.1, 0.05])
 		with self.test_session() as sess:
 			sess.run(tf.global_variables_initializer())
@@ -27,7 +27,7 @@ class TestTopDownReachProbabilities(tf.test.TestCase):
 			reach_probabilities = sess.run(tensorcfr.get_nodal_reach_probabilities())
 			self.assertNDArrayNear(reach_probabilities[1], expected_output, self.error_tolerance)
 
-	def test_level_2(self):
+	def test_level_2_via_dense_tensorcfr(self):
 		expected_output = np.array([[0.5, 0.5, 0.5],
 									[0.025, 0.225, 0],
 									[0.02, 0.08, 0],
@@ -39,7 +39,7 @@ class TestTopDownReachProbabilities(tf.test.TestCase):
 			reach_probabilities = sess.run(tensorcfr.get_nodal_reach_probabilities())
 			self.assertNDArrayNear(reach_probabilities[2], expected_output, self.error_tolerance)
 
-	def test_level_3(self):
+	def test_level_3_via_dense_tensorcfr(self):
 		expected_output = np.array([[[0.5, 0.5],
 									 [0.35, 0.15],
 									 [0, 0]],

c9a2553ea1f4f526644fea05b3291b77488f0827
Karel Ha <mathemage@gmail.com>
2018-08-15 18:12:23 +0200

    Test nodal reach probabilities for all players
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/domains/domain01/test_topdown_reach_probabilities.py
+++ tests/domains/domain01/test_topdown_reach_probabilities.py
@@ -2,14 +2,17 @@ import numpy as np
 import tensorflow as tf
 
 from src.algorithms.tensorcfr.TensorCFR import TensorCFR
-from src.commons.constants import SMALL_ERROR_TOLERANCE
+from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
+from src.commons.constants import SMALL_ERROR_TOLERANCE, ALL_PLAYERS
 from src.domains.domain01.Domain01 import get_domain01
+from src.domains.flattened_domain01_gambit.domain_from_gambit_loader import get_flattened_domain01_from_gambit
 
 
 class TestTopDownReachProbabilities(tf.test.TestCase):
 	def setUp(self):
 		self.error_tolerance = SMALL_ERROR_TOLERANCE
 		self.domain01 = get_domain01()
+		self.flattened_domain01 = get_flattened_domain01_from_gambit()
 
 	def test_level_0_via_dense_tensorcfr(self):
 		expected_output = np.array([1])
@@ -64,6 +67,22 @@ class TestTopDownReachProbabilities(tf.test.TestCase):
 			tensorcfr = TensorCFR(self.domain01)
 			reach_probabilities = sess.run(tensorcfr.get_nodal_reach_probabilities())
 			self.assertNDArrayNear(reach_probabilities[3], expected_output, self.error_tolerance)
+
+	def test_level_0_via_tensorcfr_fixed_trunk_for_all_players(self):
+		expected_output = np.array([1])
+		with self.test_session() as sess:
+			sess.run(tf.global_variables_initializer())
+			tensorcfr = TensorCFRFixedTrunkStrategies(self.flattened_domain01)
+			reach_probabilities = sess.run(tensorcfr.get_nodal_reach_probabilities(for_player=ALL_PLAYERS))
+			self.assertNDArrayNear(reach_probabilities[0], expected_output, self.error_tolerance)
+
+	def test_level_1_via_tensorcfr_fixed_trunk(self):
+		expected_output = np.array([0.5, 0.25, 0.1, 0.1, 0.05])
+		with self.test_session() as sess:
+			sess.run(tf.global_variables_initializer())
+			tensorcfr = TensorCFRFixedTrunkStrategies(self.flattened_domain01)
+			reach_probabilities = sess.run(tensorcfr.get_nodal_reach_probabilities(for_player=ALL_PLAYERS))
+			self.assertNDArrayNear(reach_probabilities[1], expected_output, self.error_tolerance)
 
 
 if __name__ == "__main__":

516e1746ded5d25c7a68119280eff07d75b8f3f0
Karel Ha <mathemage@gmail.com>
2018-08-16 22:35:23 +0200

    Create `generate_data_of_domain01.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/data/generate_data_of_domain01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+
+from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
+from src.commons.constants import SEED_FOR_TESTING
+from src.domains.available_domains import get_domain_by_name
+
+if __name__ == '__main__':
+	domain = get_domain_by_name("flattened_domain01_via_gambit")
+	tensorcfr = TensorCFRFixedTrunkStrategies(
+		domain,
+		trunk_depth=2
+	)
+	script_directory = os.path.dirname(os.path.abspath(__file__))
+	tensorcfr.generate_dataset_at_trunk_depth(
+		dataset_size=3,
+		dataset_directory=script_directory + "/out",
+		seed=SEED_FOR_TESTING
+	)

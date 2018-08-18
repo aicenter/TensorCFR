a57e56c92395d1def82c99c3a4e1d30c01fcf922
Karel Ha <mathemage@gmail.com>
2018-08-18 18:20:21 +0200

    Add `setUp()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/algorithms/tensorcfr_fixed_trunk_strategies/test_tensorcfr_fixed_trunk_strategies.py
+++ tests/algorithms/tensorcfr_fixed_trunk_strategies/test_tensorcfr_fixed_trunk_strategies.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
 
+from src.commons.constants import SMALL_ERROR_TOLERANCE
+from src.domains.flattened_domain01_gambit.domain_from_gambit_loader import get_flattened_domain01_from_gambit
+
 
 class TestNodalExpectedValuesAtTrunkDepth(TestCase):
+	def setUp(self):
+		self.error_tolerance = SMALL_ERROR_TOLERANCE
+		self.flattened_domain01 = get_flattened_domain01_from_gambit()
+
 	def test_domain01_lvl2_seed42(self):
 		raise NotImplementedError

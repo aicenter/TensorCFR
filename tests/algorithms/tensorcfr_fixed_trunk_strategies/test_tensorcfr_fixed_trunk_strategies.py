476b84bfbd9b2dcaa343acc41a87713d8a7969fc
Karel Ha <mathemage@gmail.com>
2018-08-18 19:56:02 +0200

    Move `nodal_expected_values` out of session with-block
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/algorithms/tensorcfr_fixed_trunk_strategies/test_tensorcfr_fixed_trunk_strategies.py
+++ tests/algorithms/tensorcfr_fixed_trunk_strategies/test_tensorcfr_fixed_trunk_strategies.py
@@ -34,8 +34,8 @@ class TestNodalExpectedValuesAtTrunkDepth(tf.test.TestCase):
 		expected_output = np.array(
 			[15., -35., 75., 95., -135., np.nan, -195., np.nan, 275., -295.]
 		)
+		nodal_expected_values = self.tensorcfr_domain01_td2.get_nodal_expected_values_at_trunk_depth()
 		with self.test_session() as sess:
 			sess.run(tf.global_variables_initializer())
-			nodal_expected_values = self.tensorcfr_domain01_td2.get_nodal_expected_values_at_trunk_depth()
 			self.print_debug_information(expected_output, sess, self.tensorcfr_domain01_td2, nodal_expected_values)
 			self.compare_with_expected_output(expected_output, sess.run(nodal_expected_values))

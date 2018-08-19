2ac957e0d0091e956d2250f4bcdab8688947dbda
Karel Ha <mathemage@gmail.com>
2018-08-19 20:52:34 +0200

    Implement `test_goofspiel3_lvl8_for_seed42`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- tests/algorithms/tensorcfr_fixed_trunk_strategies/test_tensorcfr_fixed_trunk_strategies.py
+++ tests/algorithms/tensorcfr_fixed_trunk_strategies/test_tensorcfr_fixed_trunk_strategies.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
 from src.commons.constants import SMALL_ERROR_TOLERANCE, DEFAULT_TOTAL_STEPS
 from src.domains.flattened_domain01_gambit.domain_from_gambit_loader import get_flattened_domain01_from_gambit
+from src.domains.flattened_goofspiel3.domain_from_gambit_loader import get_flattened_goofspiel3
 from src.utils.tensor_utils import print_tensor, print_tensors
 
 
@@ -14,6 +15,8 @@ class TestNodalExpectedValuesAtTrunkDepth(tf.test.TestCase):
 		self.total_steps = DEFAULT_TOTAL_STEPS
 		self.error_tolerance = SMALL_ERROR_TOLERANCE
 		self.flattened_domain01 = get_flattened_domain01_from_gambit()
+		# self.gs2 = get_flattened_goofspiel2()   # TODO use the 2 card version
+		self.ii_gs3_3cards = get_flattened_goofspiel3()
 		self.tensorcfr_domain01_td2 = TensorCFRFixedTrunkStrategies(self.flattened_domain01, trunk_depth=2)
 
 	@staticmethod
@@ -149,3 +152,38 @@ class TestNodalExpectedValuesAtTrunkDepth(tf.test.TestCase):
 			self.run_cfr_and_assign_average_strategies(sess, self.tensorcfr_domain01_td2)
 			self.print_debug_information(expected_output, sess, self.tensorcfr_domain01_td2, nodal_expected_values)
 			self.compare_with_expected_output(expected_output, sess.run(nodal_expected_values))
+
+	def test_ii_goofspiel3_3cards_lvl8_for_seed42(self):
+		"""
+		Strategies at level 5 after 1000 CFR iterations should converge as follows:
+
+			```
+			"II-GS3_gambit_flattened/current_infoset_strategies_lvl5:0"
+			 [[1.  0. ]    # infoset 2:32
+			 [0.5 0.5]     # infoset 2:28
+			 [1.  0. ]     # infoset 2:15
+			 [1.  0. ]     # infoset 2:11
+			 [1.  0. ]     # infoset 2:20
+			 [0.5 0.5]     # infoset 2:7
+			 [1.  0. ]]    # infoset 2:2
+			```
+
+		(see `doc/goofspiel/II-GS3_solved_via_gambit.png` for the infoset labels)
+		"""
+		self.run_test_nodal_expected_values_given_domain_level_seed(
+			flattened_domain=self.ii_gs3_3cards,
+			level=5,
+			seed=42,
+			expected_output=np.array(
+				# see `doc/goofspiel/II-GS3_solved_via_gambit.png` for the infoset labels
+				[0, 1,    # infoset 2:32
+				 -1, -1,  # infoset 2:28
+				 0, -1,   # infoset 2:15
+				 1, 1,    # infoset 2:11
+				 0, 1,    # infoset 2:20
+				 -1, -1,  # infoset 2:15
+				 0, 1,    # infoset 2:11
+				 1, 1,    # infoset 2:7
+				 0, 1]    # infoset 2:2
+			)
+		)

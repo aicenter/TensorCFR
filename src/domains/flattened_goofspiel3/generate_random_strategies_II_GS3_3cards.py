cc55806550762e1b20a605e88e1e6e2569e98d66
Karel Ha <mathemage@gmail.com>
2018-08-20 14:34:04 +0200

    Move `None` to the start of `seeds`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/flattened_goofspiel3/generate_random_strategies_II_GS3_3cards.py
+++ src/domains/flattened_goofspiel3/generate_random_strategies_II_GS3_3cards.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 	goofspiel3 = get_flattened_goofspiel3()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		seeds = [SEED_FOR_TESTING, SEED_FOR_TESTING + 1, SEED_FOR_TESTING + 2, 42, None]
+		seeds = [None, SEED_FOR_TESTING, SEED_FOR_TESTING + 1, SEED_FOR_TESTING + 2, 42]
 		for i, seed in enumerate(seeds):
 			trunk_depth_ = 3
 			print("Random strategies #{} (trunk depth {}, seed {}):".format(i + 1, trunk_depth_, seed))

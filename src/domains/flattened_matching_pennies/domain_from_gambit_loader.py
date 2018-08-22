cbedf0d4de7a1c0b1d812761dce571d110d27d28
Karel Ha <mathemage@gmail.com>
2018-08-22 17:54:37 +0200

    Rewrite `domain_from_gambit_loader` for `matching_pennies`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/flattened_matching_pennies/domain_from_gambit_loader.py
+++ src/domains/flattened_matching_pennies/domain_from_gambit_loader.py
@@ -5,23 +5,24 @@ import tensorflow as tf
 from src.domains.FlattenedDomain import FlattenedDomain
 
 
-def get_flattened_domain01_from_gambit():
+def get_flattened_matching_pennies_from_gambit():
 	path_to_domain_filename = os.path.join(
-			os.path.dirname(
-					os.path.abspath(
-							__file__)
-			),
-			'..',
-			'..',
-			'..',
-			'doc',
-			'domain01_via_gambit.efg'
+		os.path.dirname(
+			os.path.abspath(
+				__file__)
+		),
+		'..',
+		'..',
+		'..',
+		'doc',
+		'matching_pennies',
+		'matching_pennies_via_gambit.efg'
 	)
-	return FlattenedDomain.init_from_gambit_file(path_to_domain_filename, domain_name="flattened_domain01_gambit")
+	return FlattenedDomain.init_from_gambit_file(path_to_domain_filename, domain_name="flattened_matching_pennies_gambit")
 
 
 if __name__ == '__main__':
-	flattened_domain01 = get_flattened_domain01_from_gambit()
+	flattened_matching_pennies = get_flattened_matching_pennies_from_gambit()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		flattened_domain01.print_domain(sess)
+		flattened_matching_pennies.print_domain(sess)

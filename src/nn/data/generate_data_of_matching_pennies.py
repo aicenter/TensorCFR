60c8eff1d7a44d74a49b5be707146713e0eb82ee
Karel Ha <mathemage@gmail.com>
2018-08-22 22:41:40 +0200

    Use `generate_dataset_tf_while_loop` for matching pennies
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_matching_pennies.py
+++ src/nn/data/generate_data_of_matching_pennies.py
@@ -12,7 +12,13 @@ if __name__ == '__main__':
 		trunk_depth=1
 	)
 	script_directory = os.path.dirname(os.path.abspath(__file__))
-	tensorcfr.generate_dataset_at_trunk_depth(
+	# tensorcfr.generate_dataset_single_session(
+	# 	# dataset_for_nodes=False,
+	# 	dataset_size=3,
+	# 	dataset_directory=script_directory + "/out",
+	# 	seed=SEED_FOR_TESTING
+	# )
+	tensorcfr.generate_dataset_tf_while_loop(
 		# dataset_for_nodes=False,
 		dataset_size=3,
 		dataset_directory=script_directory + "/out",

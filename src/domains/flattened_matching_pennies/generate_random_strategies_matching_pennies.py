e99f82fba694190a5b0ad2550723b43de99142a3
Karel Ha <mathemage@gmail.com>
2018-08-22 18:43:36 +0200

    Implement generating random strategies for matching pennies
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/flattened_matching_pennies/generate_random_strategies_matching_pennies.py
+++ src/domains/flattened_matching_pennies/generate_random_strategies_matching_pennies.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
 
 from src.commons.constants import SEED_FOR_TESTING
+from src.domains.flattened_matching_pennies.flattened_matching_pennies_gambit import \
+	get_flattened_matching_pennies_from_gambit
 
-# TODO rewrite
 if __name__ == '__main__':
-	flattened_domain01 = ()
+	flattened_matching_pennies = get_flattened_matching_pennies_from_gambit()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		seeds = [None, SEED_FOR_TESTING, 42]
 		for i, seed in enumerate(seeds):
 			trunk_depth_ = 2
 			print("Random strategies #{} (trunk depth {}, seed {}):".format(i + 1, trunk_depth_, seed))
-			strategies = flattened_domain01.generate_random_strategies(
+			strategies = flattened_matching_pennies.generate_random_strategies(
 				seed=seed,
 				trunk_depth=trunk_depth_
 			)

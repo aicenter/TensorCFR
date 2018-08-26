e91e4866c99572ca18b529124c553b4ca4473205
Karel Ha <mathemage@gmail.com>
2018-08-26 23:57:16 +0200

    Create `generate_data_of_IIGS5`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/data/generate_data_of_IIGS5.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import os
+
+from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
+from src.commons.constants import SEED_FOR_TESTING
+from src.domains.available_domains import get_domain_by_name
+from src.utils.other_utils import get_current_timestamp
+
+if __name__ == '__main__':
+	domain = get_domain_by_name("IIGS5_gambit_flattened")
+	tensorcfr = TensorCFRFixedTrunkStrategies(
+		domain,
+		trunk_depth=10
+	)
+	script_directory = os.path.dirname(os.path.abspath(__file__))
+	print(get_current_timestamp())
+	tensorcfr.generate_dataset_at_trunk_depth(
+		# dataset_for_nodes=False,
+		dataset_size=2,
+		dataset_directory=script_directory + "/out",
+		seed=SEED_FOR_TESTING
+	)
+	print(get_current_timestamp())
+	tensorcfr.generate_dataset_single_session(
+		# dataset_for_nodes=False,
+		dataset_size=2,
+		dataset_directory=script_directory + "/out",
+		seed=SEED_FOR_TESTING
+	)
+	print(get_current_timestamp())
+	# tensorcfr.generate_dataset_tf_while_loop(
+	# 	# dataset_for_nodes=False,
+	# 	dataset_size=3,
+	# 	dataset_directory=script_directory + "/out",
+	# 	seed=SEED_FOR_TESTING
+	# )
+	# print(get_current_timestamp())

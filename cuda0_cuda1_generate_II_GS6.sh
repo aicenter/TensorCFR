8d77d15bb50fe031c32a765260dd8e0d1284f597
Karel Ha <mathemage@gmail.com>
2018-08-28 16:47:02 +0200

    Create script to generate data of IIGS6 on 2 GPUs
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ cuda0_cuda1_generate_II_GS6.sh
@@ -0,0 +1,14 @@
+cmd='python3.6 -m src.nn.data.generate_data_of_IIGS6'
+num_launches=1
+num_gpus=2
+for l in $(seq 1 $num_launches);
+do
+	echo "######################################## Launch #$l started ########################################"
+	for gpu_id in $(seq 1 $num_gpus);
+	do
+		echo "> Launching process #$gpu_id" && CUDA_VISIBLE_DEVICES=$gpu_id $cmd &
+	done
+	wait
+	echo "########################################  Launch #$l ended  ########################################"
+	echo ' '
+done

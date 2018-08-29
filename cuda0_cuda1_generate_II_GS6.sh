9bc7f4bb0576b4b6eacf5ad2f4da3ecaee543906
Karel Ha <mathemage@gmail.com>
2018-08-29 11:29:52 +0200

    Mention exclusive interval in `seq` for CUDA GPUs
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- cuda0_cuda1_generate_II_GS6.sh
+++ cuda0_cuda1_generate_II_GS6.sh
@@ -4,9 +4,9 @@ num_gpus=2
 for l in $(seq 1 $num_launches);
 do
 	echo "######################################## Launch #$l started ########################################"
-	for gpu_id in $(seq 1 $num_gpus);
+	for gpu_id in $(seq 0 $num_gpus); 	# TODO fix exclusive interval
 	do
-		echo "> Launching process #$gpu_id" && CUDA_VISIBLE_DEVICES=$gpu_id $cmd &
+		echo "> Launching process #$gpu_id" && echo CUDA_VISIBLE_DEVICES=$gpu_id $cmd &
 	done
 	wait
 	echo "########################################  Launch #$l ended  ########################################"

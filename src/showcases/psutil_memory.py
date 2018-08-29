6aea224397dfb088cf3598d9b6e794df242dcfd0
Karel Ha <mathemage@gmail.com>
2018-08-29 20:16:45 +0200

    Create `utils.other_utils.get_memory_usage()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/psutil_memory.py
+++ src/showcases/psutil_memory.py
@@ -3,9 +3,12 @@ import os
 
 import psutil
 
+from src.utils.other_utils import get_memory_usage
+
 if __name__ == '__main__':
 	"""
 	Demonstrate that `lambda x: zip(*x)` is self-inverse. In other words, Python function `zip` is idempotent.
 	"""
 	process = psutil.Process(os.getpid())
 	print("memory: {} bytes".format(process.memory_info().rss))
+	print("memory: {} bytes".format(get_memory_usage()))
--- src/utils/other_utils.py
+++ src/utils/other_utils.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import datetime
+import os
+
+import psutil
 
 
 def get_current_timestamp():
 	return datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+
+def get_memory_usage():
+	process = psutil.Process(os.getpid())
+	return process.memory_info().rss

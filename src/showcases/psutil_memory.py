ec99975887b0cb0502f36f105d996d8c589a12db
Karel Ha <mathemage@gmail.com>
2018-08-30 16:37:57 +0200

    Remove irrelevant docstring in `psutil_memory.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/psutil_memory.py
+++ src/showcases/psutil_memory.py
@@ -3,7 +3,4 @@
 from src.utils.other_utils import get_memory_usage
 
 if __name__ == '__main__':
-	"""
-	Demonstrate that `lambda x: zip(*x)` is self-inverse. In other words, Python function `zip` is idempotent.
-	"""
 	print("memory: {:,} bytes".format(get_memory_usage()))

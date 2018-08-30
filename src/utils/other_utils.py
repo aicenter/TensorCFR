537395b12885237d18b99de3188274a017211d63
Karel Ha <mathemage@gmail.com>
2018-08-30 17:32:15 +0200

    Revert back to `psutil` version of `get_memory_usage()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/utils/other_utils.py
+++ src/utils/other_utils.py
@@ -3,16 +3,23 @@ import datetime
 import os
 import subprocess
 
+import psutil
+
 
 def get_current_timestamp():
 	return datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
 
 def get_memory_usage():
+	process = psutil.Process(os.getpid())
+	return process.memory_info().rss
+
+
+def get_memory_usage_using_os():
 	pid = os.getpid()
 	cmd = ['ps', '-q', str(pid), '-o', 'rss']
 
-	proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
 	(out, err) = proc.communicate()
 
 	if len(err) == 0:

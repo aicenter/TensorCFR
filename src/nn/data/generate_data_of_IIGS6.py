29e1abc0cd99061efbd66a61b165b0fdd6d378f9
Karel Ha <mathemage@gmail.com>
2018-08-31 17:57:52 +0200

    Add arguments --seed and --size for `generate_data_of_IIGS6.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_IIGS6.py
+++ src/nn/data/generate_data_of_IIGS6.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import os
 
 from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
@@ -12,21 +13,26 @@ ACTIVATE_FILE = True
 if __name__ == '__main__' and ACTIVATE_FILE:
 	script_directory = os.path.dirname(os.path.abspath(__file__))
 
-	dataset_size = 1000
-	for starting_seed in range(dataset_size):
-		domain = get_domain_by_name("IIGS6_gambit_flattened")
-		print(get_current_timestamp())
-		tensorcfr = TensorCFRFixedTrunkStrategies(
-			domain,
-			trunk_depth=10
-		)
-		print(get_current_timestamp())
-		tensorcfr.generate_dataset_at_trunk_depth(
-			dataset_size=1,
-			dataset_directory=script_directory + "/out/IIGS6/{}_datasets/{}".format(dataset_size, get_current_timestamp()),
-			dataset_seed_to_start=starting_seed
-		)
-		print(get_current_timestamp())
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--seed", help="dataset seed", nargs='?', type=int, default=0)
+	parser.add_argument("--size", help="size of one dataset (given the seed)", nargs='?', type=int, default=1000)
+	args = parser.parse_args()
+	dataset_size = args.size
+	starting_seed = args.seed
+
+	domain = get_domain_by_name("IIGS6_gambit_flattened")
+	print(get_current_timestamp())
+	tensorcfr = TensorCFRFixedTrunkStrategies(
+		domain,
+		trunk_depth=10
+	)
+	print(get_current_timestamp())
+	tensorcfr.generate_dataset_at_trunk_depth(
+		dataset_size=1,
+		dataset_directory=script_directory + "/out/IIGS6/{}_datasets/{}".format(dataset_size, get_current_timestamp()),
+		dataset_seed_to_start=starting_seed
+	)
+	print(get_current_timestamp())
 
 	# tensorcfr.generate_dataset_single_session(
 	# 	# dataset_for_nodes=False,

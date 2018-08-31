515adf9206ceb51f6dd850203ec17c918f169959
Karel Ha <mathemage@gmail.com>
2018-08-31 15:41:20 +0200

    Loop over start seeds with `generate_dataset_at_trunk_depth` for II-GS6
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_IIGS6.py
+++ src/nn/data/generate_data_of_IIGS6.py
@@ -16,21 +16,24 @@ if __name__ == '__main__' and ACTIVATE_FILE:
 		trunk_depth=10
 	)
 	script_directory = os.path.dirname(os.path.abspath(__file__))
-	# print(get_current_timestamp())
-	# tensorcfr.generate_dataset_at_trunk_depth(
+
+	for starting_seed in range(1000):
+		print(get_current_timestamp())
+		tensorcfr.generate_dataset_at_trunk_depth(
+			dataset_size=1,
+			dataset_directory=script_directory + "/out/IIGS6/1000_datapoints/{}".format(get_current_timestamp()),
+			dataset_seed_to_start=starting_seed
+		)
+		print(get_current_timestamp())
+
+	# tensorcfr.generate_dataset_single_session(
 	# 	# dataset_for_nodes=False,
-	# 	dataset_size=2,
-	# 	dataset_directory=script_directory + "/out",
-	# 	seed=SEED_FOR_TESTING
+	# 	dataset_size=50,
+	# 	dataset_directory=script_directory + "/out/IIGS6/50_datapoints",
+	# 	#seed=SEED_FOR_TESTING
 	# )
-	print(get_current_timestamp())
-	tensorcfr.generate_dataset_single_session(
-		# dataset_for_nodes=False,
-		dataset_size=50,
-		dataset_directory=script_directory + "/out/IIGS6/50_datapoints",
-		#seed=SEED_FOR_TESTING
-	)
-	print(get_current_timestamp())
+	# print(get_current_timestamp())
+
 	# tensorcfr.generate_dataset_tf_while_loop(
 	# 	# dataset_for_nodes=False,
 	# 	dataset_size=3,

f0f78b3eb59b512e4d7b395eb460951ddc552454
Karel Ha <mathemage@gmail.com>
2018-08-31 19:33:33 +0200

    Extract `get_dataset_parameters()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_IIGS6.py
+++ src/nn/data/generate_data_of_IIGS6.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-import argparse
 import os
 
 from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
 from src.domains.available_domains import get_domain_by_name
-from src.utils.other_utils import get_current_timestamp, print_dataset_parameters
+from src.utils.other_utils import get_current_timestamp, print_dataset_parameters, get_dataset_parameters
 
 # TODO: Get rid of `ACTIVATE_FILE` hotfix
 ACTIVATE_FILE = True
@@ -13,14 +12,8 @@ ACTIVATE_FILE = True
 if __name__ == '__main__' and ACTIVATE_FILE:
 	domain_name = "IIGS6_gambit_flattened"
 	script_directory = os.path.dirname(os.path.abspath(__file__))
-
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--seed", help="dataset seed", nargs='?', type=int, default=0)
-	parser.add_argument("--size", help="size of one dataset (given the seed)", nargs='?', type=int, default=1000)
-	args = parser.parse_args()
-	dataset_size = args.size
-	starting_seed = args.seed
-	print_dataset_parameters(domain_name, starting_seed, dataset_size)
+	dataset_parameters = get_dataset_parameters()
+	print_dataset_parameters(domain_name, dataset_parameters["starting_seed"], dataset_parameters["dataset_size"])
 
 	domain = get_domain_by_name(domain_name)
 	print(get_current_timestamp())
@@ -31,8 +24,11 @@ if __name__ == '__main__' and ACTIVATE_FILE:
 	print(get_current_timestamp())
 	tensorcfr.generate_dataset_at_trunk_depth(
 		dataset_size=1,
-		dataset_directory=script_directory + "/out/IIGS6/{}_datasets/{}".format(dataset_size, get_current_timestamp()),
-		dataset_seed_to_start=starting_seed
+		dataset_directory=script_directory + "/out/IIGS6/{}_datasets/{}".format(
+			dataset_parameters["dataset_size"],
+			get_current_timestamp()
+		),
+		dataset_seed_to_start=dataset_parameters["starting_seed"]
 	)
 	print(get_current_timestamp())
 
--- src/utils/other_utils.py
+++ src/utils/other_utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import datetime
 import os
 import subprocess
@@ -27,6 +28,17 @@ def get_memory_usage_using_os():
 		return int(out_list[1])
 	else:
 		return None
+
+
+def get_dataset_parameters():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--seed", help="dataset seed", nargs='?', type=int, default=0)
+	parser.add_argument("--size", help="size of one dataset (given the seed)", nargs='?', type=int, default=1000)
+	args = parser.parse_args()
+	return {
+		"dataset_size": args.size,
+		"starting_seed": args.seed
+	}
 
 
 def print_dataset_parameters(domain_name, starting_seed, dataset_size):

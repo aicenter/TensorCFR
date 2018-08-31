f3c713a62ac46e6b5bc466beb6a689b04c5279e8
Karel Ha <mathemage@gmail.com>
2018-09-01 00:14:28 +0200

    Update generate_II_GS6.sh: seed <- dataset, size
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- generate_II_GS6.sh
+++ generate_II_GS6.sh
@@ -1,1 +1,10 @@
-python3 -m src.nn.data.generate_data_of_IIGS6
+size=${1:-1}
+datasets=${2:-1000}
+for dataset in $(seq 0 $datasets);
+do
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	seed=$((dataset * size))
+	echo "Generation #$dataset, starting with dataset_seed $seed:"
+	CUDA_VISIBLE_DEVICES=0 python3.6 -m src.nn.data.generate_data_of_IIGS6 --seed $seed --size $size
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+done

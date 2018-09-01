6727f341c2294785f447d83f0f940280a7501990
Karel Ha <mathemage@gmail.com>
2018-09-01 13:05:01 +0200

    Add $seed_offset to generate_II_GS6.sh
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- generate_II_GS6.sh
+++ generate_II_GS6.sh
@@ -1,10 +1,13 @@
-size=${1:-1}
-datasets=${2:-1000}
-for dataset in $(seq 0 $datasets);
+size_of_generation=${1:-1}
+num_generations=${2:-1000}
+seed_offset=${3:-0}
+
+last_generation=$((num_generations-1))
+for generation in $(seq 0 $last_generation);
 do
 	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-	seed=$((dataset * size))
-	echo "Generation #$dataset, starting with dataset_seed $seed:"
-	CUDA_VISIBLE_DEVICES=0 python3.6 -m src.nn.data.generate_data_of_IIGS6 --seed $seed --size $size
+	seed=$((seed_offset + generation * size_of_generation))
+	echo "Generation #$generation of size $size_of_generation, starting with dataset_seed $seed:"
+	CUDA_VISIBLE_DEVICES=0 python3.6 -m src.nn.data.generate_data_of_IIGS6 --seed $seed --size $size_of_generation
 	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 done

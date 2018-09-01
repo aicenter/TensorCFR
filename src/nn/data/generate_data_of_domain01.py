151234d0ae8479ee523918f0436b44f63030097e
Karel Ha <mathemage@gmail.com>
2018-09-01 13:24:57 +0200

    Use `argparse` in `generate_data_of_domain01.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_domain01.py
+++ src/nn/data/generate_data_of_domain01.py
@@ -2,25 +2,43 @@
 import os
 
 from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
-from src.commons.constants import SEED_FOR_TESTING
 from src.domains.available_domains import get_domain_by_name
+from src.utils.other_utils import get_current_timestamp, print_dataset_parameters, get_dataset_parameters
 
 if __name__ == '__main__':
-	domain = get_domain_by_name("flattened_domain01_via_gambit")
+	script_directory = os.path.dirname(os.path.abspath(__file__))
+	dataset_parameters = get_dataset_parameters("flattened_domain01_via_gambit")
+	print_dataset_parameters(dataset_parameters)
+
+	domain = get_domain_by_name(dataset_parameters["domain_name"])
+	print(get_current_timestamp())
 	tensorcfr = TensorCFRFixedTrunkStrategies(
 		domain,
 		trunk_depth=2
 	)
-	script_directory = os.path.dirname(os.path.abspath(__file__))
+	print(get_current_timestamp())
 	tensorcfr.generate_dataset_at_trunk_depth(
-		# dataset_for_nodes=False,
-		dataset_size=4,
-		dataset_directory=script_directory + "/out",
-		dataset_seed_to_start=SEED_FOR_TESTING
+		dataset_size=dataset_parameters["dataset_size"],
+		dataset_directory=script_directory + "/out/{}/{}_datasets".format(
+			dataset_parameters["domain_name"],
+			dataset_parameters["dataset_size"],
+		),
+		dataset_seed_to_start=dataset_parameters["starting_seed"]
 	)
+	print(get_current_timestamp())
+
 	# tensorcfr.generate_dataset_single_session(
 	# 	# dataset_for_nodes=False,
-	# 	dataset_size=4,
+	# 	dataset_size=50,
+	# 	dataset_directory=script_directory + "/out/IIGS6/50_datapoints",
+	# 	#seed=SEED_FOR_TESTING
+	# )
+	# print(get_current_timestamp())
+
+	# tensorcfr.generate_dataset_tf_while_loop(
+	# 	# dataset_for_nodes=False,
+	# 	dataset_size=3,
 	# 	dataset_directory=script_directory + "/out",
 	# 	seed=SEED_FOR_TESTING
 	# )
+	# print(get_current_timestamp())

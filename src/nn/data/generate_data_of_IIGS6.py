1174a61920dae57d9970021fd3326230e7451fc9
Karel Ha <mathemage@gmail.com>
2018-09-02 18:16:25 +0200

    Condition on `dataset_generation_method` in `generate_data_of_IIGS6.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_IIGS6.py
+++ src/nn/data/generate_data_of_IIGS6.py
@@ -2,6 +2,7 @@
 import os
 
 from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
+from src.commons.constants import DEFAULT_DATAGEN_METHOD, DATAGEN_MULTISESSIONS, DATAGEN_SINGLESESSIONS
 from src.domains.available_domains import get_domain_by_name
 from src.utils.other_utils import get_current_timestamp, print_dataset_parameters, get_dataset_parameters
 
@@ -20,24 +21,30 @@ if __name__ == '__main__' and ACTIVATE_FILE:
 		domain,
 		trunk_depth=10
 	)
-	print(get_current_timestamp())
-	tensorcfr.generate_dataset_multiple_sessions(
-		dataset_size=dataset_parameters["dataset_size"],
-		dataset_directory=script_directory + "/out/{}/{}_datasets".format(
-			dataset_parameters["domain_name"],
-			dataset_parameters["dataset_size"],
-		),
-		dataset_seed_to_start=dataset_parameters["starting_seed"]
-	)
-	print(get_current_timestamp())
 
-	# tensorcfr.generate_dataset_single_session(
-	# 	# dataset_for_nodes=False,
-	# 	dataset_size=50,
-	# 	dataset_directory=script_directory + "/out/IIGS6/50_datapoints",
-	# 	#seed=SEED_FOR_TESTING
-	# )
-	# print(get_current_timestamp())
+	print(get_current_timestamp())
+	dataset_generation_method = DEFAULT_DATAGEN_METHOD
+	if dataset_generation_method == DATAGEN_MULTISESSIONS:
+		tensorcfr.generate_dataset_multiple_sessions(
+			dataset_size=dataset_parameters["dataset_size"],
+			dataset_directory=script_directory + "/out/{}/{}_datasets".format(
+				dataset_parameters["domain_name"],
+				dataset_parameters["dataset_size"],
+			),
+			dataset_seed_to_start=dataset_parameters["starting_seed"]
+		)
+	elif dataset_generation_method == DATAGEN_SINGLESESSIONS:
+		tensorcfr.generate_dataset_single_session(
+			dataset_size=dataset_parameters["dataset_size"],
+			dataset_directory=script_directory + "/out/{}/{}_datasets".format(
+				dataset_parameters["domain_name"],
+				dataset_parameters["dataset_size"],
+			),
+			dataset_seed_to_start=dataset_parameters["starting_seed"]
+		)
+	else:
+		raise ValueError("Invalid value {} for 'dataset_generation_method'.".format(dataset_generation_method))
+	print(get_current_timestamp())
 
 	# tensorcfr.generate_dataset_tf_while_loop(
 	# 	# dataset_for_nodes=False,

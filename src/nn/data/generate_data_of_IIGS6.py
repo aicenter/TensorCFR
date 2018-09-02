c1850a3b8add162c7353e827eb7e85724ad99669
Karel Ha <mathemage@gmail.com>
2018-09-03 00:09:14 +0200

    Deactivate `generate_data_of_IIGS6*.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_IIGS6.py
+++ src/nn/data/generate_data_of_IIGS6.py
@@ -7,7 +7,7 @@ from src.domains.available_domains import get_domain_by_name
 from src.utils.other_utils import get_current_timestamp, print_dataset_parameters, get_dataset_parameters
 
 # TODO: Get rid of `ACTIVATE_FILE` hotfix
-ACTIVATE_FILE = True
+ACTIVATE_FILE = False
 
 
 if __name__ == '__main__' and ACTIVATE_FILE:
--- src/nn/data/generate_data_of_IIGS6_200x.py
+++ src/nn/data/generate_data_of_IIGS6_200x.py
@@ -6,7 +6,7 @@ from src.domains.available_domains import get_domain_by_name
 from src.utils.other_utils import get_current_timestamp
 
 # TODO: Get rid of `ACTIVATE_FILE` hotfix
-ACTIVATE_FILE = True
+ACTIVATE_FILE = False
 
 
 if __name__ == '__main__' and ACTIVATE_FILE:

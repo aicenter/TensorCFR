a0b67ffd4f5b7f32ce563a8a2d29b20f5fd779bd
Karel Ha <mathemage@gmail.com>
2018-09-04 14:47:21 +0200

    Create `generate_II_GS3.sh`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ generate_II_GS3.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+size_of_generation=${1:-1}
+num_generations=${2:-1000}
+seed_offset=${3:-0}
+
+last_generation=$((num_generations-1))
+for generation in $(seq 0 $last_generation);
+do
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	seed=$((seed_offset + generation * size_of_generation))
+	echo "Generation #$generation of size $size_of_generation, starting with dataset_seed $seed:"
+	CUDA_VISIBLE_DEVICES=0 python3.6 -m src.nn.data.generate_data_of_IIGS3 --seed $seed --size $size_of_generation
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+done

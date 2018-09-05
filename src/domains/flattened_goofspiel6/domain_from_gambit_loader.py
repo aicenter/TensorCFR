393aa4719307e46d5fe53506edd557d12ba2d5c3
Karel Ha <mathemage@gmail.com>
2018-09-05 16:42:30 +0200

    Deactivate `flattened_goofspiel6/domain_from_gambit_loader.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/domains/flattened_goofspiel6/domain_from_gambit_loader.py
+++ src/domains/flattened_goofspiel6/domain_from_gambit_loader.py
@@ -21,7 +21,11 @@ def get_flattened_goofspiel6():
 	return FlattenedDomain.init_from_gambit_file(path_to_domain_filename, domain_name="IIGS6_s1_bf_ft_gambit_flattened")
 
 
-if __name__ == '__main__':
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
+ACTIVATE_FILE = False
+
+
+if __name__ == '__main__' and ACTIVATE_FILE:
 	goofspiel6 = get_flattened_goofspiel6()
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())

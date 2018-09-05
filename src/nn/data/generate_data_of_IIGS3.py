b6dd9c85e4b8557ff426c54b62cd247d4cb70319
Karel Ha <mathemage@gmail.com>
2018-09-05 16:32:22 +0200

    Deactivate `generate_data_of_IIGS3.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_IIGS3.py
+++ src/nn/data/generate_data_of_IIGS3.py
@@ -3,7 +3,11 @@ import os
 
 from src.nn.data.generate_data import generate_data
 
-if __name__ == '__main__':
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
+ACTIVATE_FILE = False
+
+
+if __name__ == '__main__' and ACTIVATE_FILE:
 	generate_data(
 		"II-GS3_gambit_flattened",
 		script_directory=os.path.dirname(os.path.abspath(__file__)),

df1a6e1c983a19f1bcef930ba8bcd84a94d0710e
Karel Ha <mathemage@gmail.com>
2018-09-05 13:33:03 +0200

    Use `time` instead of `/usr/bin/time`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- run-python-src-files.sh
+++ run-python-src-files.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 find src/ -name *.py | grep -v  __init__ | sed -e 's/\//./g' | rev | cut -d '.' -f 2- | rev \
-    | xargs -I '{}' -t sh -c '/usr/bin/time -f "Duration: %e seconds" python3 -m {} >/dev/null || exit 255'
+    | xargs -I '{}' -t sh -c 'time -f "Duration: %e seconds" python3 -m {} >/dev/null || exit 255'

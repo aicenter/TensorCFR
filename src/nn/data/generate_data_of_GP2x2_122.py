5ef56d78d2c400008112ef6c711edbb844b34f7b
Karel Ha <mathemage@gmail.com>
2018-09-05 16:00:57 +0200

    Deactivate `generate_data_of_GP_cards2x2_122.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data_of_GP2x2_122.py
+++ src/nn/data/generate_data_of_GP2x2_122.py
@@ -3,7 +3,11 @@ import os
 
 from src.nn.data.generate_data import generate_data
 
-if __name__ == '__main__':
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
+ACTIVATE_FILE = False
+
+
+if __name__ == '__main__' and ACTIVATE_FILE:
 	generate_data(
 		"GP_cards2x2_122_gambit_flattened",
 		script_directory=os.path.dirname(os.path.abspath(__file__)),

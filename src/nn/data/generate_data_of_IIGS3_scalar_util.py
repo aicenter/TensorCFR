75f7d28522f68950e58ef697d82b03fa95e088f4
Karel Ha <mathemage@gmail.com>
2018-09-11 17:25:41 +0200

    Create `generate_data_of_IIGS3_scalar_util.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/data/generate_data_of_IIGS3_scalar_util.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+
+from src.nn.data.generate_data import generate_data
+
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
+ACTIVATE_FILE = False
+
+
+if __name__ == '__main__' and ACTIVATE_FILE:
+	generate_data(
+		"II-GS3_scalar_util_gambit_flattened",
+		script_directory=os.path.dirname(os.path.abspath(__file__)),
+		trunk_depth=4
+	)

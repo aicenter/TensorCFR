a5951293e8f07d4af4942c8e0826d6e8bed86c42
Karel Ha <mathemage@gmail.com>
2018-09-18 19:54:33 +0200

    Create `sort_by_public_states.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/features/goofspiel/sort_by_public_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+if __name__ == '__main__':
+	features = pd.read_csv(
+		"IIGS3_features.csv",
+		names=["private_card1", "private_card2", "round1", "round2"],
+		# delimiter=";|,",
+	)
+	print("features:")
+	print(features)
+
+	print("###################################")
+
+	sorted_features = features.sort_values(
+		['round1', 'round2'],
+		kind='mergesort'
+	)
+	print("sorted features:")
+	print(sorted_features)

8f4488057fb08527487f1eec586b890cabfbf8a0
Karel Ha <mathemage@gmail.com>
2018-09-18 19:48:41 +0200

    Implement `sizes_of_public_states` via `groupby()` and `sizes()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/sizes_of_public_states.py
+++ src/nn/features/goofspiel/sizes_of_public_states.py
@@ -3,6 +3,10 @@
 import pandas as pd
 
 if __name__ == '__main__':
-	features = pd.read_csv("IIGS3_features.csv", names=["private_card1", "private_card2", "round1", "round2"])
-	# features = pd.read_csv("IIGS3_1_3_false_true_lvl7.csv")
-	print(features.head())
+	features = pd.read_csv(
+		"IIGS3_features.csv",
+		names=["private_card1", "private_card2", "round1", "round2"],
+		# delimiter=";|,",
+	)
+	print(features)
+	print(features.groupby(['round1', 'round2']).size())

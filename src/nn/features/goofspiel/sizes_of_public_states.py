82414cedee61cd780a4d85d01e4e11ae6ab35840
Karel Ha <mathemage@gmail.com>
2018-09-19 12:11:13 +0200

    Switch to `IIGS3_1_3_false_true_lvl7.csv` in `sizes_of_public_states.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/sizes_of_public_states.py
+++ src/nn/features/goofspiel/sizes_of_public_states.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 if __name__ == '__main__':
 	features = pd.read_csv(
-		"IIGS3_features.csv",
+		"IIGS3_1_3_false_true_lvl7.csv",
 		names=["private_card1", "private_card2", "round1", "round2"],
-		# delimiter=";|,",
+		delimiter=";|,",
 	)
 	print(features)
 	print(features.groupby(['round1', 'round2']).size())

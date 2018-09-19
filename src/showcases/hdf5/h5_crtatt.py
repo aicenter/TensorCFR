d17a9898e5e9f98612bd0513097b2e62c20c13ae
Karel Ha <mathemage@gmail.com>
2018-09-19 17:10:21 +0200

    Showcase attributes in HDF5
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/showcases/hdf5/h5_crtatt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#
+# This examaple creates and writes two attributes on the "dset" dataset created by h5_crtdat.py.
+#
+import h5py
+import numpy as np
+
+#
+# Open an existing file using defaut properties.
+#
+file = h5py.File('dset.h5', 'r+')
+#
+# Open "dset" dataset.
+#
+dataset = file['/dset']
+#
+# Create string attribute.
+#
+attr_string = "Meter per second"
+dataset.attrs["Units"] = attr_string
+#
+# Create integer array attribute.
+#
+attr_data = np.zeros(2)
+attr_data[0] = 100
+attr_data[1] = 200
+#
+#
+dataset.attrs.create("Speed", attr_data, (2,), h5py.h5t.STD_I32BE)
+#
+# Close the file before exiting
+#
+file.close()

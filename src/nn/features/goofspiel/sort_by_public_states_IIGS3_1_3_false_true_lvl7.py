4d6acf5494df27a1604cbbe28156538eda0b7e4d
Karel Ha <mathemage@gmail.com>
2018-09-19 15:58:32 +0200

    Use `csv_` variables in `sort_by_public*.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/sort_by_public_states_IIGS3_1_3_false_true_lvl7.py
+++ src/nn/features/goofspiel/sort_by_public_states_IIGS3_1_3_false_true_lvl7.py
@@ -3,10 +3,12 @@
 import pandas as pd
 
 if __name__ == '__main__':
+	csv_basename = "IIGS3_1_3_false_true_lvl7"
+	csv_filename = "{}.csv".format(csv_basename)
 	features = pd.read_csv(
-		"IIGS3_features.csv",
+		csv_filename,
 		names=["private_card1", "private_card2", "round1", "round2"],
-		# delimiter=";|,",
+		delimiter=";|,",
 	)
 	# TODO join two CSV files
 	print("features:")

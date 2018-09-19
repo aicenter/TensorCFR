29f45be302d9dc78183feb9a2512f26c8fdb511f
Karel Ha <mathemage@gmail.com>
2018-09-19 18:14:42 +0200

    De-activate H5DF showcase scripts that need an external file
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/showcases/hdf5/h5_crtatt.py
+++ src/showcases/hdf5/h5_crtatt.py
@@ -6,29 +6,33 @@
 import h5py
 import numpy as np
 
-#
-# Open an existing file using defaut properties.
-#
-file = h5py.File('dset.h5', 'r+')
-#
-# Open "dset" dataset.
-#
-dataset = file['/dset']
-#
-# Create string attribute.
-#
-attr_string = "Meter per second"
-dataset.attrs["Units"] = attr_string
-#
-# Create integer array attribute.
-#
-attr_data = np.zeros(2)
-attr_data[0] = 100
-attr_data[1] = 200
-#
-#
-dataset.attrs.create("Speed", attr_data, (2,), h5py.h5t.STD_I32BE)
-#
-# Close the file before exiting
-#
-file.close()
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
+ACTIVATE_FILE = False
+
+if __name__ == '__main__' and ACTIVATE_FILE:
+	#
+	# Open an existing file using defaut properties.
+	#
+	file = h5py.File('dset.h5', 'r+')
+	#
+	# Open "dset" dataset.
+	#
+	dataset = file['/dset']
+	#
+	# Create string attribute.
+	#
+	attr_string = "Meter per second"
+	dataset.attrs["Units"] = attr_string
+	#
+	# Create integer array attribute.
+	#
+	attr_data = np.zeros(2)
+	attr_data[0] = 100
+	attr_data[1] = 200
+	#
+	#
+	dataset.attrs.create("Speed", attr_data, (2,), h5py.h5t.STD_I32BE)
+	#
+	# Close the file before exiting
+	#
+	file.close()
--- src/showcases/hdf5/h5_rdwt.py
+++ src/showcases/hdf5/h5_rdwt.py
@@ -6,37 +6,41 @@
 import h5py
 import numpy as np
 
-#
-# Open an existing file using default properties.
-#
-file = h5py.File('dset.h5', 'r+')
-#
-# Open "dset" dataset under the root group.
-#
-dataset = file['/dset']
-#
-# Initialize data object with 0.
-#
-data = np.zeros((4, 6))
-#
-# Assign new values
-#
-for i in range(4):
-	for j in range(6):
-		data[i][j] = i * 6 + j + 1
-#
-# Write data
-#
-print("Writing data...")
-dataset[...] = data
-#
-# Read data back and print it.
-#
-print("Reading data back...")
-data_read = dataset[...]
-print("Printing data...")
-print(data_read)
-#
-# Close the file before exiting
-#
-file.close()
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
+ACTIVATE_FILE = False
+
+if __name__ == '__main__' and ACTIVATE_FILE:
+	#
+	# Open an existing file using default properties.
+	#
+	file = h5py.File('dset.h5', 'r+')
+	#
+	# Open "dset" dataset under the root group.
+	#
+	dataset = file['/dset']
+	#
+	# Initialize data object with 0.
+	#
+	data = np.zeros((4, 6))
+	#
+	# Assign new values
+	#
+	for i in range(4):
+		for j in range(6):
+			data[i][j] = i * 6 + j + 1
+	#
+	# Write data
+	#
+	print("Writing data...")
+	dataset[...] = data
+	#
+	# Read data back and print it.
+	#
+	print("Reading data back...")
+	data_read = dataset[...]
+	print("Printing data...")
+	print(data_read)
+	#
+	# Close the file before exiting
+	#
+	file.close()

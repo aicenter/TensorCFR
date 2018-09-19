31f3d5fbc5d1a854233cab94ec10de3e67cbf5cd
Karel Ha <mathemage@gmail.com>
2018-09-19 12:14:00 +0200

    Extract `csv_filename`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/sizes_of_public_states.py
+++ src/nn/features/goofspiel/sizes_of_public_states.py
@@ -3,8 +3,9 @@
 import pandas as pd
 
 if __name__ == '__main__':
+	csv_filename = "IIGS3_1_3_false_true_lvl7.csv"
 	features = pd.read_csv(
-		"IIGS3_1_3_false_true_lvl7.csv",
+		csv_filename,
 		names=["private_card1", "private_card2", "round1", "round2"],
 		delimiter=";|,",
 	)

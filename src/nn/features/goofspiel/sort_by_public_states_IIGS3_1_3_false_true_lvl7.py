9bf8da78337cdf002dd1463ea0277363f6d9c06f
Karel Ha <mathemage@gmail.com>
2018-09-25 14:57:53 +0200

    Rename `csv_` to `features_`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/sort_by_public_states_IIGS3_1_3_false_true_lvl7.py
+++ src/nn/features/goofspiel/sort_by_public_states_IIGS3_1_3_false_true_lvl7.py
@@ -6,16 +6,17 @@ import pandas as pd
 
 if __name__ == '__main__':
 	script_directory = os.path.dirname(os.path.abspath(__file__))
-	csv_basename = "{}/IIGS3_1_3_false_true_lvl7".format(script_directory)
-	csv_filename = "{}.csv".format(csv_basename)
+	features_basename = "{}/IIGS3_1_3_false_true_lvl7".format(script_directory)
+	features_filename = "{}.csv".format(features_basename)
 	features = pd.read_csv(
-		csv_filename,
+		features_filename,
 		names=["private_card1", "private_card2", "round1", "round2"],
 		delimiter=";|,",
 	)
-	# TODO join two CSV files
 	print("features:")
 	print(features)
+
+	# TODO join two CSV files
 
 	print("###################################")
 
@@ -26,5 +27,5 @@ if __name__ == '__main__':
 	print("sorted features:")
 	print(sorted_features)
 
-	# TODO reorder columns
-	# TODO write to a CSV
+# TODO reorder columns
+# TODO write to a CSV

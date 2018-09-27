447545656c04aa6af40a2b8043745e570c540f9b
Karel Ha <mathemage@gmail.com>
2018-09-27 17:02:20 +0200

    Extract `get_features_dataframe()` and `get_concatenated_dataframe()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/prepare_datasets_IIGS3_1_3_false_true_lvl7.py
+++ src/nn/features/goofspiel/prepare_datasets_IIGS3_1_3_false_true_lvl7.py
@@ -26,20 +26,41 @@ def split_by_public_states():
 		print(sorted_concatenated[start_indices[i]:start_indices[i] + size])
 
 
+def get_features_dataframe():
+	features_filename = "{}/{}.csv".format(script_directory, features_basename)
+	features_dataframe = pd.read_csv(
+		features_filename,
+		names=["private_card1", "private_card2", "round1", "round2"],
+		delimiter=";|,",
+	)
+	print("features:")
+	print(features_dataframe)
+	return features_dataframe
+
+
+def get_concatenated_dataframe():
+	concatenated_dataframe = pd.concat(
+		[features, reaches_to_values],
+		axis=1,
+	)[[
+		"round1", "round2",
+		"private_card1", "private_card2",
+		"node_to_infoset",
+		"nodal_reach",
+		"nodal_expected_value"
+	]]
+	print("concatenated:")
+	print(concatenated_dataframe)
+	return concatenated_dataframe
+
+
 if __name__ == '__main__':
 	script_directory = os.path.dirname(os.path.abspath(__file__))
 	pd.set_option('display.max_columns', 500)
 	pd.set_option('display.width', 1000)
 
 	features_basename = "IIGS3_1_3_false_true_lvl7"
-	features_filename = "{}/{}.csv".format(script_directory, features_basename)
-	features = pd.read_csv(
-		features_filename,
-		names=["private_card1", "private_card2", "round1", "round2"],
-		delimiter=";|,",
-	)
-	print("features:")
-	print(features)
+	features = get_features_dataframe()
 
 	print("###################################")
 
@@ -59,19 +80,7 @@ if __name__ == '__main__':
 
 	print("###################################")
 
-	concatenated = pd.concat(
-		[features, reaches_to_values],
-		axis=1,
-		# sort=True
-	)[[
-		"round1", "round2",
-		"private_card1", "private_card2",
-		"node_to_infoset",
-		"nodal_reach",
-		"nodal_expected_value"
-	]]
-	print("concatenated:")
-	print(concatenated)
+	concatenated = get_concatenated_dataframe()
 
 	print("###################################")
 

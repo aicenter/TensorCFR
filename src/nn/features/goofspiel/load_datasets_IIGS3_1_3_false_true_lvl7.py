f34170fc8273f2a7518182544716c8480f2c47fc
Karel Ha <mathemage@gmail.com>
2018-10-01 11:42:14 +0200

    Define `{features,targets}_batch` from iterators
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/load_datasets_IIGS3_1_3_false_true_lvl7.py
+++ src/nn/features/goofspiel/load_datasets_IIGS3_1_3_false_true_lvl7.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
-from src.utils.tf_utils import get_default_config_proto
+from src.utils.tf_utils import get_default_config_proto, print_tensors
 
 FEATURES_BASENAME = "IIGS3_1_3_false_true_lvl7"
 
@@ -21,12 +21,24 @@ if __name__ == '__main__':
 	# Assume that each row of `features` corresponds to the same row as `labels`.
 	assert features.shape[0] == targets.shape[0]
 
-	features_placeholder = tf.placeholder(features.dtype, features.shape)
-	targets_placeholder = tf.placeholder(targets.dtype, targets.shape)
+	features_placeholder = tf.placeholder(features.dtype, features.shape, name="features")
+	targets_placeholder = tf.placeholder(targets.dtype, targets.shape, name="targets")
 
-	dataset = tf.data.Dataset.from_tensor_slices((features_placeholder, targets_placeholder))
-	iterator = dataset.make_initializable_iterator()
-	# iterator = dataset.make_one_shot_iterator()   # TODO or this one
+	features_dataset = tf.data.Dataset.from_tensor_slices(features_placeholder)
+	targets_dataset = tf.data.Dataset.from_tensor_slices(targets_placeholder)
+	feature_iterator = features_dataset.make_initializable_iterator()
+	target_iterator = targets_dataset.make_initializable_iterator()
+	features_batch = feature_iterator.get_next(name="features_batch")
+	targets_batch = target_iterator.get_next(name="targets_batch")
 
 	with tf.Session(config=get_default_config_proto()) as sess:
-		sess.run(iterator.initializer, feed_dict={features_placeholder: features, targets_placeholder: targets})
+		sess.run(feature_iterator.initializer, feed_dict={features_placeholder: features})
+		sess.run(target_iterator.initializer, feed_dict={targets_placeholder: targets})
+		batch_index = 0
+		while True:
+			try:
+				print("Batch #{}:".format(batch_index))
+				print_tensors(sess, [features_batch, targets_batch])
+			except tf.errors.OutOfRangeError:
+				break
+			batch_index += 1

1435fb14edde7779e2f57a1dc62e1e8f64783351
Karel Ha <mathemage@gmail.com>
2018-10-01 10:22:58 +0200

    Check for no files in `dataset_dir`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/prepare_datasets_IIGS3_1_3_false_true_lvl7.py
+++ src/nn/features/goofspiel/prepare_datasets_IIGS3_1_3_false_true_lvl7.py
@@ -72,26 +72,30 @@ if __name__ == '__main__':
 	script_directory = os.path.dirname(os.path.abspath(__file__))
 	features_basename = "IIGS3_1_3_false_true_lvl7"
 	features = get_features_dataframe()
-	filenames = get_files_in_directory_recursively(rootdir="{}/reach_value_datasets".format(script_directory))
+	dataset_dir = "{}/reach_value_datasets".format(script_directory)
+	filenames = get_files_in_directory_recursively(rootdir=dataset_dir)
 
-	feature_arrays, target_arrays = [], []
-	for i, reaches_to_values_filename in enumerate(filenames):
-		print("#{}-th reaches_to_values_filename == {}".format(i, reaches_to_values_filename))
-		reaches_to_values = get_reaches_to_values_dataframe()
-		concatenated = get_concatenated_dataframe(features, reaches_to_values)
-		sorted_concatenated = get_sorted_dataframes(concatenated)
+	if not filenames:
+		print("No files in {}".format(dataset_dir))
+	else:
+		feature_arrays, target_arrays = [], []
+		for i, reaches_to_values_filename in enumerate(filenames):
+			print("#{}-th reaches_to_values_filename == {}".format(i, reaches_to_values_filename))
+			reaches_to_values = get_reaches_to_values_dataframe()
+			concatenated = get_concatenated_dataframe(features, reaches_to_values)
+			sorted_concatenated = get_sorted_dataframes(concatenated)
 
-		np_dataset = sorted_concatenated.values
-		feature_arrays.append(np_dataset[:, :-1])
-		target_arrays.append(np_dataset[:, -1])
+			np_dataset = sorted_concatenated.values
+			feature_arrays.append(np_dataset[:, :-1])
+			target_arrays.append(np_dataset[:, -1])
 
-	np_features = np.stack(feature_arrays)  # shape [#seed_of_the_batch, #nodes, #features]
-	print(np_features)
-	print("np_features.shape == {}".format(np_features.shape))
-	np_targets = np.stack(target_arrays)  # shape [#seed_of_the_batch, #nodes]
-	print(np_targets)
-	print("np_targets.shape == {}".format(np_targets.shape))
+		np_features = np.stack(feature_arrays)  # shape [#seed_of_the_batch, #nodes, #features]
+		print(np_features)
+		print("np_features.shape == {}".format(np_features.shape))
+		np_targets = np.stack(target_arrays)  # shape [#seed_of_the_batch, #nodes]
+		print(np_targets)
+		print("np_targets.shape == {}".format(np_targets.shape))
 
-	dataset_filename = "{}/{}_numpy_dataset.npz".format(script_directory, features_basename)
-	np.savez_compressed(dataset_filename, features=np_features, targets=np_targets)
-	verify_npz(dataset_filename, np_features, np_targets)
+		dataset_filename = "{}/{}_numpy_dataset.npz".format(script_directory, features_basename)
+		np.savez_compressed(dataset_filename, features=np_features, targets=np_targets)
+		verify_npz(dataset_filename, np_features, np_targets)

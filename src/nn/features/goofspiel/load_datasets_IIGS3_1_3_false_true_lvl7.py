1f4b2055db638965546b56ff88172c8e00390f5b
Karel Ha <mathemage@gmail.com>
2018-10-01 11:23:44 +0200

    Switch to `{features,targets}_placeholder`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/load_datasets_IIGS3_1_3_false_true_lvl7.py
+++ src/nn/features/goofspiel/load_datasets_IIGS3_1_3_false_true_lvl7.py
@@ -7,6 +7,8 @@ import os
 import numpy as np
 import tensorflow as tf
 
+from src.utils.tf_utils import get_default_config_proto
+
 FEATURES_BASENAME = "IIGS3_1_3_false_true_lvl7"
 
 if __name__ == '__main__':
@@ -19,4 +21,12 @@ if __name__ == '__main__':
 	# Assume that each row of `features` corresponds to the same row as `labels`.
 	assert features.shape[0] == targets.shape[0]
 
-	dataset = tf.data.Dataset.from_tensor_slices((features, targets))
+	features_placeholder = tf.placeholder(features.dtype, features.shape)
+	targets_placeholder = tf.placeholder(targets.dtype, targets.shape)
+
+	dataset = tf.data.Dataset.from_tensor_slices((features_placeholder, targets_placeholder))
+	iterator = dataset.make_initializable_iterator()
+	# iterator = dataset.make_one_shot_iterator()   # TODO or this one
+
+	with tf.Session(config=get_default_config_proto()) as sess:
+		sess.run(iterator.initializer, feed_dict={features_placeholder: features, targets_placeholder: targets})

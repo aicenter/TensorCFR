411635523a2eadbedbdb1ed98a972ec2be2048b9
Karel Ha <mathemage@gmail.com>
2018-10-03 20:51:39 +0200

    Copy `sizes_of_public_states_IIGS6*` from `IIGS3`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/IIGS6/sizes_of_public_states_IIGS6_1_6_false_true_lvl10.py
+++ src/nn/features/goofspiel/IIGS6/sizes_of_public_states_IIGS6_1_6_false_true_lvl10.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 import os
 
+import numpy as np
 import pandas as pd
+
+from src.utils.other_utils import get_features_dataframe
 
 if __name__ == '__main__':
 	script_directory = os.path.dirname(os.path.abspath(__file__))
 	csv_basename = "IIGS6_1_6_false_true_lvl10"
 	csv_filename = os.path.join(script_directory, "{}.csv".format(csv_basename))
-	features = pd.read_csv(
-		csv_filename,
-		names=["private_card1", "private_card2", "private_card3", "round1", "round2", "round3"],
-		delimiter=";|,",
-	)
-	print(features)
+	features = get_features_dataframe(csv_filename)   # TODO change feature names
 
-	public_states_sizes = features.groupby(['round1', 'round2', 'round3']).size()
+	grouped_features = features.groupby(['round1', 'round2', 'round3'])
+	print("grouped_features:\n{}.".format(grouped_features.head()))
+	public_states_sizes = grouped_features.size()
 	csv_output_filename = "{}_public_states_sizes.csv".format(csv_basename)
 	public_states_sizes.to_csv(
 		csv_output_filename,

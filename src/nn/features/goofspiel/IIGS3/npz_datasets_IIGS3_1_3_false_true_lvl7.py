467483a50856d14d2f065f433896c36b56281351
Karel Ha <mathemage@gmail.com>
2018-10-03 17:25:39 +0200

    Increase `printoptions` linewidth in `npz_datasets_IIGS3*`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/IIGS3/npz_datasets_IIGS3_1_3_false_true_lvl7.py
+++ src/nn/features/goofspiel/IIGS3/npz_datasets_IIGS3_1_3_false_true_lvl7.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 from src.utils.other_utils import get_files_in_directory_recursively, get_one_hot_flattened, get_features_dataframe
 
-FEATURES_BASENAME = "IIGS3_1_3_false_true_lvl7"
+# FEATURES_BASENAME = "IIGS3_1_3_false_true_lvl7"
+FEATURES_BASENAME = "IIGS3_history"
 N_CARDS = 3
 FEATURE_COLUMNS = [
 	"round1", "round2",
@@ -62,7 +63,7 @@ def prepare_dataset():
 	"""
 	pd.set_option('display.max_columns', 500)
 	pd.set_option('display.width', 1000)
-	np.set_printoptions(edgeitems=20, suppress=True)
+	np.set_printoptions(edgeitems=20, suppress=True, linewidth=200)
 
 	script_directory = os.path.dirname(os.path.abspath(__file__))
 	features_filename = "{}/{}.csv".format(script_directory, FEATURES_BASENAME)

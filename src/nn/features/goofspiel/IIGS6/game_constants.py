6c5174d83d9364e17de3e4a7ea7a06dc4c3fb50b
Karel Ha <mathemage@gmail.com>
2018-10-03 22:08:47 +0200

    Add missing comma in `game_constants.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/IIGS6/game_constants.py
+++ src/nn/features/goofspiel/IIGS6/game_constants.py
@@ -8,7 +8,7 @@ NAMES_OF_FEATURE_CSV = [
 	"round1", "round2", "round3"
 ]
 FEATURE_COLUMNS = [
-	"round1", "round2", "round3"
+	"round1", "round2", "round3",
 	"private_card1", "private_card2", "private_card3",
 	"opponent_card1", "opponent_card2", "opponent_card3",
 	"nodal_reach"

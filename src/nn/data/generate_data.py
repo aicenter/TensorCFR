7c54c1edc0dceb83e2d0abe5be0447401d6d07a6
Karel Ha <mathemage@gmail.com>
2018-10-04 17:32:43 +0200

    Remove extra lines from `generate_data.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/data/generate_data.py
+++ src/nn/data/generate_data.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
 from src.commons.constants import DEFAULT_DATAGEN_METHOD, DATAGEN_MULTISESSIONS, DATAGEN_SINGLESESSIONS
 from src.domains.available_domains import get_domain_by_name
@@ -7,7 +6,6 @@ from src.utils.other_utils import get_dataset_parameters, print_dataset_paramete
 
 
 def generate_data(domain_name, script_directory, trunk_depth):
-
 	dataset_parameters = get_dataset_parameters(domain_name)
 	print_dataset_parameters(dataset_parameters)
 	domain = get_domain_by_name(dataset_parameters["domain_name"])
@@ -42,4 +40,3 @@ def generate_data(domain_name, script_directory, trunk_depth):
 	else:
 		raise ValueError("Invalid value {} for 'dataset_generation_method'.".format(dataset_generation_method))
 	print(get_current_timestamp())
-

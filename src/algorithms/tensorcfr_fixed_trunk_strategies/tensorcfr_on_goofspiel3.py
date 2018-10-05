b36f29799801eda2a427cdaa6345a682ebb64099
Karel Ha <mathemage@gmail.com>
2018-10-05 17:45:01 +0200

    Reformat `tensorcfr_on_goospiel*.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_fixed_trunk_strategies/tensorcfr_on_goofspiel3.py
+++ src/algorithms/tensorcfr_fixed_trunk_strategies/tensorcfr_on_goofspiel3.py
@@ -1,18 +1,18 @@
 from src import utils
 from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
-from src.algorithms.tensorcfr_flattened_domains.TensorCFRFlattenedDomains import get_cfr_strategies
 from src.domains.available_domains import get_domain_by_name
 
 if __name__ == '__main__':
 	domain = get_domain_by_name("II-GS3_gambit_flattened")
 	tensorcfr = TensorCFRFixedTrunkStrategies(domain)
 	average_strategies = tensorcfr.cfr_strategies_after_fixed_trunk(
-			total_steps=1000,
-			delay=0,
-			register_strategies_on_step=[1, 500, 999]
-	)   # TODO verify the results (final average strategies) via `gtlibrary`
+		total_steps=1000,
+		delay=0,
+		register_strategies_on_step=[1, 500, 999]
+	)  # TODO verify the results (final average strategies) via `gtlibrary`
 
 	utils.gtlibrary.export_average_strategies_to_json(
 		domain,
 		average_strategies,
-		"GS3_average_strategies")
+		"GS3_average_strategies"
+	)

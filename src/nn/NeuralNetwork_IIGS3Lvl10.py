48903ed142a8526137c9f7f82064af598a9ea667
Karel Ha <mathemage@gmail.com>
2018-10-09 20:34:42 +0200

    Implement concatenated `context[]`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/NeuralNetwork_IIGS3Lvl10.py
+++ src/nn/NeuralNetwork_IIGS3Lvl10.py
@@ -70,12 +70,16 @@ class NeuralNetwork_IIGS3Lvl10:
 			public_states_tensors = [None] * self.NUM_PUBLIC_STATES
 			public_state_means = [None] * self.NUM_PUBLIC_STATES
 			public_state_maxes = [None] * self.NUM_PUBLIC_STATES
+			context = [None] * self.NUM_PUBLIC_STATES
 			for i, public_state_list in enumerate(self.public_states_lists):
 				public_states_tensors[i] = tf.stack(public_state_list, axis=-1, name="public_state{}".format(i))
 				public_state_means[i] = tf.reduce_mean(public_states_tensors[i], axis=-1, name="public_state_mean{}".format(i))
 				public_state_maxes[i] = tf.reduce_max(public_states_tensors[i], axis=-1, name="public_state_maxes{}".format(i))
-
-			# TODO concat
+				context[i] = tf.concat(
+					[public_state_means[i], public_state_maxes[i]],
+					axis=-1,
+					name="context{}".format(i)
+				)
 
 	def construct_value_regressor(self, args):
 		"""

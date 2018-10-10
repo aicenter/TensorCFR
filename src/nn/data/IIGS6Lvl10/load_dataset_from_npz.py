1da3eb1fe9a0015b2b49104abcb5f4b871ddaacb
Karel Ha <mathemage@gmail.com>
2018-10-10 17:50:01 +0200

    Copy `load_dataset_from_npz.py` from IIGS3
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/data/IIGS6Lvl10/load_dataset_from_npz.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+
+import numpy as np
+
+from src.nn.data.DatasetFromNPZ import DatasetFromNPZ
+
+if __name__ == "__main__":
+	import argparse
+
+	np.set_printoptions(edgeitems=20, suppress=True, linewidth=200)
+	script_directory = os.path.dirname(os.path.abspath(__file__))
+	np.random.seed(42)
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
+	parser.add_argument("--epochs", default=2, type=int, help="Number of epochs.")
+	args = parser.parse_args()
+
+	train_file = "{}/80-10-10/IIGS6_1_6_false_true_lvl10_train.npz".format(script_directory)
+	train = DatasetFromNPZ(train_file)
+
+	for epoch in range(args.epochs):
+		print("Epoch #{}:".format(epoch))
+		while not train.epoch_finished():
+			print("Batch #{}:".format(train.batch_id))
+			features, targets = train.next_batch(args.batch_size)
+			print("Features:\n{}".format(features))
+			print("Targets:\n{}".format(targets))

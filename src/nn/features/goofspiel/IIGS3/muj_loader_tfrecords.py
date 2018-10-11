3b1631510863fd91dda78ba3618ca835099cbe33
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-10-11 15:29:09 +0200

    Refactor loading of `*.tfrecord` files
    Signed-off-by: janrudolf <jan.rudolf.steeldog@gmail.com>

--- src/nn/features/goofspiel/IIGS3/muj_loader_tfrecords.py
+++ src/nn/features/goofspiel/IIGS3/muj_loader_tfrecords.py
@@ -1,34 +1,14 @@
 import tensorflow as tf
-import numpy as np
 
-
-# with tf.Session() as sess:
-# 	feature = {'train/input': tf.FixedLenFeature([], tf.float32),
-# 			   'train/target': tf.FixedLenFeature([], tf.float32)}
-# 	# Create a list of filenames and pass it to a queue
-# 	filename_queue = tf.train.string_input_producer(data_path, num_epochs=1)
-# 	# Define a reader and read the next record
-# 	reader = tf.TFRecordReader()
-# 	_, serialized_example = reader.read(filename_queue)
-# 	# Decode the record read by the reader
-# 	features = tf.parse_single_example(serialized_example, features=feature)
-# 	# Convert the image data from string back to the numbers
-# 	image = tf.decode_raw(features['train/input'], tf.float32)
-#
-# 	# # Cast label data into int32
-# 	# label = tf.cast(features['train/target'], tf.int32)
-# 	# # Reshape image data into the original shape
-# 	# image = tf.reshape(image, [224, 224, 3])
-# 	#
-# 	# # Any preprocessing here ...
-#
-# 	# Creates batches by randomly shuffling tensors
-# 	# images, labels = tf.train.shuffle_batch([image, label], batch_size=10, capacity=30, num_threads=1,
-# 	# 										min_after_dequeue=10)
 from tensorflow.python.framework.errors_impl import OutOfRangeError
 
 
-def parser(record):
+NUMBER_DATASET_FILES = 1000
+BATCH_SIZE = 8
+NUM_PARALLEL_CALLS = 2
+
+
+def _parser(record):
 	keys_to_features = {
 		'sample_input': tf.FixedLenFeature((7,), tf.float32),
 		'sample_target': tf.FixedLenFeature((1,), tf.float32)
@@ -36,23 +16,30 @@ def parser(record):
 	parsed = tf.parse_single_example(record, keys_to_features)
 	return parsed["sample_input"], parsed["sample_target"]
 
+
 if __name__ == '__main__':
-	data_path = ['train_{}.tfrecord'.format(x) for x in range(1)]  # address to save the hdf5 file
+	data_path = ['train_{}.tfrecord'.format(x) for x in range(NUMBER_DATASET_FILES)]
 
 	dataset = tf.data.TFRecordDataset(filenames=data_path)
+	dataset = dataset.map(_parser, num_parallel_calls=NUM_PARALLEL_CALLS)
+	dataset = dataset.batch(BATCH_SIZE)
+	iterator = dataset.make_one_shot_iterator()
+	features = iterator.get_next()
+
 	# dataset = dataset.apply(
 	# 	tf.contrib.data.map_and_batch(parser, 32)
 	# )
 	# dataset = dataset.prefetch(buffer_size=2)
 
-	dataset = dataset.map(parser)
-	iterator = dataset.make_one_shot_iterator()
-	features = iterator.get_next()
 
 	with tf.Session() as session:
 		while True:
 			try:
+				# Runs `features` TF op and return a tuple with Numpy arrays
 				features_data = session.run(features)
+				# Outputs features_data[0] (sample_input) with shape (BATCH_SIZE,7)
+				# and features_data[1] (sample_target) with shape (BATCH_SIZE,1)
 				print('Sample input ', features_data[0], ', sample target ', features_data[1])
 			except OutOfRangeError:
+				# Ends the cycle if we run out of data samples
 				break

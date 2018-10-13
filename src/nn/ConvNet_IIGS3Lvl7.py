7b4fd7f2b4a97ab823dc6705745e96fc868cccde
Karel Ha <mathemage@gmail.com>
2018-10-13 21:15:54 +0200

    Define concatenated `public_state_contexts`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/ConvNet_IIGS3Lvl7.py
+++ src/nn/ConvNet_IIGS3Lvl7.py
@@ -112,16 +112,19 @@ class ConvNet_IIGS3Lvl7:
 				tf.reduce_max(group, axis=-1, name="max_of_public_state{}".format(i))
 				for i, group in enumerate(groups_by_public_states)
 			]
+			public_state_contexts = [
+				tf.concat(
+					[public_state_means[i], public_state_maxes[i]],
+					axis=-1,
+					name="context_of_public_state{}".format(i)
+				)
+				for i in range(len(groups_by_public_states))
+			]
 			raise NotImplementedError
 
-			# TODO concat contexts
 			# TODO concat with representations
 
-		# 	public_states_tensors = [None] * self.NUM_PUBLIC_STATES
-		# 	context = [None] * self.NUM_PUBLIC_STATES
-		# 	for i, public_state_list in enumerate(self.public_states_lists):
-		# 		public_states_tensors[i] = tf.stack(public_state_list, axis=-1, name="nodes_of_public_state{}".format(i))
-		# 		with tf.variable_scope("public_state{}".format(i)):
+		# 		with tf.variable_scope("public_state{}".format(i)): TODO
 		# 			context[i] = tf.concat(
 		# 				[public_state_means[i], public_state_maxes[i]],
 		# 				axis=-1,

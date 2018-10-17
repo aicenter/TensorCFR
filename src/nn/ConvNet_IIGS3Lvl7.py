43f7ecd4056007d94d28e9a0f62f732858f76433
Karel Ha <mathemage@gmail.com>
2018-10-17 17:55:13 +0200

    Sync CNN for IIGS3 with version for IIGS6
    - extract `huber_loss`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/ConvNet_IIGS3Lvl7.py
+++ src/nn/ConvNet_IIGS3Lvl7.py
@@ -201,15 +201,21 @@ class ConvNet_IIGS3Lvl7:
 		print("predictions constructed")
 		self.print_operations_count()
 
-	def construct_training(self):
-		with tf.variable_scope("metrics"):
-			self.loss = tf.losses.huber_loss(self.targets, self.predictions, scope="huber_loss")
+	def construct_loss(self):
+		with tf.variable_scope("loss"):
+			self.loss = self.huber_loss
 			print("loss constructed")
 			self.print_operations_count()
+
+	def construct_training(self):
+		with tf.variable_scope("metrics"):
+			with tf.variable_scope("huber_loss"):
+				self.huber_loss = tf.losses.huber_loss(self.targets, self.predictions, scope="huber_loss")
 			with tf.variable_scope("mean_squared_error"):
 				self.mean_squared_error = tf.reduce_mean(tf.squared_difference(self.targets, self.predictions))
 			with tf.variable_scope("l_infinity_error"):
 				self.l_infinity_error = tf.norm(self.targets - self.predictions, ord=np.inf)
+			self.construct_loss()
 		print("Metrics constructed")
 		self.print_operations_count()
 		with tf.variable_scope("optimization"):

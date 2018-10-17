7fd408557be780b421de871df9cd1f2e3c3f92b2
Karel Ha <mathemage@gmail.com>
2018-10-17 23:24:34 +0200

    Include nn_iigs6_epochs_experiments.sh
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ nn_iigs6_epochs_experiments.sh
@@ -0,0 +1,9 @@
+# CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 500
+# CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 5000
+# CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 50000
+# CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 500000
+
+# medium-size dataset: 210 seed files
+CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 500
+CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 5000
+CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 50000

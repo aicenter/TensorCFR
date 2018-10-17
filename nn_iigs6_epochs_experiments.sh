4094fa721b23b2d4e9c6517271a9098dc6bc71fb
Karel Ha <mathemage@gmail.com>
2018-10-17 23:36:26 +0200

    Add baseline experiments
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- nn_iigs6_epochs_experiments.sh
+++ nn_iigs6_epochs_experiments.sh
@@ -5,6 +5,12 @@
 # CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 500000
 
 # medium-size dataset: 210 seed files
-CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 500
-CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 5000
-CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 50000
+#CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 500
+#CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 5000
+#CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 50000
+
+# baselines (on local)
+PYTHON=/usr/bin/python3
+${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 50
+${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 500
+${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 5000

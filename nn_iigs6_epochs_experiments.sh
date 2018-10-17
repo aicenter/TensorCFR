0b24c481ae0c8db2c22a371756f3aeb3ee725b2e
Karel Ha <mathemage@gmail.com>
2018-10-17 23:28:33 +0200

    Add shebang
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- nn_iigs6_epochs_experiments.sh
+++ nn_iigs6_epochs_experiments.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 500
 # CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 5000
 # CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 50000

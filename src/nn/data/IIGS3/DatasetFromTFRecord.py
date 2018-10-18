0d63c60f7aa43b448a49f76a0663133ab63a46bf
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-10-18 15:16:18 +0200

    Make DatasetFromTFRecord as a class
    Signed-off-by: janrudolf <jan.rudolf.steeldog@gmail.com>

--- src/nn/data/IIGS3/DatasetFromTFRecord.py
+++ src/nn/data/IIGS3/DatasetFromTFRecord.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
-
 import os
 
 import numpy as np
+import tensorflow as tf
 
 
 class DatasetFromTFRecord:
-	def __init__(self, filename, shuffle_batches=True):
+	def __init__(self,
+				 batch_size=1,
+				 dataset_files=[],
+				 feature_input_size=1,
+				 feature_target_size=1,
+				 number_of_epochs=1,
+				 number_parallel_calls=None,
+				 shuffle_batches=True):
 		self._batch_id = 0
-		with np.load(filename) as data:
-			self._features = data["features"]
-			self._targets = data["targets"]
+		self._batch_size = batch_size
+		self._dataset_files = dataset_files
+		self._feature_input_size = feature_input_size
+		self._feature_target_size = feature_target_size
+		self._features_op = None # TensorFlow operation
+		self._number_of_epochs = number_of_epochs
+		self._number_parallel_calls = number_parallel_calls
+		self._variable_scope = 'DatasetFromTFRecord'
 
-		self._shuffle_batches = shuffle_batches
-		self._permutation = np.random.permutation(len(self._features)) if self._shuffle_batches \
-			else range(len(self._features))
+		self._init()
 
 	@property
 	def batch_id(self):
@@ -28,19 +38,38 @@ class DatasetFromTFRecord:
 	def targets(self):
 		return self._targets
 
-	def next_batch(self, batch_size):
+	def _init(self):
+		if self._features_op is None:
+			with tf.variable_scope(self._variable_scope):
+				dataset = tf.data.TFRecordDataset(filenames=self._dataset_files)
+				dataset = dataset.repeat(self._number_of_epochs)
+				dataset = dataset.map(
+					lambda tfrecord_element: self._parser(tfrecord_element),
+					num_parallel_calls=self._number_parallel_calls)
+				dataset = dataset.batch(self._batch_size)
+				iterator = dataset.make_one_shot_iterator()
+				self._features_op = iterator.get_next()
+
+	def _parser(self, tfrecord_element):
+		keys_to_features = {
+			'dataset_sample_input': tf.FixedLenFeature((self._feature_input_size,), tf.float32),
+			'dataset_sample_target': tf.FixedLenFeature((self._feature_target_size,), tf.float32)
+		}
+		parsed = tf.parse_single_example(tfrecord_element, keys_to_features)
+		return parsed["dataset_sample_input"], parsed["dataset_sample_target"]
+
+	def next_batch(self):
 		self._batch_id += 1
-		batch_size = min(batch_size, len(self._permutation))
-		batch_perm, self._permutation = self._permutation[:batch_size], self._permutation[batch_size:]
-		return self._features[batch_perm], self._targets[batch_perm] if self._targets is not None else None
+		return self._features_op
 
 	def epoch_finished(self):
-		if len(self._permutation) == 0:
-			self._batch_id = 0
-			self._permutation = np.random.permutation(len(self._features)) if self._shuffle_batches \
-				else range(len(self._features))
-			return True
-		return False
+		pass
+		# if len(self._permutation) == 0:
+		# 	self._batch_id = 0
+		# 	self._permutation = np.random.permutation(len(self._features)) if self._shuffle_batches \
+		# 		else range(len(self._features))
+		# 	return True
+		# return False
 
 
 if __name__ == "__main__":
@@ -53,15 +82,28 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--batch_size", default=3, type=int, help="Batch size.")
 	parser.add_argument("--epochs", default=2, type=int, help="Number of epochs.")
+	parser.add_argument("--feature_input_size", default=36, type=int, help="Size of the .")
+	parser.add_argument("--feature_target_size", default=36, type=int, help="Number of epochs.")
 	args = parser.parse_args()
 
-	train_file = "{}/80-10-10/IIGS3_1_3_false_true_lvl7_train.npz".format(script_directory)
-	train = DatasetFromTFRecord(train_file)
+	dataset_files = ["dataset_0.tfrecord"]
 
-	for epoch in range(args.epochs):
-		print("Epoch #{}:".format(epoch))
-		while not train.epoch_finished():
-			print("Batch #{}:".format(train.batch_id))
-			features, targets = train.next_batch(args.batch_size)
-			print("Features:\n{}".format(features))
-			print("Targets:\n{}".format(targets))
+	dataset = DatasetFromTFRecord(dataset_files=dataset_files, feature_input_size=36, feature_target_size=36)
+
+	# for epoch in range(args.epochs):
+	# 	print("Epoch #{}:".format(epoch))
+	# 	while not train.epoch_finished():
+	# 		print("Batch #{}:".format(train.batch_id))
+	# 		features, targets = train.next_batch(args.batch_size)
+	# 		print("Features:\n{}".format(features))
+	# 		print("Targets:\n{}".format(targets))
+	with tf.Session() as sess:
+		while True:
+			try:
+				feature_input, feature_target = sess.run(dataset.next_batch())
+				print(feature_input, feature_target)
+			except tf.errors.OutOfRangeError:
+				# Ends the cycle if we run out of data samples
+				break
+
+

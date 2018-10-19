8cf4cd8ede4c2c59dabaf1ba8fcf41b92a364034
Karel Ha <mathemage@gmail.com>
2018-10-19 19:19:46 +0200

    Move `DatasetFromTFRecord` to `src.nn.data`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/data/DatasetFromTFRecord.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+
+class DatasetFromTFRecord:
+	def __init__(self, batch_size=1, dataset_files=list(), feature_input_size=1, feature_target_size=1,
+	             number_of_epochs=1, number_parallel_calls=None, variable_scope_name='DatasetFromTFRecord',
+	             shuffle_batches=True, shuffle_batches_buffer_size=100000):
+		self.iterator = None
+
+		self.epoch_finished = None
+
+		self._batch_id = 0
+		self._batch_size = batch_size
+		self._dataset_files = dataset_files
+		self._feature_input_size = feature_input_size
+		self._feature_target_size = feature_target_size
+		self._features_op = None  # TensorFlow operation
+		self._number_of_epochs = number_of_epochs
+		self._number_parallel_calls = number_parallel_calls
+		self._variable_scope_name = variable_scope_name
+		self._shuffle_batches = shuffle_batches
+		self._shuffle_batches_buffer_size = shuffle_batches_buffer_size
+
+		self._init()
+
+	@property
+	def batch_id(self):
+		return self._batch_id
+
+	@property
+	def features(self):
+		return None
+
+	@property
+	def targets(self):
+		return None
+
+	def _init(self):
+		if self._features_op is None:
+			with tf.variable_scope(self._variable_scope_name):
+				dataset = tf.data.TFRecordDataset(filenames=self._dataset_files)
+				dataset = dataset.repeat(self._number_of_epochs)
+				dataset = dataset.map(
+					lambda tfrecord_element: self._parser(tfrecord_element),
+					num_parallel_calls=self._number_parallel_calls)
+				if self._shuffle_batches:
+					dataset = dataset.shuffle(buffer_size=self._shuffle_batches_buffer_size)
+				dataset = dataset.batch(self._batch_size)
+				self.iterator = dataset.make_initializable_iterator()
+				self._features_op = self.iterator.get_next()
+
+	def _parser(self, tfrecord_element):
+		keys_to_features = {
+			'dataset_sample_input' : tf.FixedLenFeature((self._feature_input_size,), tf.float32),
+			'dataset_sample_target': tf.FixedLenFeature((self._feature_target_size,), tf.float32)
+		}
+		parsed = tf.parse_single_example(tfrecord_element, keys_to_features)
+		return parsed["dataset_sample_input"], parsed["dataset_sample_target"]
+
+	def next_batch(self, session):
+		if self.epoch_finished is None or self.epoch_finished is True:
+			session.run(self.iterator.initializer)
+			self.epoch_finished = False
+			self._batch_id = 0
+
+		while True:
+			self._batch_id += 1
+			try:
+				feature_input, feature_target = session.run(self._features_op)
+				if feature_target.shape[0] != self._batch_size:
+					self.epoch_finished = True
+					break
+			except tf.errors.OutOfRangeError:
+				self.epoch_finished = True
+				break
+			yield (feature_input, feature_target)
+
+	def epoch_finished(self):
+		return self.epoch_finished

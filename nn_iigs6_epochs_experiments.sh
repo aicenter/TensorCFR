3189564e316e25235cb5b3409d319bd5d5489f76
Karel Ha <mathemage@gmail.com>
2018-10-19 17:31:22 +0200

    Update nn_iigs6_epochs_experiments.sh
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- nn_iigs6_epochs_experiments.sh
+++ nn_iigs6_epochs_experiments.sh
@@ -9,8 +9,24 @@
 #CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 5000
 #CUDA_VISIBLE_DEVICES=1 python3.6 -m src.nn.ConvNet_IIGS6Lvl10 --epochs 50000
 
-# baselines (on local)
-PYTHON=/usr/bin/python3
-${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 50
-${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 500
-${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 5000
+# @local: baselines
+#PYTHON=/usr/bin/python3
+#${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 50
+#${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 500
+#${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --epochs 5000
+
+# @aic-ml: baselines w/ complex arch
+#PYTHON=python3.6
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --extractor C-45,C-45,C-45 --regressor C-135,C-135,C-135 --epochs 5
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --extractor C-45,C-45,C-45 --regressor C-135,C-135,C-135 --epochs 50
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --extractor C-45,C-45,C-45 --regressor C-135,C-135,C-135 --epochs 500
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNetBaseline_IIGS6Lvl10 --extractor C-45,C-45,C-45 --regressor C-135,C-135,C-135 --epochs 5000
+
+# @aic-ml: 1-file dataset
+#PYTHON=python3.6
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNet_IIGS6Lvl10 --epochs 500
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNet_IIGS6Lvl10 --extractor C-45,C-45,C-45 --regressor C-135,C-135,C-135 --epochs 500
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNet_IIGS6Lvl10 --epochs 5000
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNet_IIGS6Lvl10 --extractor C-45,C-45,C-45 --regressor C-135,C-135,C-135 --epochs 5000
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNet_IIGS6Lvl10 --epochs 50000
+#CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.ConvNet_IIGS6Lvl10 --extractor C-45,C-45,C-45 --regressor C-135,C-135,C-135 --epochs 50000

a0f2a4a7125e83eba127a6bfe3e9d6cb3ae900e1
Karel Ha <mathemage@gmail.com>
2018-11-01 15:37:01 +0100

    Copy `tensorcfr_on_goofspiel6.py` to `tensorcfr_fixed_trunk_strategies`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/algorithms/tensorcfr_fixed_trunk_strategies/tensorcfr_on_goofspiel6.py
@@ -0,0 +1,25 @@
+from src import utils
+from src.algorithms.tensorcfr_flattened_domains.TensorCFRFlattenedDomains import TensorCFRFlattenedDomains, \
+	get_cfr_strategies
+from src.domains.available_domains import get_domain_by_name
+
+# TODO: Get rid of `ACTIVATE_FILE` hotfix
+ACTIVATE_FILE = False
+
+if __name__ == '__main__' and ACTIVATE_FILE:
+	domain = get_domain_by_name("IIGS6_gambit_flattened")
+	tensorcfr = TensorCFRFlattenedDomains(domain)
+	average_strategies = get_cfr_strategies(
+		total_steps=1000,
+		tensorcfr_instance=tensorcfr,
+		quiet=True,
+		# profiling=True,
+		delay=0,
+		register_strategies_on_step=[0, 250, 500, 750, 999]
+	)  # TODO verify the results (final average strategies) via `gtlibrary`
+
+	utils.gtlibrary.export_average_strategies_to_json(
+		domain,
+		average_strategies,
+		"GS6_average_strategies"
+	)

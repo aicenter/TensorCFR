be643d14346a608990bff6a9ef4910c6a4009f7b
Karel Ha <mathemage@gmail.com>
2018-11-01 15:45:02 +0100

    Adjust IIGS6 for `TensorCFRFixedTrunkStrategies`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_fixed_trunk_strategies/tensorcfr_on_goofspiel6.py
+++ src/algorithms/tensorcfr_fixed_trunk_strategies/tensorcfr_on_goofspiel6.py
@@ -1,6 +1,5 @@
 from src import utils
-from src.algorithms.tensorcfr_flattened_domains.TensorCFRFlattenedDomains import TensorCFRFlattenedDomains, \
-	get_cfr_strategies
+from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
 from src.domains.available_domains import get_domain_by_name
 
 # TODO: Get rid of `ACTIVATE_FILE` hotfix
@@ -8,12 +7,12 @@ ACTIVATE_FILE = False
 
 if __name__ == '__main__' and ACTIVATE_FILE:
 	domain = get_domain_by_name("IIGS6_gambit_flattened")
-	tensorcfr = TensorCFRFlattenedDomains(domain)
-	average_strategies = get_cfr_strategies(
+	tensorcfr = TensorCFRFixedTrunkStrategies(
+		domain,
+		trunk_depth=0
+	)
+	average_strategies = tensorcfr.cfr_strategies_after_fixed_trunk(
 		total_steps=1000,
-		tensorcfr_instance=tensorcfr,
-		quiet=True,
-		# profiling=True,
 		delay=0,
 		register_strategies_on_step=[0, 250, 500, 750, 999]
 	)  # TODO verify the results (final average strategies) via `gtlibrary`

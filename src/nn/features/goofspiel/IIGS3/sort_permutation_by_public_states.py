37d29055f0a7ec1122bc8c282a74160fc7a4dc32
Karel Ha <mathemage@gmail.com>
2018-11-02 18:28:19 +0100

    Reduce only to `get_permutation_by_public_states()`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/features/goofspiel/IIGS3/sort_permutation_by_public_states.py
+++ src/nn/features/goofspiel/IIGS3/sort_permutation_by_public_states.py
@@ -1,44 +1,26 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pandas as pd
 
-from src.nn.features.goofspiel.IIGS3.game_constants import FEATURE_COLUMNS, TARGET_COLUMNS
-
-ACTIVATE_FILE = True
-
-
-def get_reaches_to_values_dataframe(filename):
-	reaches_to_values_dataframe = pd.read_csv(
-		filename,
-		names=["nodal_index", "node_to_infoset", "nodal_reach", "nodal_expected_value"],
-		delimiter=",",
-		skiprows=1,
-		index_col=0
-	)
-	return reaches_to_values_dataframe
-
-
-def get_concatenated_dataframe(features_dataframe, reaches_to_values_dataframe):
-	concatenated_dataframe = pd.concat(
-		[features_dataframe, reaches_to_values_dataframe],
-		axis=1,
-	)[FEATURE_COLUMNS + TARGET_COLUMNS]
-	return concatenated_dataframe
-
-
-def get_sorted_dataframes(concatenated_dataframe):  # sort by lexicographically by public states and infosets
-	sorted_dataframe = concatenated_dataframe.sort_values(
-		FEATURE_COLUMNS,
-		kind='mergesort'
-	)
-	print("sorted: ")
-	print(sorted_dataframe.head())
-	return sorted_dataframe
+from src.nn.features.goofspiel.IIGS3.game_constants import FEATURE_COLUMNS, FEATURES_BASENAME, \
+	NAMES_OF_FEATURE_CSV
+from src.utils.other_utils import get_features_dataframe
 
 
 def get_permutation_by_public_states():
-	raise NotImplementedError
+	"""
+	:return: The permutation that sorts game nodes of IIGS3 at level 7 by their public states and infosets.
+	"""
+	pd.set_option('display.max_columns', 500)
+	pd.set_option('display.width', 1000)
+	np.set_printoptions(edgeitems=20, suppress=True, linewidth=200)
+
+	features_filename = "{}.csv".format(FEATURES_BASENAME)
+	features = get_features_dataframe(features_filename, NAMES_OF_FEATURE_CSV, quiet=True)
+	features = features[FEATURE_COLUMNS[:-1]]
+	print(features)
 
 
-if __name__ == '__main__' and ACTIVATE_FILE:
+if __name__ == '__main__':
 	get_permutation_by_public_states()

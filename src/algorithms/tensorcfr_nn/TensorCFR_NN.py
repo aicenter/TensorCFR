6995b36e2d959a50704de719f6af7eecb22298d7
Karel Ha <mathemage@gmail.com>
2018-11-02 14:05:26 +0100

    Make `get_sorted_permutation()` an external function
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_nn/TensorCFR_NN.py
+++ src/algorithms/tensorcfr_nn/TensorCFR_NN.py
@@ -5,19 +5,17 @@ from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategi
 from src.domains.available_domains import get_domain_by_name
 
 
+def get_sorted_permutation():
+	return [2, 1, 0]
+
+
 class NeuralNetMockUp:
 	def predict(self, input_tensor):
 		return [5, 5, 6]
 
 
 class TensorCFR_NN(TensorCFRFixedTrunkStrategies):
-	def get_sorted_permutation(self):
-		return [2, 1, 0]
-
-	def predict_equilibrium_values(self, input_tensor, neural_net, permutation_tensor=None):
-		if permutation_tensor is None:
-			permutation_tensor = self.get_sorted_permutation()
-
+	def predict_equilibrium_values(self, input_tensor, neural_net, permutation_tensor):
 		permutation = tf.contrib.distributions.bijectors.Permute(permutation=permutation_tensor)
 
 		# permute input reach probabilities
@@ -41,4 +39,4 @@ if __name__ == '__main__':
 
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		print(sess.run(tensorcfr.predict_equilibrium_values([-1., 0., 1.], nn)))
+		print(sess.run(tensorcfr.predict_equilibrium_values([-1., 0., 1.], nn, get_sorted_permutation())))

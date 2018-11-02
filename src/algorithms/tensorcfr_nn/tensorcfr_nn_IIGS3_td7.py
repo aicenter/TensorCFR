184c29f17ec71a9cb15789ca0d44ac8970be089a
Karel Ha <mathemage@gmail.com>
2018-11-02 18:56:04 +0100

    Print via `print_tensors`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_nn/tensorcfr_nn_IIGS3_td7.py
+++ src/algorithms/tensorcfr_nn/tensorcfr_nn_IIGS3_td7.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from src.algorithms.tensorcfr_nn.TensorCFR_NN import TensorCFR_NN
 from src.domains.available_domains import get_domain_by_name
 from src.nn.features.goofspiel.IIGS3.sorting_permutation_by_public_states import get_permutation_by_public_states
+from src.utils.tf_utils import print_tensors
 
 if __name__ == '__main__':
 	domain_ = get_domain_by_name("II-GS3_gambit_flattened")
@@ -17,4 +18,4 @@ if __name__ == '__main__':
 	equilibrium_values = tensorcfr.predict_equilibrial_values(input_reaches)
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		print(sess.run(equilibrium_values))
+		print_tensors(sess, [input_reaches, equilibrium_values])

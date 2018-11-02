ebe879df88ae25e9b2c7b674188f9c4c76006cb7
Karel Ha <mathemage@gmail.com>
2018-11-02 18:46:27 +0100

    Extract `input_reaches`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/algorithms/tensorcfr_nn/tensorcfr_nn_IIGS3_td7.py
+++ src/algorithms/tensorcfr_nn/tensorcfr_nn_IIGS3_td7.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import tensorflow as tf
 
-from src.algorithms.tensorcfr_nn.TensorCFR_NN import TensorCFR_NN, get_sorted_permutation
+from src.algorithms.tensorcfr_nn.TensorCFR_NN import TensorCFR_NN
 from src.domains.available_domains import get_domain_by_name
+from src.nn.features.goofspiel.IIGS3.sorting_permutation_by_public_states import get_permutation_by_public_states
 
 if __name__ == '__main__':
 	domain_ = get_domain_by_name("II-GS3_gambit_flattened")
@@ -11,8 +12,9 @@ if __name__ == '__main__':
 		trunk_depth=7
 	)
 
-	permutation = get_sorted_permutation()
-	equilibrium_values = tensorcfr.predict_equilibrial_values([-1., 0., 2.])
+	permutation = get_permutation_by_public_states()
+	input_reaches = tf.constant([-1., 0., 2.])
+	equilibrium_values = tensorcfr.predict_equilibrial_values(input_reaches)
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		print(sess.run(equilibrium_values))

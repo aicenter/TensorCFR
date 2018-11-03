567891d4e091696a5c532d9b799c78898c1132bf
Karel Ha <mathemage@gmail.com>
2018-11-03 12:44:26 +0100

    Introduce parameter `pass_input_using` to `network.predict()`
    - `AbstractNN`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/AbstractNN.py
+++ src/nn/AbstractNN.py
@@ -3,5 +3,5 @@ from abc import abstractmethod
 
 class AbstractNN:
 	@abstractmethod
-	def predict(self, input_features):
+	def predict(self, input_features, pass_input_using=None):
 		raise NotImplementedError

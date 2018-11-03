4f20b9d60829117499632aeccf22c4d524bf2c93
Karel Ha <mathemage@gmail.com>
2018-11-03 12:42:41 +0100

    Introduce parameter `pass_input_using` to `network.predict()`
    - IIGS3, CNN
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/ConvNet_IIGS3Lvl7.py
+++ src/nn/ConvNet_IIGS3Lvl7.py
@@ -299,8 +299,15 @@ class ConvNet_IIGS3Lvl7(AbstractNN):
 		)
 		return mean_squared_error, l_infinity_error
 
-	def predict(self, features):
-		return self.session.run(self.predictions, {self.input_reaches: features})
+	def predict(self, features, pass_input_using="numpy"):
+		if pass_input_using == "numpy":
+			return self.session.run(self.predictions, {self.input_reaches: features})
+		elif pass_input_using == "tensorflow":
+			handle = tf.get_session_handle(features)
+			handle_of_features = self.session.run(handle)
+			return self.session.run(self.predictions, {self.input_reaches: handle_of_features})
+		else:
+			raise ValueError("Invalid value '{}' of 'pass_input_using'.".format(pass_input_using))
 
 	def print_operations_count(self):
 		print("--> Total size of computation graph: {} operations".format(count_graph_operations(self.graph)))
@@ -369,5 +376,11 @@ if __name__ == '__main__' and ACTIVATE_FILE:
 	print("L-infinity error on testset: {}".format(testset_error_infinity))
 
 	print()
-	print("Predictions of initial 2 training examples:")
-	print(network.predict(trainset.features[:2]))
+	print("Predictions of initial 2 training examples (passing input using numpy):")
+	print(network.predict(trainset.features[:2], pass_input_using="numpy"))
+
+	print()
+	print("Predictions of initial 2 training examples (passing input using tensorflow):")
+	with network.session.graph.as_default():
+		input_to_prediction = tf.constant(trainset.features[:2], name="input_to_prediction")
+		print(network.predict(input_to_prediction, pass_input_using="tensorflow"))

0440190f6b4df6db4a536ffdddef356c2afe9cc2
Dominik Andreas Seitz <seitzdom@fel.cvut.cz>
2018-11-06 11:45:58 +0100

    Added load/save, load/checkpoints methods
    Signed-off-by: Dominik Andreas Seitz <seitzdom@fel.cvut.cz>

--- src/nn/AbstractNN.py
+++ src/nn/AbstractNN.py
@@ -1,7 +1,36 @@
 from abc import abstractmethod
+import tensorflow as tf
 
 
 class AbstractNN:
 	@abstractmethod
 	def predict(self, input_features, pass_input_using=None):
 		raise NotImplementedError
+
+	#def __init__(self,threads):
+		#self.graph = tf.Graph()
+		#self.session = tf.Session(graph=self.graph,config=tf.ConfigProto(inter_op_parallelism_threads=threads,
+		                                                                 #intra_op_parallelism_threads=threads))
+
+	def construct_saver(self):
+		self.saver = tf.train.Saver()
+
+	def construct_checkpoint(self,steps):
+
+		if self.saver in globals():
+
+			try:
+
+				self.checkpoint = tf.train.CheckpointSaverHook(saver=self.saver,save_steps=steps,checkpoint_basename="model.ckpt")
+
+			except NameError:
+				print("saver not defined yet")
+
+	def call_saver(self,path):
+		self.saver.save(self.session,path)
+
+	def load_checkpoint(self,path):
+		raise NotImplementedError
+
+
+

40008d5f6f33f675bf13f296bac96792ca7a94bb
Dominik Andreas Seitz <seitzdom@fel.cvut.cz>
2018-11-06 14:52:35 +0100

    Updated abstract methods to call eachother
    Signed-off-by: Dominik Andreas Seitz <seitzdom@fel.cvut.cz>

--- src/nn/AbstractNN.py
+++ src/nn/AbstractNN.py
@@ -11,10 +11,11 @@ class AbstractNN:
 		#self.graph = tf.Graph()
 		#self.session = tf.Session(graph=self.graph,config=tf.ConfigProto(inter_op_parallelism_threads=threads,
 		                                                                 #intra_op_parallelism_threads=threads))
-
+	@abstractmethod
 	def construct_saver(self):
 		self.saver = tf.train.Saver()
 
+	@abstractmethod
 	def construct_checkpoint(self,steps):
 
 		if self.saver in globals():
@@ -26,9 +27,16 @@ class AbstractNN:
 			except NameError:
 				print("saver not defined yet")
 
+	@abstractmethod
 	def call_saver(self,path):
+
+		if self.saver not in tf.global_variables():
+
+			AbstractNN.construct_saver(self)
+
 		self.saver.save(self.session,path)
 
+	@abstractmethod
 	def load_checkpoint(self,path):
 		raise NotImplementedError
 

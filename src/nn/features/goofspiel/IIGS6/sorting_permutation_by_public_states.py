a44b974b8d7c13ad8a08ae1f82ce91f83e728dd2
Karel Ha <mathemage@gmail.com>
2018-11-07 21:14:23 +0100

    Implement `get_permutation_by_public_states()` for IIGS6 at level 10
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/features/goofspiel/IIGS6/sorting_permutation_by_public_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+
+import numpy as np
+import pandas as pd
+
+from src.nn.features.goofspiel.IIGS6.game_constants import FEATURE_COLUMNS, FEATURES_BASENAME, \
+	NAMES_OF_FEATURE_CSV
+from src.utils.other_utils import get_features_dataframe
+
+
+def get_permutation_by_public_states(verbose=False):
+	"""
+	:return: The permutation that sorts game nodes of IIGS6 at level 10 by their public states and infosets.
+	"""
+	pd.set_option('display.max_columns', 500)
+	pd.set_option('display.width', 1000)
+	np.set_printoptions(edgeitems=20, suppress=True, linewidth=200)
+
+	script_directory = os.path.dirname(os.path.abspath(__file__))
+	features_filename = "{}/{}.csv".format(script_directory, FEATURES_BASENAME)
+	features = get_features_dataframe(features_filename, NAMES_OF_FEATURE_CSV, quiet=True)
+	columns_without_reach = FEATURE_COLUMNS[:-1]
+	features = features[columns_without_reach]
+	features = features.sort_values(
+		columns_without_reach,
+		kind='mergesort'
+	)
+	if verbose:
+		print(features)
+	permutation = list(features.index)
+	return permutation
+
+
+if __name__ == '__main__':
+	print("The permutation that sorts game nodes of IIGS6 at level 10 by their public states and infosets:")
+	print(get_permutation_by_public_states(verbose=True))

26a49c86829edc4c88f7ee9f3227e02cc3f9764e
Karel Ha <mathemage@gmail.com>
2018-11-07 23:53:17 +0100

    Add `dataset_dir`
    - to tensorcfr_cnn_iigs6_td10_online_training@aic-ml.sh
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- experiments/tensorcfr_nn/IIGS6Lvl10/tensorcfr_cnn_iigs6_td10_online_training@aic-ml.sh
+++ experiments/tensorcfr_nn/IIGS6Lvl10/tensorcfr_cnn_iigs6_td10_online_training@aic-ml.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 PYTHON=python3.6
-CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.algorithms.tensorcfr_nn.tensorcfr_CNN_IIGS6_td10_online_training
+DATADIR="data/IIGS6Lvl10/IIGS6_1_6_false_true_lvl10_npz_900_seeds"  # Warning: copy the dataset into this location !!
+COMMON_ARGS="--dataset_directory ${DATADIR} --epochs 100"
+
+CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.algorithms.tensorcfr_nn.tensorcfr_CNN_IIGS6_td10_online_training ${COMMON_ARGS}

a9747cbd279c1bda3fe2ac649a8f064d6c2cda7d
Karel Ha <mathemage@gmail.com>
2018-11-08 00:48:59 +0100

    Add experiments for {1,2,3,5,7} layers
    - in tensorcfr_cnn_iigs6_td10_online_training@aic-ml.sh
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- experiments/tensorcfr_nn/IIGS6Lvl10/tensorcfr_cnn_iigs6_td10_online_training@aic-ml.sh
+++ experiments/tensorcfr_nn/IIGS6Lvl10/tensorcfr_cnn_iigs6_td10_online_training@aic-ml.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
 PYTHON=python3.6
+SCRIPT=src.algorithms.tensorcfr_nn.tensorcfr_CNN_IIGS6_td10_online_training
 DATADIR="data/IIGS6Lvl10/IIGS6_1_6_false_true_lvl10_npz_900_seeds"  # Warning: copy the dataset into this location !!
-COMMON_ARGS="--dataset_directory ${DATADIR} --epochs 100"
+COMMON_ARGS="--dataset_directory ${DATADIR} --epochs 1000"
 
-CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.algorithms.tensorcfr_nn.tensorcfr_CNN_IIGS6_td10_online_training ${COMMON_ARGS}
+# CUDA_VISIBLE_DEVICES=1 nohup python3.6 -m src.algorithms.tensorcfr_nn.tensorcfr_CNN_IIGS6_td10_online_training --dataset_directory data/IIGS6Lvl10/IIGS6_1_6_false_true_lvl10_npz_900_seeds --epochs 10 --extractor C-46 --regressor C-138 &>tensorcfr_CNN_IIGS6_td10_online_training.out.1_layer &
+
+# 1 layers
+ARCH="--extractor C-46 --regressor C-138"
+CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m ${SCRIPT} ${COMMON_ARGS} ${ARCH} &>tensorcfr_CNN_IIGS6_td10_online_training.out.1_layers
+
+# 2 layers
+ARCH="--extractor C-46,C-46 --regressor C-138,C-138"
+CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m ${SCRIPT} ${COMMON_ARGS} ${ARCH} &>tensorcfr_CNN_IIGS6_td10_online_training.out.2_layers
+
+# 3 layers
+ARCH="--extractor C-46,C-46,C-46 --regressor C-138,C-138,C-138"
+CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m ${SCRIPT} ${COMMON_ARGS} ${ARCH} &>tensorcfr_CNN_IIGS6_td10_online_training.out.3_layers
+
+# 5 layers
+ARCH="--extractor C-46,C-46,C-46,C-46,C-46 --regressor C-138,C-138,C-138,C-138,C-138"
+CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m ${SCRIPT} ${COMMON_ARGS} ${ARCH} &>tensorcfr_CNN_IIGS6_td10_online_training.out.5_layers
+
+# 7-layers
+ARCH="--extractor C-46,C-46,C-46,C-46,C-46,C-46,C-46 --regressor C-138,C-138,C-138,C-138,C-138,C-138,C-138"
+CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m ${SCRIPT} ${COMMON_ARGS} ${ARCH} &>tensorcfr_CNN_IIGS6_td10_online_training.out.7_layers

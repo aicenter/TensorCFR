f21de1eb88bc96e46e7b82f2cfa8c75db715d08e
Karel Ha <mathemage@gmail.com>
2018-11-10 13:32:21 +0100

    Remove `@abstractmethod` from `save_to_ckpt` and `restore_from_ckpt`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/AbstractNN.py
+++ src/nn/AbstractNN.py
@@ -6,7 +6,6 @@ class AbstractNN:
 	def predict(self, input_features, pass_input_using=None):
 		raise NotImplementedError
 
-	@abstractmethod
 	def save_to_ckpt(self, path):
 		if str(path).endswith(".ckpt"):
 			self.saver.save(self.session, path)
@@ -14,10 +13,9 @@ class AbstractNN:
 			self.saver.save(self.session, path + ".ckpt")
 		print("Saving to " + path + " successful")
 
-	@abstractmethod
 	def restore_from_ckpt(self, path):
 		if str(path).endswith(".ckpt"):
 			self.saver.restore(self.session, path)
 		else:
 			self.saver.restore(self.session, path + ".ckpt")
-		print("Restoring from "+path +" successful")
+		print("Restoring from " + path + " successful")

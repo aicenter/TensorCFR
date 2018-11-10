3fce5c6916020a40df717245cf4207d3f217aeb6
Dominik Andreas Seitz <seitzdom@fel.cvut.cz>
2018-11-10 11:52:57 +0100

    Abstract NN class cleaned up
    Signed-off-by: Dominik Andreas Seitz <seitzdom@fel.cvut.cz>

--- src/nn/AbstractNN.py
+++ src/nn/AbstractNN.py
@@ -8,26 +8,6 @@ class AbstractNN:
 	def predict(self, input_features, pass_input_using=None):
 		raise NotImplementedError
 
-	#def __init__(self,threads):
-		#self.graph = tf.Graph()
-		#self.session = tf.Session(graph=self.graph,config=tf.ConfigProto(inter_op_parallelism_threads=threads,
-		                                                                 #intra_op_parallelism_threads=threads))
-	@abstractmethod
-	def construct_saver(self):
-		self.saver = tf.train.Saver()
-
-	@abstractmethod
-	def construct_checkpoint(self,steps):
-
-		if self.saver in globals():
-
-			try:
-
-				self.checkpoint = tf.train.CheckpointSaverHook(saver=self.saver,save_steps=steps,checkpoint_basename="model.ckpt")
-
-			except NameError:
-				print("saver not defined yet")
-
 	@abstractmethod
 	def call_saver(self,path):
 
@@ -38,9 +18,6 @@ class AbstractNN:
 		self.saver.save(self.session,path)
 
 	@abstractmethod
-	def load_checkpoint(self,path):
-		raise NotImplementedError
-
 	def save_to_ckpt(self, path):
 		if str(path).endswith(".ckpt"):
 			self.saver.save(self.session, path)
@@ -48,6 +25,7 @@ class AbstractNN:
 			self.saver.save(self.session, path + ".ckpt")
 		print("Saving to " + path + " successful")
 
+	@abstractmethod
 	def restore_from_ckpt(self, path):
 		if str(path).endswith(".ckpt"):
 			self.saver.restore(self.session, path)

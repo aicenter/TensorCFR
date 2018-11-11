3193f4d2045fd2fd4fd7d108df159b2b3ba774f3
Karel Ha <mathemage@gmail.com>
2018-11-11 01:28:09 +0100

    Loop over array of ARCHS in `submit_all.sh`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- experiments/1,2,3,5,7layers,train-dev-test_14000-1750-1750_tfrecords/submit_all.sh
+++ experiments/1,2,3,5,7layers,train-dev-test_14000-1750-1750_tfrecords/submit_all.sh
@@ -8,3 +8,18 @@
 #CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.Runner_CNN_IIGS6Lvl10_TFRecords --dataset_directory ${DATADIR} --epochs 512 --extractor C-46,C-46,C-46 --regressor C-138,C-138,C-138
 #CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.Runner_CNN_IIGS6Lvl10_TFRecords --dataset_directory ${DATADIR} --epochs 512 --extractor C-46,C-46,C-46,C-46,C-46 --regressor C-138,C-138,C-138,C-138,C-138
 #CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m src.nn.Runner_CNN_IIGS6Lvl10_TFRecords --dataset_directory ${DATADIR} --epochs 512 --extractor C-46,C-46,C-46,C-46,C-46,C-46,C-46 --regressor C-138,C-138,C-138,C-138,C-138,C-138,C-138
+
+ARCHS=(
+"--extractor C-46 --regressor C-138"
+"--extractor C-46,C-46 --regressor C-138,C-138"
+"--extractor C-46,C-46,C-46 --regressor C-138,C-138,C-138"
+"--extractor C-46,C-46,C-46,C-46,C-46 --regressor C-138,C-138,C-138,C-138,C-138"
+"--extractor C-46,C-46,C-46,C-46,C-46,C-46,C-46 --regressor C-138,C-138,C-138,C-138,C-138,C-138,C-138"
+)
+for ARCH in "${ARCHS[@]}"
+do
+    NAME="CNN_${ARCH//[ -]/}_IIGS6Lvl10_17500seeds_tfrecords"
+	CMD="qsub -N \"${NAME}\" -v \"ARCH=${ARCH}\" x_layers.sh "
+	echo ${CMD}
+	${CMD}
+done

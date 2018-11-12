48a2ff016bb370b98d88e5a97abb466935d9ba06
Karel Ha <mathemage@gmail.com>
2018-11-12 19:24:14 +0100

    Include other `summarize.sh`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ experiments/tensorcfr_nn/IIGS6Lvl10/tensorcfr_cnn_online_training/04/summarize.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+LOGS=*.out.*_layers
+OUTFILE=summary.out
+>${OUTFILE}
+
+cat ${LOGS} | grep "args" >>${OUTFILE}
+echo >>${OUTFILE}
+
+cat ${LOGS} | grep "testset" >>${OUTFILE}
+echo >>${OUTFILE}
+
+cat ${LOGS} | grep "exploitability of" >>${OUTFILE}

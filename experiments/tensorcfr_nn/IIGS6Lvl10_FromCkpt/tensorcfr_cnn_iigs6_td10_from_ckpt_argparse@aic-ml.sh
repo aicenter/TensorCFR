66b26d5231cc3341e6600fa9409d4b1a31a559f8
Karel Ha <mathemage@gmail.com>
2018-11-13 23:18:44 +0100

    Run 10 times the 1-layer TensorCFR_NN
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- experiments/tensorcfr_nn/IIGS6Lvl10_FromCkpt/tensorcfr_cnn_iigs6_td10_from_ckpt_argparse@aic-ml.sh
+++ experiments/tensorcfr_nn/IIGS6Lvl10_FromCkpt/tensorcfr_cnn_iigs6_td10_from_ckpt_argparse@aic-ml.sh
@@ -13,7 +13,10 @@ CKPT_DIR="Runner_CNN_IIGS6Lvl10_TFRecords-2018-11-11_021836-bs=32,ce=64.0,dr=0.1
 CKPT_BASENAME="final_2018-11-11_07:46:15.ckpt"
 CKPTS="--ckpt_dir ${CKPT_COMMON_DIR} --ckpt_basename ${CKPT_BASENAME}"
 ARCH="--extractor C-46 --regressor C-138"
-CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m ${SCRIPT} ${COMMON_ARGS} ${ARCH} ${CKPTS} |& tee ${OUTFILE}.1_layers
+RUNS=10
+for i in `seq 10` ; do
+    echo CUDA_VISIBLE_DEVICES=1 ${PYTHON} -m ${SCRIPT} ${COMMON_ARGS} ${ARCH} ${CKPTS} |& tee ${OUTFILE}.1_layers.run${i}
+done
 
 ## 5 layers
 #CKPT_DIR="Runner_CNN_IIGS6Lvl10_TFRecords-2018-11-11_030427-bs=32,ce=64.0,dr=0.1,e=512,e=C-46,C-46,C-46,C-46,C-46,r=C-138,C-138,C-138,C-138,C-138,t=1,tr=0.8"

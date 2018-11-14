56e48e66e4eafa5e7b37991aab1e63c97f947ddd
Karel Ha <mathemage@gmail.com>
2018-11-14 19:09:53 +0100

    Copy & inherit `FullyConvNet_IIGS6Lvl10_no_context_pooling`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ src/nn/FullyConvNet_IIGS6Lvl10_no_context_pooling.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# taken from https://github.com/ufal/npfl114/blob/3b35b431be3c84c2f2d51a4e2353d65cd30ee8fe/labs/04/mnist_competition.py
+
+from src.nn import ConvNet_IIGS6Lvl10
+
+
+class FullyConvNet_IIGS6Lvl10_no_context_pooling(ConvNet_IIGS6Lvl10):
+	pass

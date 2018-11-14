caf87e8ab13fff54c7c4f096e2bcfaedc7197097
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-11-14 15:55:55 +0100

    Plot multiple graphs into a one figure.
    Signed-off-by: janrudolf <jan.rudolf.steeldog@gmail.com>

--- src/utils/reading_tf_log.py
+++ src/utils/reading_tf_log.py
@@ -1,62 +1,94 @@
 import os
+import matplotlib
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 from src.commons.constants import PROJECT_ROOT
 
 if __name__ == '__main__':
-	# log_file_path = os.path.join(
+	# log_file_path1 = os.path.join(
 	# 	PROJECT_ROOT,
 	# 	'logs',
 	# 	'Runner_CNN_IIGS6Lvl10_NPZ-2018-11-12_125341-bs=2,e=10,e=C-46,r=C-46,t=1',
 	# 	'events.out.tfevents.1542023648.doom14.metacentrum.cz.v2')
-	log_file_path = os.path.join(
+	log_file_path1 = os.path.join(
+		PROJECT_ROOT,
+		'logs_KAREL',
+		'Runner_CNN_IIGS6Lvl10_TFRecords-2018-11-11_021744-bs=32,ce=64.0,dr=0.1,e=512,e=C-46,C-46,C-46,r=C-138,C-138,C-138,t=1,tr=0.8',
+		'events.out.tfevents.1541899082.glados10.cerit-sc.cz.v2')
+	log_file_path2 = os.path.join(
+		PROJECT_ROOT,
+		'logs_KAREL',
+		'Runner_CNN_IIGS6Lvl10_TFRecords-2018-11-11_021752-bs=32,ce=64.0,dr=0.1,e=512,e=C-46,C-46,r=C-138,C-138,t=1,tr=0.8',
+		'events.out.tfevents.1541899086.white1.cerit-sc.cz.v2')
+	log_file_path3 = os.path.join(
+		PROJECT_ROOT,
+		'logs_KAREL',
+		'Runner_CNN_IIGS6Lvl10_TFRecords-2018-11-11_021836-bs=32,ce=64.0,dr=0.1,e=512,e=C-46,r=C-138,t=1,tr=0.8',
+		'events.out.tfevents.1541899127.white1.cerit-sc.cz.v2')
+	log_file_path4 = os.path.join(
+		PROJECT_ROOT,
+		'logs_KAREL',
+		'Runner_CNN_IIGS6Lvl10_TFRecords-2018-11-11_030427-bs=32,ce=64.0,dr=0.1,e=512,e=C-46,C-46,C-46,C-46,C-46,r=C-138,C-138,C-138,C-138,C-138,t=1,tr=0.8',
+		'events.out.tfevents.1541901887.glados16.cerit-sc.cz.v2')
+	log_file_path5 = os.path.join(
 		PROJECT_ROOT,
 		'logs_KAREL',
 		'Runner_CNN_IIGS6Lvl10_TFRecords-2018-11-11_133801-bs=32,ce=64.0,dr=0.1,e=512,e=C-46,C-46,C-46,C-46,C-46,C-46,C-46,r=C-138,C-138,C-138,C-138,C-138,C-138,C-138,t=1,tr=0.8',
 		'events.out.tfevents.1541939902.konos1.fav.zcu.cz.v2')
+	log_files_paths = [
+		log_file_path1,
+		log_file_path2,
+		log_file_path3,
+		log_file_path4,
+		log_file_path5
+	]
 
-	logs_values = dict()
-	logs_steps = list()
+	plotting_values = 'train/loss'
 
-	for e in tf.train.summary_iterator(log_file_path):
+	plot_names = ['1', '2', '3', '4', '5']
 
-		# print(e.step)
+	graph_logs_values = list()
 
-		# logs_steps_len = len(logs_steps)
-		#
-		# if logs_steps_len == 0:
-		# 	logs_steps.append(e.step)
-		#
-		# if logs_steps_len > 0 and e.step != logs_steps[logs_steps_len - 1]:
-		# 	logs_steps.append(e.step)
+	for log_file_path in log_files_paths:
+		logs_values = dict()
 
-		for v in e.summary.value:
-			# print(str(v.tag) + " -> " + str(v.simple_value))
-			if v.tag not in logs_values.keys():
-				logs_values[v.tag] = {
-					'steps': [e.step],
-					'values': [v.simple_value]
-				}
-			else:
-				logs_values[v.tag]['steps'].append(e.step)
-				logs_values[v.tag]['values'].append(v.simple_value)
-		# print("----")
+		for e in tf.train.summary_iterator(log_file_path):
+			for v in e.summary.value:
+				if v.tag not in logs_values.keys():
+					logs_values[v.tag] = {
+						'steps': [e.step],
+						'values': [v.simple_value]
+					}
+				else:
+					logs_values[v.tag]['steps'].append(e.step)
+					logs_values[v.tag]['values'].append(v.simple_value)
+		graph_logs_values.append(logs_values)
 
-	# print values
-	for k in logs_values.keys():
-		print(k)
-		print('\t')
-		print(logs_values[k]['steps'])
-		print(logs_values[k]['values'])
-		print("---")
+	for idx, graph_log_values in enumerate(graph_logs_values):
+		plt.plot(graph_log_values[plotting_values]['steps'], graph_log_values[plotting_values]['values'], linewidth=1)
 
-	# plotting
-	import matplotlib.pyplot as plt
-	plotting_graph = 'train/loss'
-	plt.plot(logs_values[plotting_graph]['steps'], logs_values[plotting_graph]['values'])
+	# # print values
+	# for k in logs_values.keys():
+	# 	print(k)
+	# 	print('\t')
+	# 	print(logs_values[k]['steps'])
+	# 	print(logs_values[k]['values'])
+	# 	print("---")
+	#
+	# # plotting
+	# import matplotlib.pyplot as plt
+	# plotting_graph = 'train/loss'
+	# plt.plot(logs_values[plotting_graph]['steps'], logs_values[plotting_graph]['values'])
+	#
+	# plotting_graph = 'train/l_infinity_error'
+	# plt.plot(logs_values[plotting_graph]['steps'], [0.150]*len(logs_values[plotting_graph]['steps']))
+	#
+	#
 	plt.xlabel('Steps')
-	plt.ylabel('L-infinity Error')
-	plt.ylim(0, 0.2) # set the y axis to be shown in the interval (0, 0.2)
-	plt.legend(['NN 1'])
-	plt.title('Hello world')
-	plt.show()
+	plt.ylabel('ylabel')
+	plt.ylim(0.025, 0.2)# 0.08, 0.11) # set the y axis to be shown in the fixed interval (0, 0.2)
+	plt.legend(plot_names)
+	plt.title('title')
+	plt.savefig("pic.png")
+	# plt.plot()

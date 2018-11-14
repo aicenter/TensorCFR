15fc8e356a23a8a38ba71cbfdd717e0406bc9dbe
Karel Ha <mathemage@gmail.com>
2018-11-14 19:13:23 +0100

    Disable ckpt functionality for Runner
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/Runner_CNN_IIGS6Lvl10_TFRecords_no_context_pooling.py
+++ src/nn/Runner_CNN_IIGS6Lvl10_TFRecords_no_context_pooling.py
@@ -15,11 +15,13 @@ if __name__ == '__main__' and ACTIVATE_FILE:
 	runner = Runner_CNN_IIGS6Lvl10_TFRecords_no_context_pooling()
 	runner.run_neural_net(ckpt_every=2)
 
-	runner_from_ckpt = Runner_CNN_IIGS6Lvl10_TFRecords_no_context_pooling()
+	# TODO modify
+	# runner_from_ckpt = Runner_CNN_IIGS6Lvl10_TFRecords_no_context_pooling()
 	# runner_from_ckpt.run_neural_net_from_ckpt(ckpt_dir=runner.args.logdir, ckpt_basename=runner.ckpt_basenames[-1])
 
+	# TODO modify
 	# Note: you can test this on:
 	# i.e.
 	# --ckpt_dir "logs/Runner_CNN_IIGS6Lvl10_TFRecords-2018-11-11_204734-bs=32,ce=2,dr=0.1,e=3,e=C-46,r=C-46,t=1,tr=0.8"
 	# --ckpt_basename "final_2018-11-11_20:47:52.ckpt successful"
-	runner_from_ckpt.run_neural_net_from_ckpt()
+	# runner_from_ckpt.run_neural_net_from_ckpt()

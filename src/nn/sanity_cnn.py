661cc4fbab4e604990cd6ba44107df636517cc4d
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-11-15 15:22:49 +0100

    Adds updated sanity cnn scripts

--- src/nn/sanity_cnn.py
+++ src/nn/sanity_cnn.py
@@ -8,12 +8,12 @@ from src.nn.ConvNet_IIGS6Lvl10 import ConvNet_IIGS6Lvl10
 
 class SanityCNN(ConvNet_IIGS6Lvl10):
 	NUM_NODES = 3 # 14400
-	NUM_ROUNDS = 3
+	NUM_ROUNDS = 1 # 3
 
 	PUBLIC_FEATURES_DIM = 3
-	INFOSET_FEATURES_DIM = 6
+	INFOSET_FEATURES_DIM = 3 # 6
 	NODAL_FEATURES_DIM = 6
-	FEATURES_DIM_PER_ROUND = PUBLIC_FEATURES_DIM + INFOSET_FEATURES_DIM + NODAL_FEATURES_DIM
+	FEATURES_DIM_PER_ROUND = 3 # PUBLIC_FEATURES_DIM + INFOSET_FEATURES_DIM + NODAL_FEATURES_DIM
 	REACH_PROB_DIM = 1
 
 	INPUT_FEATURES_DIM = NUM_ROUNDS * FEATURES_DIM_PER_ROUND + REACH_PROB_DIM
@@ -62,6 +62,53 @@ class SanityCNN(ConvNet_IIGS6Lvl10):
 		self.targets = tf.placeholder(FLOAT_DTYPE, [None, self.NUM_NODES], name="targets")
 		print("Targets constructed")
 		self.print_operations_count()
+
+	def construct_context_pooling(self):
+		"""
+		Add layers to build context: means and maxes per each public state.
+
+		:return:
+		"""
+		with tf.variable_scope("context_pooling"):
+			groups_by_public_states = tf.split(
+				value=self.latest_layer,
+				num_or_size_splits= 1, # self._sizes_of_public_states,
+				axis=-1,     # NCL
+				name="split_by_public_states"
+			)
+
+			# pooling operations
+			public_state_means = [None] * self.NUM_PUBLIC_STATES
+			public_state_maxes = [None] * self.NUM_PUBLIC_STATES
+			public_state_contexts = [None] * self.NUM_PUBLIC_STATES
+			tiled_contexts = [None] * self.NUM_PUBLIC_STATES
+			for i, group in enumerate(groups_by_public_states):
+				with tf.variable_scope("public_state{}".format(i)):
+					public_state_means[i] = tf.reduce_mean(group, axis=-1, keepdims=True, name="mean{}".format(i))
+					public_state_maxes[i] = tf.reduce_max(group, axis=-1, keepdims=True, name="max{}".format(i))
+					public_state_contexts[i] = tf.concat(
+						[public_state_means[i], public_state_maxes[i]],
+						axis=1,
+						name="context{}".format(i)
+					)
+					tiled_contexts[i] = tf.tile(
+						public_state_contexts[i],
+						multiples=[1, 1, self._sizes_of_public_states[i]],
+						name="tiled_context{}".format(i)
+					)
+
+		# concatenate with extractor's outputs to form regressor's input
+		with tf.variable_scope("concatenate"):
+			full_context = tf.concat(
+				tiled_contexts,
+				axis=-1,
+				name="full_context"
+			)
+			self.latest_layer = tf.concat(
+				[self.latest_layer, full_context],
+				axis=1,
+				name="features_with_contexts"
+			)
 
 if __name__ == '__main__':
 	pass
--- src/nn/sanity_cnn_runner.py
+++ src/nn/sanity_cnn_runner.py
@@ -1,5 +1,6 @@
 from src.nn.Runner_CNN_IIGS6Lvl10_NPZ import Runner_CNN_IIGS6Lvl10_NPZ
 from src.nn.sanity_cnn import SanityCNN
+from src.utils.other_utils import get_current_timestamp
 
 
 class SanityCNNRunner(Runner_CNN_IIGS6Lvl10_NPZ):
@@ -9,7 +10,31 @@ class SanityCNNRunner(Runner_CNN_IIGS6Lvl10_NPZ):
 		return self.network
 
 	def run_neural_net(self, ckpt_every=None, ckpt_dir=None):
-		pass
+		dataset_directory = self.args.dataset_directory
+		self.create_logdir()
+
+		devset, testset, trainset = self.init_datasets(dataset_directory)
+		self.network = self.construct_network()
+
+		if ckpt_dir is None:
+			ckpt_dir = self.args.logdir
+
+		for self.epoch in range(self.args.epochs):
+			self.train_one_epoch(trainset)
+			self.evaluate_devset(devset)
+
+			# checkpoint every `ckpt_every` epochs
+			if ckpt_every and ckpt_dir is not None and int(self.epoch) % int(ckpt_every) == 0:
+				ckpt_basename = "epoch_{}_{}".format(str(self.epoch), str(get_current_timestamp()))
+				self.ckpt_basenames.append(ckpt_basename)
+				self.network.save_to_ckpt(ckpt_dir, ckpt_basename)
+
+		ckpt_basename = "final_{}.ckpt".format(get_current_timestamp())
+		self.ckpt_basenames.append(ckpt_basename)
+		self.network.save_to_ckpt(ckpt_dir, ckpt_basename)  # final model
+
+		self.evaluate_testset(testset)
+		self.showcase_predictions(trainset)
 
 
 if __name__ == '__main__':

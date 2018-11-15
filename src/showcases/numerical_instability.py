2db6a8dae6b6cc8399f3f88856ec917023b20636
janrudolf <jan.rudolf.steeldog@gmail.com>
2018-11-15 18:13:52 +0100

    Updates operations on different graph

--- src/showcases/numerical_instability.py
+++ src/showcases/numerical_instability.py
@@ -3,21 +3,37 @@ import tensorflow as tf
 
 
 if __name__ == '__main__':
-	x1 = tf.placeholder(tf.float32)
-	c1 = tf.constant(2.0)
-	mul_op = tf.multiply(x1, c1)
+	# x1 = tf.placeholder(tf.float32)
+	# c1 = tf.placeholder(tf.float32)
+	# mul_op = tf.multiply(x1, c1)
+	#
+	# x2 = tf.placeholder(tf.float32)
+	# c2 = tf.placeholder(tf.float32)
+	# div_op = tf.divide(x2, c2)
 
-	x2 = tf.placeholder(tf.float32)
-	c2 = tf.constant(2.0)
-	div_op = tf.divide(x2, c2)
+	y = np.array([1.0, 1.0, 1.0])
 
-	y = np.array([1, 2, 3])
+	for prime in [2, 3, 5, 7, 9, 11, 12, 13]:
+		for _ in range(10000):
+			graph1 = tf.Graph()
+			with graph1.as_default():
+				x1 = tf.placeholder(tf.float32)
+				c1 = tf.placeholder(tf.float32)
+				mul_op = tf.multiply(x1, c1)
 
-	for _ in range(10000):
-		with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
-			y = session.run(mul_op, feed_dict={x1: y})
-			print("Mul: " + str(y))
+				with tf.Session() as session:
+					y = session.run(mul_op, feed_dict={x1: y, c1: prime})
+					print("Mul: " + str(y))
+			del graph1
 
-		with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
-			y = session.run(div_op, feed_dict={x2: y})
-			print("Div: " + str(y))
+			graph2 = tf.Graph()
+			with graph2.as_default():
+				x2 = tf.placeholder(tf.float32)
+				c2 = tf.placeholder(tf.float32)
+				div_op = tf.divide(x2, c2)
+				
+				with tf.Session() as session:
+					y = session.run(div_op, feed_dict={x2: y, c2: prime})
+					print("Div: " + str(y))
+			del graph2
+	print(y)

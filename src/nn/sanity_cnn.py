fe8dbaa5bd9eba251d80f6807e391b18e0d59911
Karel Ha <mathemage@gmail.com>
2018-12-16 18:47:47 +0100

    Remove main from `sanity_cnn`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- src/nn/sanity_cnn.py
+++ src/nn/sanity_cnn.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 
 from src.commons.constants import FLOAT_DTYPE
@@ -7,21 +6,21 @@ from src.nn.ConvNet_IIGS6Lvl10 import ConvNet_IIGS6Lvl10
 
 
 class SanityCNN(ConvNet_IIGS6Lvl10):
-	NUM_NODES = 3 # 14400
-	NUM_ROUNDS = 1 # 3
+	NUM_NODES = 3  # 14400
+	NUM_ROUNDS = 1  # 3
 
 	PUBLIC_FEATURES_DIM = 3
-	INFOSET_FEATURES_DIM = 3 # 6
+	INFOSET_FEATURES_DIM = 3  # 6
 	NODAL_FEATURES_DIM = 6
-	FEATURES_DIM_PER_ROUND = 3 # PUBLIC_FEATURES_DIM + INFOSET_FEATURES_DIM + NODAL_FEATURES_DIM
+	FEATURES_DIM_PER_ROUND = 3  # PUBLIC_FEATURES_DIM + INFOSET_FEATURES_DIM + NODAL_FEATURES_DIM
 	REACH_PROB_DIM = 1
 
 	INPUT_FEATURES_DIM = NUM_ROUNDS * FEATURES_DIM_PER_ROUND + REACH_PROB_DIM
 	TARGETS_DIM = 1
-	NUM_PUBLIC_STATES = 1 # 3 ** NUM_ROUNDS
+	NUM_PUBLIC_STATES = 1  # 3 ** NUM_ROUNDS
 
 	def construct_input(self):
-		self._one_hot_features_np = np.array([[0,1],[0,0],[1,0]]) # np.array([[0,0,1],[1,0,0]])
+		self._one_hot_features_np = np.array([[0, 1], [0, 0], [1, 0]])  # np.array([[0,0,1],[1,0,0]])
 
 		with tf.variable_scope("input"):
 			self.input_reaches = tf.placeholder(
@@ -74,8 +73,8 @@ class SanityCNN(ConvNet_IIGS6Lvl10):
 		with tf.variable_scope("context_pooling"):
 			groups_by_public_states = tf.split(
 				value=self.latest_layer,
-				num_or_size_splits= 1, # self._sizes_of_public_states,
-				axis=-1,     # NCL
+				num_or_size_splits=1,  # self._sizes_of_public_states,
+				axis=-1,  # NCL
 				name="split_by_public_states"
 			)
 
@@ -111,8 +110,3 @@ class SanityCNN(ConvNet_IIGS6Lvl10):
 				axis=1,
 				name="features_with_contexts"
 			)
-
-ACTIVATE_FILE = False
-
-if __name__ == '__main__'  and ACTIVATE_FILE:
-	pass

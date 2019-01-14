d99a19c9f78945cc2f13dadaecd66867733d87ef
Dominik Andreas Seitz <seitzdom@fel.cvut.cz>
2019-01-14 10:32:59 +0100

    Generate ranges for for player 1 after 3rd public round  IIGS6
    Signed-off-by: Dominik Andreas Seitz <seitzdom@fel.cvut.cz>

---
+++ src/nn/data/generate_ranges_of_lvl10_IIGS6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+
+from src.nn.data.generate_data import generate_data
+from src.utils.other_utils import activate_script
+
+
+if __name__ == '__main__' and activate_script():
+	generate_data(
+		"IIGS6_gambit_flattened",
+		script_directory=os.path.dirname(os.path.abspath(__file__)),
+		trunk_depth=10
+	)

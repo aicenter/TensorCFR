a228b96027b5e9d811ee7f407520cf691b2cd59d
Dominik Andreas Seitz <seitzdom@fel.cvut.cz>
2019-01-23 18:43:13 +0100

    bash script for metacentrum to generate cfv and sep reaches for IIGS6
    Signed-off-by: Dominik Andreas Seitz <seitzdom@fel.cvut.cz>

---
+++ src/nn/data/generate_cf_sep_reaches_IIGS6.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#PBS -N II-GS6_50_data_pt
+#PBS -q gpu
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=10:ngpus=1:gpu_cap=cuda35:mem=30gb:scratch_local=10gb
+
+# README
+# This script generates dataset for II-GS6 on Metacentrum's server.
+#
+# Run this command in Metacentrum's command line to run the job:
+#  qsub generate_50_datapoints_of_IIGS6_6cards_as_metacentrum_batch_job.sh
+
+# configure variables
+FRONTNODE_HOME="/storage/brno2/home/seitzdom"
+REPO_DIR="${FRONTNODE_HOME}/beyond-deepstack/TensorCFR"
+FRONTNODE_DATA="${FRONTNODE_HOME}/beyond-deepstack/data/IIGS6/IIGS6_cf_sep_reach"
+DATASET_DIR="${SCRATCHDIR}/TensorCFR/src/nn/data/out/IIGS6_cf_sep_reach"
+
+trap 'clean_scratch' TERM EXIT  # nastaveni uklidu SCRATCHE v pripade chyby
+module add tensorflow-1.7.1-gpu-python3
+
+# move to Metacentrum's temporal drive
+cd ${SCRATCHDIR} || exit 1
+cp -r ${REPO_DIR} .           # copy repo from FRONTNODE
+
+# run TensorCFR
+cd TensorCFR
+python -m src.nn.data.generate_ranges_of_lvl10_IIGS6
+
+# copy results back from temporal drive
+mkdir -p ${FRONTNODE_DATA}
+mv ${DATASET_DIR}/* ${FRONTNODE_DATA} || export CLEAN_SCRATCH=false # presune vysledky do domovskeho adresare nebo je ponecha ve scratchi v pripade chyby
+

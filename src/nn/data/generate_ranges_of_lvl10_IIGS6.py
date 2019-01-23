584c7e47190d090da37a266988b9cad799f4b57d
Dominik Andreas Seitz <seitzdom@fel.cvut.cz>
2019-01-23 18:13:20 +0100

    generate ranges and cf values of lvl10 IIGS6
    Signed-off-by: Dominik Andreas Seitz <seitzdom@fel.cvut.cz>

--- src/nn/data/generate_ranges_of_lvl10_IIGS6.py
+++ src/nn/data/generate_ranges_of_lvl10_IIGS6.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python3
 import os
 
-from src.algorithms.tensorcfr_fixed_trunk_strategies.TensorCFRFixedTrunkStrategies import TensorCFRFixedTrunkStrategies
-from src.domains.available_domains import get_domain_by_name
-from src.nn.data.generate_data import generate_data
+from src.nn.data.generate_data_cv_sep_reach import generate_data_cv_sep_reach
 from src.utils.other_utils import activate_script
-import tensorflow as tf
 
-trunk_depth = 10
-domain = get_domain_by_name("IIGS6_gambit_flattened")
-tensorcfr = TensorCFRFixedTrunkStrategies(domain)
-
-
-session = tf.Session(graph=tensorcfr)
-
-with session.as_default():
-	tf.Print(example)
 
 if __name__ == '__main__' and activate_script():
-	generate_data(
+	generate_data_cv_sep_reach(
 		"IIGS6_gambit_flattened",
 		script_directory=os.path.dirname(os.path.abspath(__file__)),
 		trunk_depth=10

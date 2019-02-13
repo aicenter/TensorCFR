5848b058a896ef87dcea909a8df83fcee406cd24
Jan Rudolf <jan.rudolf.steeldog@gmail.com>
2019-02-13 19:32:24 +0100

    Delete unused and buggy import.

--- src/nn/data/postprocessing_ranges.py
+++ src/nn/data/postprocessing_ranges.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import os
-from src.algorithms.tensorcfr_nn import TensorCFR_Goofstack
 from src.nn.data.preprocessing_ranges import *
 from src.nn.AbstractNN import AbstractNN
 

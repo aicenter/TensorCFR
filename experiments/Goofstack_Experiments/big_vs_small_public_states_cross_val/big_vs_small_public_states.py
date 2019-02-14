95ba39c36455d74c38bf1106c170527db54922fe
Dominik Andreas Seitz <seitzdom@fel.cvut.cz>
2019-02-14 16:55:45 +0100

    blablabla
    Signed-off-by: Dominik Andreas Seitz <seitzdom@fel.cvut.cz>

---
+++ experiments/Goofstack_Experiments/big_vs_small_public_states_cross_val/big_vs_small_public_states.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+import os
+from src.nn.data.postprocessing_ranges import huber_loss,load_nn,linf_loss
+#from experiments.Goofstack_Experiments.layer_depth_vs_width import plot_losses
+
+from src.utils.other_utils import activate_script
+
+
+
+##TODO run network train & val huber loss on old public states (1,1,1) and run same network architecture on
+## small public states from new public states
+## save model.history.history in pickle file
+## https://stackoverflow.com/questions/11218477/how-can-i-use-pickle-to-save-a-dict
+
+
+
+
+

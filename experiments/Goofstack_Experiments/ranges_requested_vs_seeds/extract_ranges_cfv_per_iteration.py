4283330d4036afdf38f4563b7de9621a21ca5fff
Dominik Andreas Seitz <seitzdom@fel.cvut.cz>
2019-02-28 10:31:06 +0100

    extract ranges per cfr iteration
    Signed-off-by: Dominik Andreas Seitz <seitzdom@fel.cvut.cz>

---
+++ experiments/Goofstack_Experiments/ranges_requested_vs_seeds/extract_ranges_cfv_per_iteration.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# import os
+#
+# from src.algorithms.tensorcfr_nn.TensorCFR_Goofstack
+# from src.utils.other_utils import activate_script
+#
+#
+# if __name__ == '__main__' and activate_script():
+# 	gener(
+# 		"IIGS6_gambit_flattened",
+# 		script_directory=os.path.dirname(os.path.abspath(__file__)),
+# 		trunk_depth=10
+# 	)
